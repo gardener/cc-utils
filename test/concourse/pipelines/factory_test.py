bf726b410075ee125984c14f65806e87a6435d3e
Christian Cwienk <ccwienk@dr1fter.de>
2018-04-25 08:35:26 +0200

    unittest to ensure repository triggering logic

--- test/concourse/pipelines/factory_test.py
+++ test/concourse/pipelines/factory_test.py
@@ -49,4 +49,53 @@ class DefinitionFactoryTest(unittest.TestCase):
         # variants may add attributes
         self. assertEqual(variant_b, {'foo': 'bar', 123: 555, 'xxx': 31})
 
+    def test_repository_triggers_logic(self):
+        '''
+        if a repository "triggers" a build, it will (apart from triggering the given build)
+        receive a webhook configuration.
+        By default, only the 'main' repository "triggers". Triggering can be explicitly
+        configured using the 'trigger' attribute. Ensure that different trigger configurations
+        for the same repository will be honoured properly for each variant.
+        '''
+        base_def = {'repo': {'name': 'main_repo', 'branch': 'dontcare', 'path': 'foo/bar'}}
+        variants = {
+            'variant2':
+            {
+                'repo': {'name': 'main_repo', 'trigger': False},
+                'repos':
+                [
+                    {'name': 'other_repo', 'branch': 'x_branch', 'path': 'x/path', 'trigger':True}
+                ]
+            },
+            'variant1':
+            {
+                'repos': [{'name': 'other_repo', 'branch': 'x_branch', 'path': 'b/path'}]
+            },
+            'variant3':
+            {
+                'repo': {'name': 'main_repo', 'trigger': False}
+            },
+        }
+        descriptor = DefDescriptor(name='foo', base_definition=base_def, variants=variants)
+        factory = DefinitionFactory(raw_definition_descriptor=descriptor)
+
+        result = factory.create_pipeline_definition()
+        variant = result.variant('variant1')
+
+        main_repo = variant.repository('main_repo')
+        other_repo = variant.repository('other_repo')
+
+        # main_repo should "trigger" the job (and thus a webhook ought to be generated for it)
+        self.assertTrue(main_repo.should_trigger())
+
+        # non-main repos should not "trigger"
+        self.assertFalse(other_repo.should_trigger())
+
+        variant2 = result.variant('variant2')
+
+        main_repo_v2 = variant2.repository('main_repo')
+        other_repo_v2 = variant2.repository('other_repo')
+
+        self.assertFalse(main_repo_v2.should_trigger())
+        self.assertTrue(other_repo_v2.should_trigger())
 
