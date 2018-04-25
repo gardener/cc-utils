4fa58fb1126f6d3d815377bf7badf8d8d89c58cb
Christian Cwienk <christian.cwienk@sap.com>
2018-04-25 10:53:12 +0200

    rm unnecessary module init coding

--- concourse/__init__.py
+++ concourse/__init__.py
@@ -11,8 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import sys
-import os
-own_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(own_dir, os.path.pardir))
