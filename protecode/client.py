ec5044bd35448e23c68fbf4e6f7c0df6013236d1
Christian Cwienk <christian.cwienk@sap.com>
2018-05-16 13:38:45 +0200

    protecode client factory method, implement file upload

--- protecode/client.py
+++ protecode/client.py
@@ -11,9 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from functools import partial
 
+import requests
+
 from util import not_empty, not_none, urljoin
+from http_requests import AuthenticatedRequestBuilder
 
 class ProtecodeApiRoutes(object):
     def __init__(self, base_url):
@@ -34,6 +38,37 @@ class ProtecodeApiRoutes(object):
 
 
 class ProtecodeApi(object):
-    def __init__(self, api_routes, basic_credentials):
+    def __init__(self, api_routes, basic_credentials, tls_verify=False):
         self._routes = not_none(api_routes)
         self._credentials = not_none(basic_credentials)
+        self._auth = (basic_credentials.username(), basic_credentials.passwd())
+        self._request_builder = AuthenticatedRequestBuilder(
+            basic_auth_username=basic_credentials.username(),
+            basic_auth_passwd=basic_credentials.passwd(),
+            verify_ssl=tls_verify
+        )
+
+    def upload(self, application_name, group_id, data, custom_attribs={}):
+        url = self._routes.upload(file_name=application_name)
+        headers = {'Group': str(group_id)}
+        headers.update({'META-' + k: v for k,v in custom_attribs.items()})
+
+        result = requests.put(
+            url=url,
+            headers=headers,
+            auth=self._auth,
+            data=data,
+        )
+
+        return result.json()
+
+
+def from_cfg(protecode_cfg):
+    not_none(protecode_cfg)
+    routes = ProtecodeApiRoutes(base_url=protecode_cfg.api_url())
+    api = ProtecodeApi(
+        api_routes=routes,
+        basic_credentials=protecode_cfg.credentials(),
+        tls_verify=protecode_cfg.tls_verify()
+    )
+    return api
