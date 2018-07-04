dd2806464fb97889610ab0c62f4d904db5c42b90
Raphael Vogel <raphael.vogel@sap.com>
2018-07-04 13:03:04 +0200

    add public build logs attribute in options trait

--- concourse/pipelines/model/traits/options.py
+++ concourse/pipelines/model/traits/options.py
@@ -17,9 +17,10 @@ from concourse.pipelines.modelbase import Trait, TraitTransformer
 
 class OptionsTrait(Trait):
     def build_logs_to_retain(self):
-        if 'build_logs_to_retain' in self.raw:
-            return self.raw.get('build_logs_to_retain')
-        return 1000
+        return self.raw.get('build_logs_to_retain', 1000)
+
+    def public_build_logs(self):
+        return self.raw.get('public_build_logs', False)
 
     def transformer(self):
         return OptionsTraitTransformer(trait=self, name=self.name)
