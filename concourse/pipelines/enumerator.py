df09d85db3a112d17306df4701d36a36a7465936
Andreas Burger <andreas.burger@sap.com>
2018-08-09 15:11:06 +0200

    reword comment

--- concourse/pipelines/enumerator.py
+++ concourse/pipelines/enumerator.py
@@ -303,8 +303,8 @@ class DefinitionDescriptor(object):
         for override in self.override_definitions:
             effective_definition = merge_dicts(effective_definition, override)
 
-        # TODO: Currently, the main-repository is passed along. However, the merging of the effective definition might
-        # have actually changed that in various ways.
+        # TODO: Currently, the main-repository is passed along. Look into whether it's feasible/useful to infer the
+        # main-repository from the passed definition
         return DefinitionDescriptor(
                 pipeline_name=self.pipeline_name,
                 pipeline_definition=effective_definition,
