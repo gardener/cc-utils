aefb3c25e617106380451e96639bbe12ad1d4a59
Christian Cwienk <christian.cwienk@sap.com>
2018-08-22 11:36:35 +0200

    fix typo in git-ssh-cmd env var

--- gitutil.py
+++ gitutil.py
@@ -76,7 +76,7 @@ class GitHelper(object):
             with open(tmp_id, 'w') as f:
                 f.write(self.github_cfg.credentials().private_key())
             os.chmod(tmp_id, 0o400)
-            os.environ['GIT_SSH_CMD'] = 'ssh -i {id_file} {suppress_hostcheck}'.format(
+            os.environ['GIT_SSH_COMMAND'] = 'ssh -i {id_file} {suppress_hostcheck}'.format(
                 id_file=tmp_id,
                 suppress_hostcheck='-o "StrictHostKeyChecking no"',
             )
