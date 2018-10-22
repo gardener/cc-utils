2e0da5b840c0b25b68ef2822a4d2522d127269ac
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2018-10-22 13:29:24 +0200

    Revert "add branch name to repo resource name"
    This reverts commit ffcd6945adb2d327fe91adc69b6ba686e89a2920.

--- concourse/model/resources.py
+++ concourse/model/resources.py
@@ -263,7 +263,7 @@ class RepositoryConfig(Resource):
 
     def resource_name(self):
         # TODO: replace usages with access to resource_id
-        return self._resource_identifier.name() + '_' + self.branch()
+        return self._resource_identifier.name()
 
     def name(self):
         # TODO: replace usages with access to resource_id
