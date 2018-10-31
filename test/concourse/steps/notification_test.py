56bf956ccfa037c1f82a6f0ddb8d2d6a1f8730de
Christian Cwienk <christian.cwienk@sap.com>
2018-10-31 14:46:46 +0100

    unit tests for notification functions

--- test/concourse/steps/notification_test.py
+++ test/concourse/steps/notification_test.py
@@ -8,7 +8,7 @@ from unittest.mock import MagicMock
 
 import test_utils
 
-from concourse.steps import step_def
+from concourse.steps import step_def, step_lib_def
 from concourse.model.step import PipelineStep
 
 
@@ -33,6 +33,7 @@ class NotificationStepTest(unittest.TestCase):
         self.render_step = step_def('notification')
 
         self.old_cwd = os.getcwd()
+        os.chdir(self.tmp_dir.name)
 
     def tearDown(self):
         self.tmp_dir.cleanup()
@@ -50,3 +51,48 @@ class NotificationStepTest(unittest.TestCase):
 
         # try to compile (-> basic syntax check)
         return compile(step_snippet, 'notification', 'exec')
+
+
+class NotificationStepLibTest(unittest.TestCase):
+    def setUp(self):
+        self.tmp_dir = tempfile.TemporaryDirectory()
+        self.meta_dir = os.path.join(self.tmp_dir.name, 'meta')
+        os.mkdir(self.meta_dir)
+        test_utils.populate_meta_dir(self.meta_dir)
+
+        self.render_step_lib = step_lib_def('notification')
+
+        self.old_cwd = os.getcwd()
+        os.chdir(self.tmp_dir.name)
+
+    def tearDown(self):
+        self.tmp_dir.cleanup()
+        os.chdir(self.old_cwd)
+
+    def test_meta_vars(self):
+        exec(self.render_step_lib())
+
+        result = eval('meta_vars()')
+
+        for name in (
+            'atc-external-url',
+            'build-team-name',
+            'build-pipeline-name',
+            'build-job-name',
+            'build-name'
+        ):
+            self.assertEqual(result[name], name)
+
+    def test_job_url(self):
+        exec(self.render_step_lib())
+        v = {
+            'atc-external-url': 'f://x',
+            'build-team-name': 'team',
+            'build-pipeline-name': 'pl',
+            'build-job-name': 'bjn',
+            'build-name': 'bn'
+        }
+        examinee = vars()['job_url']
+        result = examinee(v)
+
+        self.assertEqual(result, 'f://x/teams/team/pipelines/pl/jobs/bjn/builds/bn')
