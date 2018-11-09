1b8581dbbb6f08133dc1b1724431e584f0e01687
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2018-11-09 15:05:48 +0100

    image reference filters

---
+++ concourse/steps/scan_container_images.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed
+# under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+
+import product.model
+
+
+def image_reference_filter(include_regexes=(), exclude_regexes=()):
+    # compile regexes
+    include_functions = [re.compile(r).fullmatch for r in include_regexes]
+    exclude_functions = [re.compile(r).fullmatch for r in exclude_regexes]
+
+    def _img_ref_filter(image_reference: product.model.ContainerImage):
+        matches = True
+        if include_functions:
+            matches &= any(
+                map(lambda f: f(image_reference.image_reference()), include_functions)
+            )
+
+        # exclusion filter has precedence
+        if exclude_functions:
+            matches &= not any(
+                map(lambda f: f(image_reference.image_reference()), exclude_functions)
+            )
+
+        return matches
+
+    return _img_ref_filter
---
+++ test/concourse/steps/scan_container_images_test.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed
+# under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+import concourse.steps.scan_container_images as scan_container_images
+import product.model
+
+examinee = scan_container_images.image_reference_filter
+
+
+@pytest.fixture
+def img_ref():
+    def _img_ref(image_reference, name='abc', version='1.2.3'):
+        return product.model.ContainerImage.create(
+                name=name,
+                version=version,
+                image_reference=image_reference,
+        )
+    return _img_ref
+
+
+def test_image_reference_filter(img_ref):
+    ref1 = img_ref('image1')
+    ref2 = img_ref('image2/foo')
+    ref3 = img_ref('another_image1')
+    ref4 = img_ref('unrelated/for/testing')
+
+    default_filter = examinee()
+
+    assert default_filter(ref1) # by default, nothing should be filtered out
+    assert default_filter(ref2)
+    assert default_filter(ref3)
+    assert default_filter(ref4)
+
+    # include only images starting with 'image'
+    include_filter = examinee(include_regexes=('image.*',))
+
+    assert include_filter(ref1)
+    assert include_filter(ref2)
+    assert not include_filter(ref3)
+    assert not include_filter(ref4)
+
+    # exclude images containing the string 'image'
+    exclude_filter = examinee(exclude_regexes=('.*image.*',))
+
+    assert not exclude_filter(ref1)
+    assert not exclude_filter(ref2)
+    assert not exclude_filter(ref3)
+    assert exclude_filter(ref4)
+
+    # exclude should have precedency over include
+    exclude_with_predence = examinee(include_regexes=('.*image.*',), exclude_regexes=('.*2.*',))
+
+    assert exclude_with_predence(ref1)
+    assert not exclude_with_predence(ref2)
+    assert exclude_with_predence(ref3)
+    assert not exclude_with_predence(ref4)
