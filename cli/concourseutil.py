ccf73ef4a08731abeaa03f0bf289dccc0cb3ddde
Andreas Burger <andreas.burger@sap.com>
2018-11-12 16:23:29 +0000

    add cli function to deploy (or upgrade) a webhook dispatcher

--- cli/concourseutil.py
+++ cli/concourseutil.py
@@ -316,3 +316,24 @@ def trigger_resource_check(
         pipeline_name=pipeline_name,
         resource_name=resource_name,
     )
+
+
+def deploy_or_upgrade_webhook_dispatcher(
+    webhook_dispatcher_deployment_name: CliHints.non_empty_string(
+        help="webhook dispatcher deployment config name"
+    ),
+    chart_dir: CliHints.existing_dir(help="directory of webhook dispatcher chart"),
+    deployment_name: str='webhook-dispatcher',
+):
+    chart_dir = os.path.abspath(chart_dir)
+
+    cfg_factory = ctx().cfg_factory()
+    webhook_dispatcher_deployment_cfg = cfg_factory.webhook_dispatcher_deployment(
+        webhook_dispatcher_deployment_name
+    )
+
+    setup.deploy_webhook_dispatcher_landscape(
+        webhook_dispatcher_deployment_cfg=webhook_dispatcher_deployment_cfg,
+        chart_dir=chart_dir,
+        deployment_name=deployment_name,
+    )
