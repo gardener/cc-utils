6d989daee67a7dc119a8d2ea8b298ab9218feb62
Raphael Vogel <raphael.vogel@sap.com>
2018-11-27 13:44:28 +0100

    enumerate required org webhooks

--- concourse/util.py
+++ concourse/util.py
@@ -26,9 +26,13 @@ from model.github import (
 from model.concourse import (
     ConcourseTeamCredentials,
     ConcourseConfig,
+    JobMappingSet,
     JobMapping,
 )
-from util import info, warning, fail
+from model.webhook_dispatcher import (
+    WebhookDispatcherDeploymentConfig,
+)
+from util import info, warning, fail, ctx, create_url_from_attributes
 
 
 def list_github_resources(
@@ -173,3 +177,44 @@ def _sync_webhook(
     )
     if removed > 0:
         info('removed {c} outdated hook(s)'.format(c=removed))
+
+
+def _enumerate_required_org_webhooks(
+    whd_deployment_cfg: WebhookDispatcherDeploymentConfig,
+):
+    '''Returns tuples of 'github orgname', 'github api object' and 'webhook url' '''
+    cfg_factory = ctx().cfg_factory()
+
+    whd_cfg_name = whd_deployment_cfg.webhook_dispatcher_config_name()
+    whd_cfg = cfg_factory.webhook_dispatcher(whd_cfg_name)
+
+    concourse_cfg_names = whd_cfg.concourse_config_names()
+    concourse_cfgs = map(cfg_factory.concourse, concourse_cfg_names)
+
+    for concourse_cfg in concourse_cfgs:
+        job_mapping_set = cfg_factory.job_mapping(concourse_cfg.job_mapping_cfg_name())
+
+        for github_orgname, github_cfg_name in _enumerate_github_org_configs(job_mapping_set):
+            github_api = _create_github_api_object(
+                github_cfg=cfg_factory.github(github_cfg_name),
+            )
+
+            webhook_url = create_url_from_attributes(
+                netloc=whd_deployment_cfg.ingress_host(),
+                scheme='https',
+                path='github-webhook',
+                params='',
+                query = '{name}={value}'.format(name='whd_config_name', value=whd_cfg_name),
+                fragment=''
+            )
+
+            yield (github_orgname, github_api, webhook_url)
+
+
+def _enumerate_github_org_configs(job_mapping_set: JobMappingSet,):
+    '''Returns tuples of github org names and github config names'''
+    for job_mapping in job_mapping_set.job_mappings().items():
+        github_org_configs = job_mapping.github_organizations()
+
+        for github_org_config in github_org_configs:
+            yield (github_org_config.org_name(), github_org_config.github_cfg_name)
