304bef99c74f059ee0ab9d22e088b0c760dbfb56
Andreas Burger <andreas.burger@sap.com>
2018-12-11 16:16:44 +0000

    add python script for automatic template version bump

---
+++ .ci/bump_job_image_version.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import pathlib
+import fileinput
+
+repo_dir = os.environ.get('SOURCE_PATH')
+if repo_dir is None:
+    raise RuntimeError('SOURCE_PATH environment variable must be set')
+
+version_path = os.environ.get('VERSION_PATH')
+if version_path is None:
+    raise RuntimeError('VERSION_PATH environment variable must be set')
+
+template_file = pathlib.Path(repo_dir, 'concourse', 'resources', 'defaults.mako')
+version_file = pathlib.Path(version_path, 'version')
+
+if not template_file.is_file():
+    raise RuntimeError(f'Template file not found at {template_file}')
+if not version_file.is_file():
+    raise RuntimeError(f'Verson file not found at {version_file}')
+
+version = version_file.read_text()
+lines_replaced = 0
+
+for line in fileinput.FileInput(str(template_file), inplace=True):
+    if 'tag:' in line:
+        if lines_replaced is not 0:
+            raise RuntimeError(f'More than one image tag found in template file')
+        leading_spaces = line.index('tag:')
+        print(f'{leading_spaces * " "}tag: "{version}"')
+        lines_replaced = 1
+    else:
+        print(line, end='')
