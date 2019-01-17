cdbd53619365b37fd1a59d9fe06355b50916b91b
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2019-01-17 08:20:34 +0100

    also log repository name for erroneous pipeline definitions

--- concourse/replicator.py
+++ concourse/replicator.py
@@ -375,7 +375,7 @@ class ReplicationResultProcessor(object):
             codeowners_enumerator.enumerate_remote_repo(github_repo_helper=repo_helper)
         ))
 
-        info(f'Sending notification e-mail to {recipients}')
+        info(f'Sending notification e-mail to {recipients} ({main_repo["path"]})')
         email_cfg = self._cfg_set.email()
         _send_mail(
             email_cfg=email_cfg,
