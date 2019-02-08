476c41d84b798156e4ada3e771debed5b58fe874
Christian Cwienk <christian.cwienk@sap.com>
2019-02-08 13:52:38 +0100

    convenience function for filter-image (handle down/upload)

--- cli/containerutil.py
+++ cli/containerutil.py
@@ -1,8 +1,11 @@
+import tempfile
+
+import container.registry
 import container.util
 import util
 
 
-def filter_image(
+def filter_image_file(
     in_file:str,
     out_file:str,
     remove_files:[str]=(),
@@ -17,10 +20,32 @@ def filter_image(
     [0] https://github.com/opencontainers/image-spec
     '''
     if not remove_files:
-        util.warn('no files to remove were specified - the output will remain unaltered')
+        util.warning('no files to remove were specified - the output will remain unaltered')
 
     container.util.filter_container_image(
         image_file=in_file,
         out_file=out_file,
         remove_entries=remove_files,
     )
+
+
+def filter_image(
+    source_ref:str,
+    target_ref:str,
+    remove_files:[str]=[],
+):
+    with tempfile.NamedTemporaryFile() as in_fh:
+        container.registry.retrieve_container_image(image_reference=source_ref, outfileobj=in_fh)
+
+        # XXX enable filter_image_file / filter_container_image to work w/o named files
+        with tempfile.NamedTemporaryFile() as out_fh:
+            filter_image_file(
+                in_file=in_fh.name,
+                out_file=out_fh.name,
+                remove_files=remove_files
+            )
+
+            container.registry.publish_container_image(
+                image_reference=target_ref,
+                image_file_obj=out_fh,
+            )
