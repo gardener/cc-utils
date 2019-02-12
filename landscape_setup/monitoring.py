fdb3b1c46c8319475a26850fbfbc8656e5ae876f
Raphael Vogel <raphael.vogel@sap.com>
2019-02-12 10:24:15 +0100

    add deployment gear for postgres exporter

--- landscape_setup/monitoring.py
+++ landscape_setup/monitoring.py
@@ -19,16 +19,23 @@ from landscape_setup import kube_ctx
 from landscape_setup.utils import (
     ensure_cluster_version,
     execute_helm_deployment,
+    LiteralStr,
 )
+from model import ConfigFactory
 from model.kubernetes import (
     KubernetesConfig,
     MonitoringConfig,
+)
+from model.concourse import (
+    ConcourseConfig,
 )
 
 
 @ensure_annotations
 def deploy_monitoring_landscape(
     kubernetes_cfg: KubernetesConfig,
+    concourse_cfg: ConcourseConfig,
+    cfg_factory: ConfigFactory,
 ):
     # Set the global context to the cluster specified in KubernetesConfig
     kube_ctx.set_kubecfg(kubernetes_cfg.kubeconfig())
@@ -45,6 +52,19 @@ def deploy_monitoring_landscape(
         'stable/kube-state-metrics',
         'kube-state-metrics',
         kube_state_metrics_helm_values,
+    )
+
+    # deploy postgresql exporter
+    postgresql_helm_values = create_postgresql_helm_values(
+        concourse_cfg=concourse_cfg,
+        cfg_factory=cfg_factory,
+    )
+    execute_helm_deployment(
+        kubernetes_cfg,
+        monitoring_namespace,
+        'stable/prometheus-postgres-exporter',
+        'prometheus-postgres-exporter',
+        postgresql_helm_values,
     )
 
 
@@ -81,5 +101,53 @@ def create_kube_state_metrics_helm_values(
         },
         "collectors": used_collectors,
         "namespace": ','.join(namespaces_to_monitor)
+    }
+    return helm_values
+
+
+def create_postgresql_helm_values(
+    concourse_cfg: ConcourseConfig,
+    cfg_factory: ConfigFactory,
+):
+    helm_chart_default_values_name = concourse_cfg.helm_chart_default_values_config()
+    default_helm_values = cfg_factory.concourse_helmchart(helm_chart_default_values_name).raw
+    helm_chart_values_name = concourse_cfg.helm_chart_values()
+    custom_helm_values = cfg_factory.concourse_helmchart(helm_chart_values_name).raw
+
+    helm_values = {
+        "service": {
+            "annotations": {
+                "prometheus.io/scrape": "true"
+            }
+        },
+        "rbac": {
+            "create": False,
+            "pspEnabled": False
+        },
+        "serviceAccount": {
+            "create": False
+        },
+        "config": {
+            "datasource": {
+                "host": "concourse-postgresql.concourse.svc.cluster.local",
+                "user": default_helm_values.get('postgresql').get('postgresUser'),
+                "password": custom_helm_values.get('postgresql').get('postgresPassword'),
+                "database": default_helm_values.get('postgresql').get('postgresDatabase'),
+                "sslmode": "disable"
+            },
+            "disableDefaultMetrics": True,
+            "queries": LiteralStr('''
+                pg_database:
+                    query: "SELECT pg_database.datname, pg_database_size(pg_database.datname)
+                            as size FROM pg_database"
+                    metrics:
+                    - datname:
+                        usage: "LABEL"
+                        description: "Name of the database"
+                    - size:
+                        usage: "GAUGE"
+                        description: "Disk space used by the database" '''
+            )
+        }
     }
     return helm_values
