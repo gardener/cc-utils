7a5df96791960a48addce2dd6495323ba325042d
Andreas Burger <andreas.burger@sap.com>
2019-03-14 09:49:14 +0000

    cli: enable listing of only outdated draft releases
    see diff for detailed definition of an 'outdated draft release'
    also: sort imports

--- cli/githubutil.py
+++ cli/githubutil.py
@@ -21,10 +21,13 @@ from gitutil import GitHelper
 from github.util import (
     GitHubRepositoryHelper,
     GitHubRepoBranch,
+    _add_user_to_team,
+    _add_all_repos_to_team,
     _create_github_api_object,
     _create_team,
-    _add_user_to_team,
-    _add_all_repos_to_team
+    find_greatest_github_release_version,
+    outdated_draft_releases,
+
 )
 from github.release_notes.util import (
     ReleaseNotes,
@@ -146,15 +149,42 @@ def list_draft_releases(
     github_cfg_name: str,
     github_repository_owner: str,
     github_repository_name: str,
+    only_outdated: bool = False,
 ):
+    '''List all draft releases in a GitHub repository. If the `--only-outdated` flag is set,
+    only outdated draft releases are printed. A draft release is considered outdated iff:
+        1: its version is smaller than the greatest release version (according to semver) AND
+            2a: it is NOT a hotfix draft release AND
+            2b: there are no hotfix draft releases with the same major and minor version
+            OR
+            3a: it is a hotfix draft release AND
+            3b: there is a hotfix draft release of greater version (according to semver)
+                with the same major and minor version
+
+    Hotfix draft release in this context are draft releases with a semver patch version that is
+    not equal to 0.
+    '''
     github_cfg = ctx().cfg_factory().github(github_cfg_name)
     github_helper = GitHubRepositoryHelper(
         owner=github_repository_owner,
         name=github_repository_name,
         github_cfg=github_cfg,
     )
-    release_list = [release for release in github_helper.repository.releases() if release.draft]
-    for draft_release in release_list:
+    if only_outdated:
+        releases = [release for release in github_helper.repository.releases()]
+        non_draft_releases = [release for release in releases if not release.draft]
+        greatest_release_version = find_greatest_github_release_version(non_draft_releases)
+    else:
+        releases = github_helper.repository.releases()
+
+    draft_releases = [release for release in releases if release.draft]
+
+    if only_outdated:
+        draft_releases = outdated_draft_releases(
+            draft_releases=draft_releases,
+            greatest_release_version=greatest_release_version,
+        )
+    for draft_release in draft_releases:
         print(draft_release.name)
 
 
