7bbf2648ffae17a0eab95baf961dbf4e43bc9cbe
Christian Cwienk <christian.cwienk@sap.com>
2019-05-20 17:25:43 +0200

    refactor image_scan trait (protecode as option)

--- concourse/model/traits/image_scan.py
+++ concourse/model/traits/image_scan.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from deprecated import deprecated
+
 from concourse.model.job import (
     JobVariant,
 )
@@ -30,6 +32,86 @@ from concourse.model.base import (
 from product.scanning import ProcessingMode
 
 from .component_descriptor import COMPONENT_DESCRIPTOR_DIR_INPUT
+
+
+PROTECODE_ATTRS = (
+    AttributeSpec.optional(
+        name='parallel_jobs',
+        default=12,
+        doc='amount of parallel scanning threads',
+        type=int,
+    ),
+    AttributeSpec.optional(
+        name='cve_threshold',
+        default=7,
+        doc='CVE threshold to interpret as an error',
+        type=int,
+    ),
+    AttributeSpec.optional(
+        name='processing_mode',
+        default='upload_if_changed',
+        doc='Protecode processing mode',
+        type=ProcessingMode,
+    ),
+    AttributeSpec.optional(
+        name='reference_protecode_group_ids',
+        default=(),
+        doc='''
+        an optional list of protecode group IDs to import triages from.
+        ''',
+    ),
+    AttributeSpec.required(
+        name='protecode_group_id',
+        doc='technical protecode group id to upload to',
+        type=int,
+    ),
+    AttributeSpec.optional(
+        name='protecode_cfg_name',
+        default=None,
+        doc='protecode cfg name to use (see cc-utils)',
+    ),
+    AttributeSpec.optional(
+        name='upload_registry_prefix',
+        default=None,
+        doc='''
+        if specified, all matching container images are also uploaded as copies to
+        the specified container registry. The original image reference names are
+        mangled.
+        '''
+    ),
+)
+
+
+class ProtecodeScanCfg(Trait):
+    @classmethod
+    def _attribute_specs(cls):
+        return PROTECODE_ATTRS
+
+    def reference_protecode_group_ids(self):
+        return self.raw['reference_protecode_group_ids']
+
+    def protecode_group_id(self):
+        return self.raw.get('protecode_group_id')
+
+    def protecode_cfg_name(self):
+        return self.raw.get('protecode_cfg_name')
+
+    def parallel_jobs(self):
+        return self.raw.get('parallel_jobs')
+
+    def cve_threshold(self):
+        return self.raw.get('cve_threshold')
+
+    def processing_mode(self):
+        return self.raw.get('processing_mode')
+
+    def upload_registry_prefix(self):
+        return self.raw['upload_registry_prefix']
+
+    def validate(self):
+        super().validate()
+        # Use enum.Enum's validation to validate configured processing mode.
+        ProcessingMode(self.processing_mode())
 
 
 class FilterCfg(ModelBase):
@@ -73,6 +155,12 @@ ATTRIBUTES = (
         name='email_recipients',
         default=(),
         doc='optional email recipients to be notified about critical scan results',
+    ),
+    AttributeSpec.optional(
+        name='protecode',
+        default=None,
+        type=ProtecodeScanCfg,
+        doc='if present, perform protecode scanning',
     ),
 
     # XXX attrs below are kept temporarily to not break old cfgs - to be removed
@@ -134,25 +222,40 @@ class ImageScanTrait(Trait):
     def email_recipients(self):
         return self.raw['email_recipients']
 
+    def protecode(self):
+        # XXX remove backward compatibility
+        if not 'protecode' in self.raw:
+            util.warning('legacy protecode cfg - adjust pipeline_definition!')
+            return ProtecodeScanCfg(raw=self.raw)
+        # TODO: after schema change, protecode cfg should become optional
+        return ProtecodeScanCfg(raw=self.raw['protecode'])
+
     # XXX attrs below are kept temporarily to not break old cfgs - to be removed
+    @deprecated
     def reference_protecode_group_ids(self):
         return self.raw['reference_protecode_group_ids']
 
+    @deprecated
     def protecode_group_id(self):
         return self.raw.get('protecode_group_id')
 
+    @deprecated
     def protecode_cfg_name(self):
         return self.raw.get('protecode_cfg_name')
 
+    @deprecated
     def parallel_jobs(self):
         return self.raw.get('parallel_jobs')
 
+    @deprecated
     def cve_threshold(self):
         return self.raw.get('cve_threshold')
 
+    @deprecated
     def processing_mode(self):
         return self.raw.get('processing_mode')
 
+    @deprecated
     def upload_registry_prefix(self):
         return self.raw['upload_registry_prefix']
 
