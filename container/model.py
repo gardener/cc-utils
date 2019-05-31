8534acf0318c39c95b21c9440b320f90de9cf51c
Christian Cwienk <christian.cwienk@sap.com>
2019-05-31 10:07:57 +0200

    make compliant to python3.6

--- container/model.py
+++ container/model.py
@@ -6,5 +6,5 @@ processing_callback: callable; called with (in_fh, out_fh)
 ContainerImageUploadRequest = collections.namedtuple(
     'ContainerImageUploadRequest',
     ['source_ref', 'target_ref', 'processing_callback'],
-    defaults=[None],
+    # defaults=[None], XXX re-enable after upgrading to Python3.7
 )
