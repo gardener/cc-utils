07ef3a86c8a832e1ce710e30dff648918debc839
Andreas Burger <andreas.burger@sap.com>
2019-07-01 15:30:47 +0000

    add functionality to clamav model class

--- model/clamav.py
+++ model/clamav.py
@@ -14,11 +14,50 @@
 # limitations under the License.
 
 from model.base import NamedModelElement
+from model.proxy import DockerImageConfig
 
 
 class ClamAVConfig(NamedModelElement):
-    '''A stub for the ClamAV config.
-
-    Not intended to be instantiated by users of this module
+    '''Not intended to be instantiated by users of this module
     '''
-    pass
+
+    def namespace(self):
+        return self.raw.get('namespace')
+
+    def freshclam_image_config(self):
+        return DockerImageConfig(self.raw.get('freshclam_image'))
+
+    def clamav_image_config(self):
+        return DockerImageConfig(self.raw.get('clamav_image'))
+
+    def service_name(self):
+        return self.raw.get('service_name')
+
+    def service_port(self):
+        return self.raw.get('service_port')
+
+    def cluster_url(self):
+        return f'{self.service_name}.{self.namespace()}.svc.cluster.local:{self.service_port()}'
+
+    def container_registry_config_name(self):
+        return self.raw.get('container_registry')
+
+    def replicas(self):
+        return self.raw.get('replicas')
+
+    def _required_attributes(self):
+        yield from super()._required_attributes()
+        yield from [
+            'namespace',
+            'service_name',
+            'service_port',
+            'freshclam_image',
+            'clamav_image',
+            'replicas',
+        ]
+
+    def _optional_attributes(self):
+        yield from super()._optional_attributes()
+        yield from [
+            'container_registry',
+        ]
