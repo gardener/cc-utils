6dfa9abb8e0c5036da058356ee65d5bc96474af6
Andreas Burger <andreas.burger@sap.com>
2019-07-04 08:49:36 +0000

    prepare routes for future ClamAV client

---
+++ clamav/client/routes.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed
+# under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ensure import ensure_annotations
+from util import urljoin
+
+
+class ClamAVRoutes(object):
+    '''Constructs ClamAV REST API endpoint URLs for the ClamAV base URL.
+
+    Not intended to be used outside of this module.
+    '''
+    @ensure_annotations
+    def __init__(self, base_url: str):
+        '''
+        @param base_url: the ClamAV cluster-service URL
+        '''
+        self.base_url = base_url
+
+    def _api_url(self, *parts, **kwargs):
+        return urljoin(self.base_url, parts)
+
+    def scan(self):
+        return self._api_url('scan')
+
+    def info(self):
+        return self._api_url('info')
+
+    def monitor(self):
+        return self._api_url('monitor')
+
+    def health(self):
+        return self._api_url('health')
+
+    def signature_version(self):
+        return self._api_url('signature-version')
