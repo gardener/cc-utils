9b1ffffd2b8f1a4622ed2587f16aba15763575a4
Andreas Burger <andreas.burger@sap.com>
2019-07-09 15:31:20 +0000

    add factory functions for clamav client

---
+++ ccc/clamav.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed
+# under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import util
+
+from clamav.client import ClamAVClient
+from clamav.routes import ClamAVRoutes
+from model.clamav import ClamAVConfig
+
+
+def client_from_config(clamav_config: ClamAVConfig):
+    url = clamav_config.service_url()
+    routes = ClamAVRoutes(base_url=url)
+    return ClamAVClient(routes=routes)
+
+
+def client_from_config_name(clamav_config_name: str):
+    cfg_factory = util.ctx().cfg_factory()
+    clamav_config = cfg_factory.clamav(clamav_config_name)
+    return client_from_config(clamav_config)
