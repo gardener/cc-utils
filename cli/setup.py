5a2ba4d26e6445dee58d7ffe30695c5065ca6f4d
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2019-08-06 10:48:16 +0200

    add setup.py for gardener-cicd-cli

---
+++ cli/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import os
+
+own_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def requirements():
+    yield f'gardener-cicd-libs=={version()}'
+
+
+def modules():
+    return []
+    return [
+        os.path.basename(os.path.splitext(module)[0]) for module in
+        os.scandir(path=own_dir)
+        if module.is_file() and module.name.endswith('.py')
+    ]
+
+
+def version():
+    with open(os.path.join(own_dir, os.pardir, 'VERSION')) as f:
+        return f.read().strip()
+
+
+setuptools.setup(
+    name='gardener-cicd-cli',
+    version=version(),
+    description='Gardener CI/CD Command Line Interface',
+    python_requires='>=3.7.*',
+    py_modules=modules(),
+    packages=setuptools.find_packages(),
+    install_requires=list(requirements()),
+    entry_points={
+        'console_scripts': [
+            'gardener-ci = gardener_ci.cli_gen:main',
+            'cli.py = gardener_ci.cli_gen:main', # XXX backwards-compatibilty - rm this
+        ],
+    },
+)
