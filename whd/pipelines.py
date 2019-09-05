bbd37638cd2b8f06702cf5f07a4414b5bca46a7d
Andreas Burger <andreas.burger@sap.com>
2019-09-05 08:19:59 +0000

    whd: do not unpause pipelines after deployment

--- whd/pipelines.py
+++ whd/pipelines.py
@@ -41,7 +41,7 @@ def update_repository_pipelines(
         cfg_set=cfg_set,
     )
     deployer = concourse.replicator.ConcourseDeployer(
-        unpause_pipelines=True,
+        unpause_pipelines=False,
         expose_pipelines=True,
     )
 
