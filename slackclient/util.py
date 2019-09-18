403e71491f3dbc20f1756f3a5ad0bce589b3c09b
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2019-09-18 09:17:24 +0200

    workaround crappy slack-API - retry upon sporadic errs
    Slack-API will sporadically fail (pbly due to timeout) to convert large
    Markdown uploads. Mitigate this by performing some retries.

--- slackclient/util.py
+++ slackclient/util.py
@@ -15,7 +15,7 @@
 
 import slack
 
-from util import info
+from util import info, warning
 from model.slack import SlackConfig
 
 
@@ -45,7 +45,9 @@ class SlackHelper(object):
         # https://api.slack.com/changelog/2018-04-truncating-really-long-messages
         # Instead we use the file upload mechanism so that this limit does not apply.
         # For contents of result see https://api.slack.com/methods/files.upload
-        response = client.files_upload(
+        response = self._post_with_retry(
+            client=client,
+            retries=5,
             channels=channel,
             content=message,
             title=title,
@@ -54,6 +56,20 @@ class SlackHelper(object):
         if not response['ok']:
             raise RuntimeError(f"failed to post to slack channel '{channel}': {response['error']}")
         return response
+
+    def _post_with_retry(self, client, retries=5, **kwargs):
+        try:
+            response = client.files_upload(**kwargs)
+            return response
+        except slack.errors.SlackApiError as sae:
+            error_code = sae.response.get('error')
+            if retries < 1:
+                raise sae # no retries left (or none requested)
+            if error_code == 'markdown_conversion_failed_because_of_read_failed':
+                warning(f'received {error_code} - retrying {retries}')
+                return self._post_with_retry(client=client, retries=retries-1, **kwargs)
+            else:
+                raise sae # only retry for known sporadic err
 
     def delete_file(
         self,
