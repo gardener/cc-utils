9aad5223c9cb144bee62c3320c99bfe9dfe9e1f3
Andreas Burger <andreas.burger@sap.com>
2019-10-01 07:36:31 +0000

    reorder imports

--- concourse/util.py
+++ concourse/util.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import functools
 import dataclasses
+import functools
 import json
 import os
 
