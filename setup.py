6a32b506714de3099e97a1a86cd9e400efce5df5
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2019-11-06 15:00:16 +0100

    2nd try..

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ own_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 def requirements():
+    yield 'gardener-cicd-base'
+
     with open(os.path.join(own_dir, 'requirements.txt')) as f:
         for line in f.readlines():
             line = line.strip()
@@ -14,11 +16,25 @@ def requirements():
 
 
 def modules():
-    return [
+    module_names = [
         os.path.basename(os.path.splitext(module)[0]) for module in
         os.scandir(path=own_dir)
         if module.is_file() and module.name.endswith('.py')
     ]
+
+    # remove modules already contained in gardener-cicd-base
+    module_names.remove('util')
+    module_names.remove('ctx')
+    return module_names
+
+
+def packages():
+    package_names = setuptools.find_packages()
+
+    # remove packages already contained in gardener-cicd-base
+    package_names.remove('ci')
+    package_names.remove('model')
+    return package_names
 
 
 def version():
@@ -32,7 +48,7 @@ setuptools.setup(
     description='Gardener CI/CD Libraries',
     python_requires='>=3.7.*',
     py_modules=modules(),
-    packages=setuptools.find_packages(),
+    packages=packages(),
     package_data={
         '':['*.mako'],
         'ci':['version'],
