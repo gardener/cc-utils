44033e22a2f457ad736fff001f9643c272074f1a
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2019-11-06 15:03:24 +0100

    exempt setup from release-pkg

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ def modules():
     # remove modules already contained in gardener-cicd-base
     module_names.remove('util')
     module_names.remove('ctx')
+    module_names.remove('setup')
     return module_names
 
 
