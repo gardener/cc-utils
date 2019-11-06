c359695de0761b923dcea78faf110679933fd80a
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2019-11-06 14:43:04 +0100

    Revert "split gardener-cicd-libs"
    This reverts commit 54d779eabd196f47b68a09852e2ce1a3d849dc76.

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ own_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 def requirements():
-    yield 'gardener-cicd-base'
-
     with open(os.path.join(own_dir, 'requirements.txt')) as f:
         for line in f.readlines():
             line = line.strip()
@@ -16,23 +14,11 @@ def requirements():
 
 
 def modules():
-    module_names = [
+    return [
         os.path.basename(os.path.splitext(module)[0]) for module in
         os.scandir(path=own_dir)
         if module.is_file() and module.name.endswith('.py')
     ]
-
-    # remove modules already contained in gardener-cicd-base
-    module_names.remove('util')
-    module_names.remove('ctx')
-
-
-def packages():
-    package_names = setuptools.find_packages()
-
-    # remove packages already contained in gardener-cicd-base
-    package_names.remove('ci')
-    package_names.remove('model')
 
 
 def version():
@@ -46,7 +32,7 @@ setuptools.setup(
     description='Gardener CI/CD Libraries',
     python_requires='>=3.7.*',
     py_modules=modules(),
-    packages=packages(),
+    packages=setuptools.find_packages(),
     package_data={
         '':['*.mako'],
         'ci':['version'],
