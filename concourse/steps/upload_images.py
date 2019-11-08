1af452eebff8aad9ac070cb4f6d2d7aa86a2bd02
Christian Cwienk <christian.cwienk@sap.com>
2019-11-08 15:03:57 +0100

    skip image re-upload if target already exists

--- concourse/steps/upload_images.py
+++ concourse/steps/upload_images.py
@@ -15,6 +15,10 @@ def republish_image(
 
     tgt_ref = ci.util.urljoin(tgt_prefix, ':'.join((img_ref, tag)))
 
+    if container.registry._image_exists(image_reference=tgt_ref):
+        print(f'skipping image upload (already exists): {tgt_ref}')
+        return src_ref, tgt_ref
+
     with tempfile.NamedTemporaryFile() as tmp_file:
         container.registry.retrieve_container_image(image_reference=src_ref, outfileobj=tmp_file)
         container.registry.publish_container_image(image_reference=tgt_ref, image_file_obj=tmp_file)
