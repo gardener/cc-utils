3700a7fc3a1690cc9a64f50ba7755f5e6f932484
Raphael Vogel <raphael.vogel@sap.com>
2019-11-20 10:33:05 +0100

    adapt tests for 'release commit' and 'next dev cycle commit' steps

--- test/concourse/steps/release_test.py
+++ test/concourse/steps/release_test.py
@@ -6,7 +6,7 @@ from github.release_notes.util import ReleaseNotes
 import concourse.steps.release
 
 
-class TestReleaseCommitsStep(object):
+class TestReleaseCommitStep(object):
     @pytest.fixture()
     def examinee(self, tmp_path):
         # create required temporary file relative to the provided temporary directory
@@ -18,23 +18,15 @@ class TestReleaseCommitsStep(object):
             release_version='1.0.0',
             repository_version_file_path='version_file',
             repository_branch='master',
-            version_operation='bump_minor',
-            prerelease_suffix='dev',
-            rebase_before_release=True,
             release_commit_callback=None,
-            dev_cycle_commit_callback=None,
             ):
-            return concourse.steps.release.ReleaseCommitsStep(
+            return concourse.steps.release.ReleaseCommitStep(
                 git_helper=MagicMock(),
                 repo_dir=repo_dir,
                 release_version=release_version,
-                version_operation=version_operation,
-                prerelease_suffix=prerelease_suffix,
                 repository_version_file_path=repository_version_file_path,
                 repository_branch=repository_branch,
-                rebase_before_release=rebase_before_release,
                 release_commit_callback=release_commit_callback,
-                next_version_callback=dev_cycle_commit_callback,
             )
         return _examinee
 
@@ -50,6 +42,51 @@ class TestReleaseCommitsStep(object):
         with pytest.raises(ValueError):
             # pass non-existing relative file-name
             examinee(release_commit_callback='no_such_file').validate()
+
+    def test_validation_fail_on_missing_version_file(self, examinee, tmp_path):
+        with pytest.raises(ValueError):
+            examinee(repository_version_file_path='no_such_file').validate()
+
+    def test_validation_fail_on_invalid_semver(self, examinee):
+        with pytest.raises(ValueError):
+            examinee(release_version='invalid_semver').validate()
+
+
+class NextDevCycleCommitStep(object):
+    @pytest.fixture()
+    def examinee(self, tmp_path):
+        # create required temporary file relative to the provided temporary directory
+        temporary_version_file = tmp_path.joinpath('version_file')
+        temporary_version_file.touch()
+
+        def _examinee(
+            repo_dir=str(tmp_path),
+            release_version='1.0.0',
+            repository_version_file_path='version_file',
+            repository_branch='master',
+            version_operation='bump_minor',
+            prerelease_suffix='dev',
+            dev_cycle_commit_callback=None,
+            ):
+            return concourse.steps.release.NextDevCycleCommitStep(
+                git_helper=MagicMock(),
+                repo_dir=repo_dir,
+                release_version=release_version,
+                repository_version_file_path=repository_version_file_path,
+                repository_branch=repository_branch,
+                version_operation=version_operation,
+                prerelease_suffix=prerelease_suffix,
+                next_version_callback=dev_cycle_commit_callback,
+            )
+        return _examinee
+
+    def test_validation(self, examinee, tmp_path):
+        # create temporary files in the provided directory
+        temporary_callback_file = tmp_path.joinpath('callback_script')
+        temporary_callback_file.touch()
+        examinee(
+            dev_cycle_commit_callback='callback_script',
+        ).validate()
 
     def test_validation_fail_on_missing_dev_cycle_callback_script(self, examinee, tmp_path):
         with pytest.raises(ValueError):
