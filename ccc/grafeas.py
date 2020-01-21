f77159b717c8c86ed69c8986dc78b64828c9316a
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2020-01-21 12:16:24 +0100

    retrieve and filter vulnerabilities from GCRs

--- ccc/grafeas.py
+++ ccc/grafeas.py
@@ -1,14 +1,26 @@
+import logging
+import urllib.parse
+
 import google.cloud.devtools.containeranalysis_v1
 import grafeas.grafeas_v1
 import grafeas.grafeas_v1.gapic.transports.grafeas_grpc_transport
 
+import container.registry
 import model.container_registry
 
 gcrp_transport = grafeas.grafeas_v1.gapic.transports.grafeas_grpc_transport
 container_analysis_v1 = google.cloud.devtools.containeranalysis_v1
 
+logger = logging.getLogger(__name__)
 
-def grafeas_client(credentials: model.container_registry.GcrCredentials):
+
+class VulnerabilitiesRetrievalFailed(RuntimeError):
+    pass
+
+
+def grafeas_client(container_registry_cfg: model.container_registry.ContainerRegistryConfig):
+    credentials = container_registry_cfg.credentials()
+
     service_address = container_analysis_v1.ContainerAnalysisClient.SERVICE_ADDRESS
     default_oauth_scope = (
         'https://www.googleapis.com/auth/cloud-platform',
@@ -20,3 +32,38 @@ def grafeas_client(credentials: model.container_registry.GcrCredentials):
     )
 
     return grafeas.grafeas_v1.GrafeasClient(transport)
+
+
+def retrieve_vulnerabilities(image_reference: str, cvss_threshold: int=7.0):
+    image_reference = container.registry.normalise_image_reference(image_reference)
+    # XXX: should tell required privilege (-> read-vulnerabilities)
+    registry_cfg = model.container_registry.find_config(image_reference=image_reference)
+    if not registry_cfg:
+        raise VulnerabilitiesRetrievalFailed('no registry-cfg found')
+
+    logger.info(f'using {registry_cfg.name()}')
+
+    client = grafeas_client(container_registry_cfg=registry_cfg)
+
+    # XXX / HACK: assuming we always handle GCRs (we should rather check!), the first URL path
+    # element is the GCR project name
+    project_name = urllib.parse.urlparse(image_reference).path.split('/')[1]
+    try:
+        hash_reference = container.registry.to_hash_reference(image_reference)
+    except Exception as e:
+        raise VulnerabilitiesRetrievalFailed(e)
+
+    logger.info(f'retrieving vulnerabilites for {project_name} / {hash_reference}')
+
+    filter_str = f'resourceUrl = "https://{hash_reference}" AND kind="VULNERABILITY"'
+
+    try:
+        for r in client.list_occurrences(f'projects/{project_name}', filter_=filter_str):
+            # r has type grafeas.grafeas_v1.types.Occurrence
+            vuln = r.vulnerability # grafeas.grafeas_v1.types.VulnerabilityOccurrence
+            if not hasattr(vuln, 'cvss_score'):
+                continue
+            if vuln.cvss_score >= cvss_threshold:
+                yield r
+    except Exception as e:
+        raise VulnerabilitiesRetrievalFailed(e)
