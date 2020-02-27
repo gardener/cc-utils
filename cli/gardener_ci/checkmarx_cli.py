87f37cb889d680bb2b219057ec89a6090eb3dbe1
Andreas Burger <andreas.burger@sap.com>
2020-02-27 13:59:12 +0000

    fix linting issue

--- cli/gardener_ci/checkmarx_cli.py
+++ cli/gardener_ci/checkmarx_cli.py
@@ -4,8 +4,14 @@ import product.model
 import ci.util
 
 
-def upload_and_scan_from_component_descriptor(checkmarx_cfg_name: str, team_id: str, component_descriptor: str):
-    component_descriptor = product.model.ComponentDescriptor.from_dict(ci.util.parse_yaml_file(component_descriptor))
+def upload_and_scan_from_component_descriptor(
+    checkmarx_cfg_name: str,
+    team_id: str,
+    component_descriptor: str
+):
+    component_descriptor = product.model.ComponentDescriptor.from_dict(
+        ci.util.parse_yaml_file(component_descriptor)
+    )
 
     for component in component_descriptor.components():
         res = checkmarx.facade.upload_and_scan_repo(checkmarx_cfg_name, team_id, component)
