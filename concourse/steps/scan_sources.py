146e765c5df01bd5287931cb20dcbef869466fb2
Johannes Krayl <johannes.krayl@sap.com>
2020-03-05 16:58:52 +0100

    Add concurrency to scan sources trait

--- concourse/steps/scan_sources.py
+++ concourse/steps/scan_sources.py
@@ -1,4 +1,5 @@
-import dataclasses
+import concurrent.futures
+
 
 import ci.util
 import checkmarx.project
@@ -9,14 +10,22 @@ import product.model
 def scan_sources(
         checkmarx_cfg_name: str,
         team_id: str,
-        component_descriptor: str
+        component_descriptor: str,
+        max_workers: int = 8,
 ):
     component_descriptor = product.model.ComponentDescriptor.from_dict(
         ci.util.parse_yaml_file(component_descriptor)
     )
 
+    client = checkmarx.util.create_checkmarx_client(checkmarx_cfg_name)
+
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
+
+    scan_job_args = []
     for component in component_descriptor.components():
-        client = checkmarx.util.create_checkmarx_client(checkmarx_cfg_name)
-        scan_result = checkmarx.project.upload_and_scan_repo(client, team_id, component)
-        print(dataclasses.asdict(scan_result))
+        scan_job_args.append((client, team_id, component))
+
+    scan_results = executor.map(checkmarx.project.upload_and_scan_repo, scan_job_args)
+
+    for scan_result in scan_results:
         checkmarx.util.print_scan_result(scan_result=scan_result)
