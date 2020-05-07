5c4480b70f2e610340a5620b7d2f238a8e708a5e
Christian Cwienk (d051236) <christian.cwienk@sap.com>
2020-05-07 13:42:17 +0200

    make region-name configurable

--- ccc/aws.py
+++ ccc/aws.py
@@ -6,15 +6,17 @@ import ci.util
 import model.aws
 
 
-def session(aws_cfg: typing.Union[str, model.aws.AwsProfile]):
+def session(aws_cfg: typing.Union[str, model.aws.AwsProfile], region_name=None):
     if isinstance(aws_cfg, str):
         cfg_factory = ci.util.ctx().cfg_factory()
         aws_cfg = cfg_factory.aws(aws_cfg)
 
+    region_name = region_name or aws_cfg.region()
+
     session = boto3.Session(
         aws_access_key_id=aws_cfg.access_key_id(),
         aws_secret_access_key=aws_cfg.secret_access_key(),
-        region_name=aws_cfg.region(),
+        region_name=region_name,
     )
 
     return session
