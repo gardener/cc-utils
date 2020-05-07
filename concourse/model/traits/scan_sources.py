aed04527b0176b25ce6dc45f77ab872ade0409f4
Christian Cwienk <christian.cwienk@sap.com>
2020-05-07 09:31:18 +0200

    increase timeout for cx (tends to take looooong)

--- concourse/model/traits/scan_sources.py
+++ concourse/model/traits/scan_sources.py
@@ -122,7 +122,7 @@ class SourceScanTraitTransformer(TraitTransformer):
             name=concourse.model.traits.component_descriptor.DIR_NAME,
             variable_name=concourse.model.traits.component_descriptor.ENV_VAR_NAME,
         )
-        self.source_scan_step.set_timeout(duration_string='12h')
+        self.source_scan_step.set_timeout(duration_string='18h')
         yield self.source_scan_step
 
     def process_pipeline_args(self, pipeline_args: JobVariant):
