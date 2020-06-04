ab2ee2b8598b3127eeec9cfab18b526c2d7f598c
Christian Cwienk <christian.cwienk@sap.com>
2020-06-04 13:16:46 +0200

    convenience factory function for gstorage-client

--- ccc/gcp.py
+++ ccc/gcp.py
@@ -2,14 +2,20 @@ import functools
 
 import google.oauth2.service_account as service_account
 import googleapiclient.discovery
+import google.cloud.storage
 
 import ci.util
 
 
-def credentials(gcp_cfg: str):
+def _to_gcp_cfg(gcp_cfg: str):
     if isinstance(gcp_cfg, str):
         cfg_factory = ci.util.ctx().cfg_factory()
         gcp_cfg = cfg_factory.gcp(gcp_cfg)
+    return gcp_cfg
+
+
+def credentials(gcp_cfg: str):
+    gcp_cfg = _to_gcp_cfg(gcp_cfg=gcp_cfg)
 
     credentials = service_account.Credentials.from_service_account_info(
         gcp_cfg.service_account_key(),
@@ -22,3 +28,15 @@ def authenticated_build_func(gcp_cfg: str):
     creds = credentials(gcp_cfg=gcp_cfg)
 
     return functools.partial(googleapiclient.discovery.build, credentials=creds)
+
+
+def cloud_storage_client(gcp_cfg: str, *args, **kwargs):
+    gcp_cfg = _to_gcp_cfg(gcp_cfg=gcp_cfg)
+    creds = credentials(gcp_cfg=gcp_cfg)
+
+    return google.cloud.storage.Client(
+        project=gcp_cfg.project(),
+        credentials=creds,
+        *args,
+        **kwargs,
+    )
