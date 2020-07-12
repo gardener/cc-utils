4975210e70819d9d6ecc25c821c37d980ad49d43
Ashwani Kumar <ash.kumar@sap.com>
2020-07-12 21:14:56 +0530

    Enhanced slack model class with oncall app configs

--- model/slack.py
+++ model/slack.py
@@ -22,5 +22,11 @@ class SlackConfig(NamedModelElement):
     def api_token(self):
         return self.raw.get('api_token')
 
+    def oncall_app_signing_secret(self):
+        return self.raw.get('oncall_app_signing_secret')
+
+    def oncall_app_bot_token(self):
+        return self.raw.get('oncall_app_bot_token')
+
     def _required_attributes(self):
         return ['api_token']
