e1d104981ea496f10aa0460b258ae71704323daa
Christian Cwienk <christian.cwienk@sap.com>
2020-08-19 11:58:25 +0200

    ctx-repository-cfg

---
+++ model/ctx_repository.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2019-2020 SAP SE or an SAP affiliate company. All rights reserved. This file is
+# licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from model.base import (
+    NamedModelElement,
+)
+
+
+class CtxRepositoryCfg(NamedModelElement):
+    '''
+    a ctx-repository cfg, identifying a component-descriptor (v2) repository, defining a
+    repository context.
+    '''
+    def base_url(self):
+        return self.raw.get('base_url')
+
+    def description(self):
+        return self.raw.get('description', '<no description available>')
+
+    def _required_attributes(self):
+        return 'base_url',
