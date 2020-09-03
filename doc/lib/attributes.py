0ba669afd75ec136b4fe7d61209438ce5ba8fac0
Andreas Burger <andreas.burger@sap.com>
2020-09-03 11:17:04 +0000

    rework handling of tables in trait-doc
    - factor out old inlined function and return generated table
      values to increase readability
    - rework fill_table a bit
      + move setting of table-headers and values closer together
      + handle setting of table values explicitly, instead of delegating to
        now factored-out function
    - adjust to changes in enum types:
      + old way to document enums is gone, adjust to new
      + remove handling of enums without doc, they are now handled
        elsewhere (see previous commit)

--- doc/lib/attributes.py
+++ doc/lib/attributes.py
@@ -59,54 +59,69 @@ class AttributesDocumentation(object):
     def children(self):
         return self._child_elements
 
-    def fill_table(self, table_builder):
-        if issubclass(self._model_element_type, enum.Enum):
-            table_builder.add_table_header(['value', 'explanation'])
+    def _attr_spec_to_table_row(self, attr_spec, prefix=None):
+        name = attr_spec.name()
+        required = 'yes' if attr_spec.is_required() else 'no'
+
+        default_value = attr_spec.default_value()
+        if callable(default_value):
+            default_value = default_value.__name__
         else:
-            table_builder.add_table_header(['name', 'required?', 'default', 'type', 'explanation'])
+            default_value = str(default_value)
 
-        def attr_to_table_row(attr_spec, prefix=None):
-            name = attr_spec.name()
-            required = 'yes' if attr_spec.is_required() else 'no'
+        doc = textwrap.dedent(attr_spec.doc())
 
-            default_value = attr_spec.default_value()
-            if callable(default_value):
-                default_value = default_value.__name__
-            else:
-                default_value = str(default_value)
+        type_ = attr_spec.type()
+        if isinstance(type_, typing._GenericAlias):
+            if type_.__origin__ is dict:
+                # assumption: type is typing.Dict[T1, T2]
+                key_type, val_type = type_.__args__
+                self.add_child(
+                    model_element_type=val_type,
+                    element_name=f'{name}.<user-chosen>'
+                )
+                type_str = type_._name + f'[{str(key_type)}, {str(val_type)}]'
+            elif type_.__origin__ in (list, set):
+                type_str = type_._name + f'[{str(type_.__args__[0])}]'
+                # Also check type to support list of enum values
+                if (
+                    issubclass(type_.__args__[0], base_model.AttribSpecMixin)
+                    or issubclass(type_.__args__[0], enum.Enum)
+                ):
+                    self.add_child(model_element_type=type_.__args__[0], element_name=name)
 
-            doc = textwrap.dedent(attr_spec.doc())
-
-            type_ = attr_spec.type()
-            if isinstance(type_, typing._GenericAlias):
-                if type_.__origin__ is dict:
-                    # assumption: type is typing.Dict[T1, T2]
-                    key_type, val_type = type_.__args__
-                    self.add_child(
-                        model_element_type=val_type,
-                        element_name=f'{name}.<user-chosen>'
-                    )
-                    type_str = type_._name + f'[{str(key_type)}, {str(val_type)}]'
-                elif type_.__origin__ in (list, set):
-                    type_str = type_._name + f'[{str(type_.__args__[0])}]'
-                    # Also check type to support list of enum values
-                    if issubclass(type_.__args__[0], base_model.AttribSpecMixin):
-                        self.add_child(model_element_type=type_.__args__[0], element_name=name)
-                else:
-                    type_str = type_.__name__
-            elif issubclass(type_, base_model.AttribSpecMixin):
-                # recurse to child element
-                self.add_child(model_element_type=type_, element_name=name)
-                type_str = type_.__name__
             else:
                 type_str = type_.__name__
+        elif (
+            issubclass(type_, base_model.AttribSpecMixin)
+            or issubclass(type_, enum.Enum)
+        ):
+            # recurse to child element
+            self.add_child(model_element_type=type_, element_name=name)
+            type_str = type_.__name__
+        else:
+            type_str = type_.__name__
 
-            if issubclass(self._model_element_type, enum.Enum):
-                table_builder.add_table_row((name, doc))
-            else:
-                table_builder.add_table_row((name, required, default_value, type_str, doc))
+        if issubclass(self._model_element_type, enum.Enum):
+            return (name, doc)
+        else:
+            return (name, required, default_value, type_str, doc)
 
-        for attr_spec in self._model_element_type._attribute_specs():
-            attr_to_table_row(attr_spec)
+    def fill_table(self, table_builder):
+
+        if issubclass(self._model_element_type, base_model.EnumWithDocumentation):
+            table_builder.add_table_header(['value', 'explanation'])
+            for e in self._model_element_type:
+                table_builder.add_table_row((e.value, e.__doc__))
+
+        elif issubclass(self._model_element_type, base_model.AttribSpecMixin):
+
+            table_builder.add_table_header(
+                ['name', 'required?', 'default', 'type', 'explanation']
+            )
+            for attr_spec in self._model_element_type._attribute_specs():
+                table_builder.add_table_row(self._attr_spec_to_table_row(attr_spec))
+        else:
+            raise NotImplementedError
 
         return table_builder
