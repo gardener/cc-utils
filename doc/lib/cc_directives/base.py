65373ca27c66deb41cfa5250c63a9119d5e7fea4
Andreas Burger <andreas.burger@sap.com>
2020-09-03 11:12:09 +0000

    simplify rendering of enums without documentation
    render a simple bullet list of values instead of a table

--- doc/lib/cc_directives/base.py
+++ doc/lib/cc_directives/base.py
@@ -17,6 +17,8 @@ import sphinxutil
 import attributes
 import enum
 
+from concourse.model.base import EnumWithDocumentation
+
 
 class AttributesDocMixin(sphinxutil.SphinxUtilsMixin):
     def attributes(self, model_element_type):
@@ -35,9 +37,15 @@ class AttributesDocMixin(sphinxutil.SphinxUtilsMixin):
 
             self.add_subtitle(subtitle)
 
-            table_builder = self.create_table_builder()
-            attributes_doc.fill_table(table_builder)
-            self.add_table(table_builder)
+            if (
+                issubclass(attributes_doc._model_element_type, enum.Enum)
+                and not issubclass(attributes_doc._model_element_type, EnumWithDocumentation)
+            ):
+                self.add_bullet_list([f'``{e.value}``' for e in attributes_doc._model_element_type])
+            else:
+                table_builder = self.create_table_builder()
+                attributes_doc.fill_table(table_builder)
+                self.add_table(table_builder)
 
             for child_element in attributes_doc.children():
                 render_element_attribs(child_element._prefix, child_element)
