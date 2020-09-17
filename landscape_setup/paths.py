b97db2cd048009af3ccc489592ff85366c9cfaa0
zkdev <dev@zeekay.dev>
2020-09-17 10:54:19 +0200

    Add whitesource charts

---
+++ landscape_setup/paths.py
@@ -0,0 +1,6 @@
+import os
+
+
+owndir = os.path.abspath(os.path.dirname(__file__))
+chartdirt = os.path.abspath(os.path.join(owndir, 'charts'))
+repodir = os.path.abspath(os.path.join(owndir, os.pardir))
