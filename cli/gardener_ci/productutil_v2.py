47b4fa66b05e6b7f86e5f2d46f25a66461f36cdc
Christian Cwienk <christian.cwienk@sap.com>
2020-10-01 22:01:20 +0200

    drop-in-replacement cmd for add-dependencies

---
+++ cli/gardener_ci/productutil_v2.py
@@ -0,0 +1,109 @@
+'''
+temporary (hopefully short..) migration util as a backwards-compatible replacement for
+productutil.py -> add_dependencies
+
+different to the aforementioned cmd, a component-descriptor-v2 is expected as input, and
+created as output
+'''
+
+import dataclasses
+import sys
+import yaml
+
+import gci.componentmodel as cm
+
+import ci.util
+import product.model as pm
+
+CliHint = ci.util.CliHint
+
+parse = yaml.safe_load
+
+
+def _raw_component_dep_to_v2(raw: dict):
+  name = raw['name']
+
+  return cm.ComponentReference(
+    componentName=name,
+    name=name,
+    version=raw['version'],
+  )
+
+
+def _raw_image_dep_to_v2(raw: dict):
+  name = raw['name']
+  version = raw['version']
+  img_ref = raw['image_reference']
+
+  return cm.Resource(
+    name=name,
+    version=version,
+    type=cm.ResourceType.OCI_IMAGE,
+    access=cm.OciAccess(
+      type=cm.AccessType.OCI_REGISTRY,
+      imageReference=img_ref,
+    )
+  )
+
+
+def _raw_generic_dep_to_v2(raw: dict):
+  name = raw['name']
+  version = raw['version']
+
+  return cm.Resource(
+    name=name,
+    version=version,
+    type=cm.ResourceType.GENERIC,
+    access=None,
+  )
+
+
+def add_dependencies(
+  descriptor_src_file: str,
+  component_name: str,
+  component_version: str,
+  descriptor_out_file: str=None,
+  component_dependencies: [str]=[],
+  container_image_dependencies: [str]=[],
+  generic_dependencies: [str]=[],
+):
+  component_descriptor = cm.ComponentDescriptor.from_dict(
+    ci.util.parse_yaml_file(descriptor_src_file)
+  )
+  component = component_descriptor.component
+
+  # perform sanity checks (component_name and version *must* match)
+  if not component.name == component_name:
+    raise ValueError(f'{component_name=} != {component.name=}')
+  if not component.version == component_version:
+    raise ValueError(f'{component_version=} != {component.version=}')
+
+  component.componentReferences += [
+    _raw_component_dep_to_v2(parse(cdep)) for cdep in component_dependencies
+  ]
+  for img_dep in container_image_dependencies:
+    img_dep = parse(img_dep)
+    img_res = _raw_image_dep_to_v2(img_dep)
+    if (rel := img_dep.get('relation')) and pm.Relation(rel) is pm.Relation.LOCAL:
+      component.localResources.append(img_res)
+    else:
+      component.externalResources.append(img_res)
+
+  for gen_dep in generic_dependencies:
+    gen_dep = parse(gen_dep)
+    gen_res = _raw_generic_dep_to_v2(gen_dep)
+    if (rel := gen_dep.get('relation')) and pm.Relation(rel) is pm.Relation.LOCAL:
+      component.localResources.append(gen_res)
+    else:
+      component.externalResources.append(gen_res)
+
+  if descriptor_out_file:
+    outfh = open(descriptor_out_file, 'w')
+  else:
+    outfh = sys.stdout
+
+  yaml.dump(
+    data=dataclasses.asdict(component_descriptor),
+    Dumper=cm.EnumValueYamlDumper,
+    stream=outfh,
+  )
