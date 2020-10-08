1c47e0180ebec95e34c67a9e8e657bb14a0475e4
Andreas Burger <andreas.burger@sap.com>
2020-10-08 13:05:16 +0000

    update unit-test

--- test/concourse/steps/scan_container_images_test.py
+++ test/concourse/steps/scan_container_images_test.py
@@ -13,58 +13,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import dacite
 import pytest
 
 import concourse.steps.images as images
-import product.model
+import gci.componentmodel
 
 examinee = images.image_reference_filter
 
 
 @pytest.fixture
-def img_ref():
-    def _img_ref(image_reference, name='abc', version='1.2.3'):
-        return product.model.ContainerImage.create(
-                name=name,
-                version=version,
-                image_reference=image_reference,
+def resource():
+    def _resource(
+        image_reference,
+        name='abc',
+        version='1.2.3',
+    ):
+        return dacite.from_dict(
+            data_class=gci.componentmodel.Resource,
+            data={
+                'name': name,
+                'version': version,
+                'type': 'ociImage',
+                'access': {
+                    'type': 'ociRegistry',
+                    'imageReference': image_reference,
+                },
+            },
+            config=dacite.Config(
+                cast=[
+                    gci.componentmodel.ResourceType,
+                    gci.componentmodel.AccessType,
+                ],
+            ),
         )
-    return _img_ref
+    return _resource
 
 
-def test_image_reference_filter(img_ref):
-    ref1 = img_ref('image1:bar')
-    ref2 = img_ref('image2/foo:bar')
-    ref3 = img_ref('another_image1:bar')
-    ref4 = img_ref('unrelated/for/testing:bar')
+def test_image_reference_filter(resource):
+    res1 = resource('image1:bar')
+    res2 = resource('image2/foo:bar')
+    res3 = resource('another_image1:bar')
+    res4 = resource('unrelated/for/testing:bar')
 
     default_filter = examinee()
 
-    assert default_filter(ref1) # by default, nothing should be filtered out
-    assert default_filter(ref2)
-    assert default_filter(ref3)
-    assert default_filter(ref4)
+    assert default_filter(res1) # by default, nothing should be filtered out
+    assert default_filter(res2)
+    assert default_filter(res3)
+    assert default_filter(res4)
 
     # include only images starting with 'image'
     include_filter = examinee(include_regexes=('image.*',))
 
-    assert include_filter(ref1)
-    assert include_filter(ref2)
-    assert not include_filter(ref3)
-    assert not include_filter(ref4)
+    assert include_filter(res1)
+    assert include_filter(res2)
+    assert not include_filter(res3)
+    assert not include_filter(res4)
 
     # exclude images containing the string 'image'
     exclude_filter = examinee(exclude_regexes=('.*image.*',))
 
-    assert not exclude_filter(ref1)
-    assert not exclude_filter(ref2)
-    assert not exclude_filter(ref3)
-    assert exclude_filter(ref4)
+    assert not exclude_filter(res1)
+    assert not exclude_filter(res2)
+    assert not exclude_filter(res3)
+    assert exclude_filter(res4)
 
     # exclude should have precedency over include
     exclude_with_predence = examinee(include_regexes=('.*image.*',), exclude_regexes=('.*2.*',))
 
-    assert exclude_with_predence(ref1)
-    assert not exclude_with_predence(ref2)
-    assert exclude_with_predence(ref3)
-    assert not exclude_with_predence(ref4)
+    assert exclude_with_predence(res1)
+    assert not exclude_with_predence(res2)
+    assert exclude_with_predence(res3)
+    assert not exclude_with_predence(res4)
