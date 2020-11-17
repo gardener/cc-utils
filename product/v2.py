41e8b0cce50ab6015c88028b8a9b3dfe79d2c303
Christian Cwienk <christian.cwienk@sap.com>
2020-11-17 10:31:40 +0100

    guard against None-access even more

--- product/v2.py
+++ product/v2.py
@@ -675,7 +675,7 @@ def resources(
     for resource in (r for r in resources if resource_types is None or r.type in resource_types):
         if resource.access is None and None in resource_access_types:
             yield resource
-        elif resource.access.type in resource_access_types:
+        elif resource.access and resource.access.type in resource_access_types:
             yield resource
         else:
             if resource_policy is ResourcePolicy.IGNORE_NONMATCHING_ACCESS_TYPES:
