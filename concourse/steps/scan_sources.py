f6c0b205e44c4588df6b3c785783f489f1c6565f
Johannes Krayl <johannes.krayl@sap.com>
2020-11-30 14:57:59 +0100

    nits and minor improvements

--- concourse/steps/scan_sources.py
+++ concourse/steps/scan_sources.py
@@ -1,21 +1,13 @@
-from datetime import datetime
-import tempfile
 import typing
 
-import ccc.github
 import ci.util
 import checkmarx.util
-import mail.model
 import product.model
 import product.util
-import reutil
-import sdo.model
-import sdo.util
 import whitesource.client
 import whitesource.component
 import whitesource.util
 import product.v2
-
 
 import gci.componentmodel as cm
 
@@ -75,19 +67,17 @@ def scan_component_with_whitesource(
     component_descriptor = cm.ComponentDescriptor.from_dict(
         ci.util.parse_yaml_file(component_descriptor_path)
     )
+    components = product.v2.components(component_descriptor_v2=component_descriptor)
 
     product_name = component_descriptor.component.name
 
-    components = product.v2.components(component_descriptor_v2=component_descriptor)
-
     # get scan artifacts with configured label
     scan_artifacts_gen = whitesource.component._get_scan_artifacts_from_components(components)
-
     scan_artifacts = tuple(scan_artifacts_gen)
     ci.util.info(f'will scan {len(scan_artifacts)} artifact')
 
     for scan_artifact in scan_artifacts:
-        scan_artifact_with_ws(
+        whitesource.util.scan_artifact_with_ws(
             extra_whitesource_config=extra_whitesource_config,
             product_token=product_token,
             requester_mail=requester_mail,
@@ -95,127 +85,10 @@ def scan_component_with_whitesource(
             ws_client=ws_client,
         )
 
-    notify_users(
+    whitesource.util.notify_users(
         notification_recipients=notification_recipients,
         cve_threshold=cve_threshold,
         ws_client=ws_client,
         product_name=product_name,
         product_token=product_token,
     )
-
-
-def scan_artifact_with_ws(
-    extra_whitesource_config: typing.Dict,
-    product_token: str,
-    requester_mail: str,
-    scan_artifact: sdo.model.ScanArtifact,
-    ws_client: whitesource.client.WhitesourceClient,
-):
-    clogger = sdo.util.component_logger(scan_artifact.name)
-
-    clogger.info('init scan')
-    github_api = ccc.github.github_api_from_gh_access(access=scan_artifact.access)
-    github_repo = github_api.repository(
-        owner=scan_artifact.access.org_name(),
-        repository=scan_artifact.access.repository_name(),
-    )
-
-    clogger.info('guessing commit hash')
-    # guess git-ref for the given component's version
-    commit_hash = product.util.guess_commit_from_source(
-        artifact_name=scan_artifact.name,
-        commit_hash=scan_artifact.access.commit,
-        ref=scan_artifact.access.ref,
-        github_repo=github_repo,
-    )
-
-    path_filter_func = reutil.re_filter(
-        exclude_regexes=scan_artifact.label.path_config.exclude_paths,
-        include_regexes=scan_artifact.label.path_config.include_paths,
-    )
-
-    # store in tmp file
-    with tempfile.TemporaryFile() as tmp_file:
-        # download whitesource component
-        clogger.info('downloading component for scan')
-        component_filename = whitesource.component.download_component(
-            clogger=clogger,
-            github_repo=github_repo,
-            path_filter_func=path_filter_func,
-            ref=commit_hash,
-            target=tmp_file,
-        )
-        # needed
-        tmp_file.seek(0)
-
-        # POST component>
-        clogger.info('POST project')
-        ws_client.post_project(
-            extra_whitesource_config=extra_whitesource_config,
-            file=tmp_file,
-            filename=component_filename,
-            product_token=product_token,
-            project_name=scan_artifact.name,
-            requester_email=requester_mail,
-        )
-        # TODO save scanned commit hash or tag in project tag show scanned version
-        # version for agent will create a new project
-        # https://whitesource.atlassian.net/wiki/spaces/WD/pages/34046170/HTTP+API+v1.1#HTTPAPIv1.1-ProjectTags
-
-
-def notify_users(
-    ws_client: whitesource.client.WhitesourceClient,
-    product_token: str,
-    cve_threshold: float,
-    notification_recipients: typing.List[str],
-    product_name: str
-):
-    # get all projects
-    ci.util.info('retrieving all projects')
-    projects = ws_client.get_all_projects_of_product(product_token=product_token)
-
-    # generate reporting table for console
-    tables = whitesource.util.generate_reporting_tables(
-        projects=projects,
-        threshold=cve_threshold,
-        tablefmt='simple',
-    )
-
-    whitesource.util.print_cve_tables(tables=tables)
-
-    if len(notification_recipients) > 0:
-        # generate reporting table for notification
-        tables = whitesource.util.generate_reporting_tables(
-            projects=projects,
-            threshold=cve_threshold,
-            tablefmt='html',
-        )
-
-        # get product risk report
-        ci.util.info('retrieving product risk report')
-        prr = ws_client.get_product_risk_report(product_token=product_token)
-
-        # assemble html body
-        body = whitesource.util.assemble_mail_body(
-            tables=tables,
-            threshold=cve_threshold,
-        )
-
-        # send mail
-        ci.util.info('sending notification')
-        now = datetime.now()
-        fname = f'{product_name}-{now.year}.{now.month}-{now.day}-product-risk-report.pdf'
-
-        attachment = mail.model.Attachment(
-            mimetype_main='application',
-            mimetype_sub='pdf',
-            bytes=prr.content,
-            filename=fname,
-        )
-
-        whitesource.util.send_mail(
-            body=body,
-            recipients=notification_recipients,
-            product_name=product_name,
-            attachments=[attachment],
-        )
--- whitesource/util.py
+++ whitesource/util.py
@@ -1,12 +1,20 @@
+import datetime
 import functools
-
-import tabulate
+import tempfile
 import typing
 
+import tabulate
+
+import ccc.github
 import ci.util
 import mailutil
 import mail.model
+import product.util
+import reutil
+import sdo.util
+import sdo.model
 import whitesource.client
+import whitesource.component
 import whitesource.model
 
 
@@ -155,3 +163,113 @@ def print_cve_tables(tables):
     print()
     print('\n\n'.join(tables))
     print()
+
+
+def notify_users(
+    ws_client: whitesource.client.WhitesourceClient,
+    product_token: str,
+    cve_threshold: float,
+    notification_recipients: typing.List[str],
+    product_name: str
+):
+    ci.util.info('retrieving all projects')
+    projects = ws_client.get_all_projects_of_product(product_token=product_token)
+
+    # generate simple reporting table for console output
+    tables = generate_reporting_tables(
+        projects=projects,
+        threshold=cve_threshold,
+        tablefmt='simple',
+    )
+
+    print_cve_tables(tables=tables)
+
+    if len(notification_recipients) > 0:
+        # generate html reporting table for email notifications
+        tables = generate_reporting_tables(
+            projects=projects,
+            threshold=cve_threshold,
+            tablefmt='html',
+        )
+
+        ci.util.info('retrieving product risk report')
+        prr = ws_client.get_product_risk_report(product_token=product_token)
+
+        body = assemble_mail_body(
+            tables=tables,
+            threshold=cve_threshold,
+        )
+
+        ci.util.info('sending notification')
+        attachment_file_name = datetime.datetime.now().strftime('%Y-%m-%d-product-risk-report.pdf')
+        print(attachment_file_name)
+        attachment = mail.model.Attachment(
+            mimetype_main='application',
+            mimetype_sub='pdf',
+            bytes=prr.content,
+            filename=attachment_file_name,
+        )
+
+        send_mail(
+            body=body,
+            recipients=notification_recipients,
+            product_name=product_name,
+            attachments=[attachment],
+        )
+
+
+def scan_artifact_with_ws(
+    extra_whitesource_config: typing.Dict,
+    product_token: str,
+    requester_mail: str,
+    scan_artifact: sdo.model.ScanArtifact,
+    ws_client: whitesource.client.WhitesourceClient,
+):
+    clogger = sdo.util.component_logger(scan_artifact.name)
+
+    clogger.info('init scan')
+    github_api = ccc.github.github_api_from_gh_access(access=scan_artifact.access)
+    github_repo = github_api.repository(
+        owner=scan_artifact.access.org_name(),
+        repository=scan_artifact.access.repository_name(),
+    )
+
+    clogger.info('guessing commit hash')
+    # guess git-ref for the given version
+    commit_hash = product.util.guess_commit_from_source(
+        artifact_name=scan_artifact.name,
+        commit_hash=scan_artifact.access.commit,
+        ref=scan_artifact.access.ref,
+        github_repo=github_repo,
+    )
+
+    path_filter_func = reutil.re_filter(
+        exclude_regexes=scan_artifact.label.path_config.exclude_paths,
+        include_regexes=scan_artifact.label.path_config.include_paths,
+    )
+
+    with tempfile.TemporaryFile() as tmp_file:
+        clogger.info('downloading component for scan')
+        component_filename = whitesource.component.download_component(
+            clogger=clogger,
+            github_repo=github_repo,
+            path_filter_func=path_filter_func,
+            ref=commit_hash,
+            target=tmp_file,
+        )
+        # don't change the following line, lest things no longer work
+        # sets the file position at the offset 0 == start of the file
+        tmp_file.seek(0)
+
+        clogger.info('POST project')
+        ws_client.post_project(
+            extra_whitesource_config=extra_whitesource_config,
+            file=tmp_file,
+            filename=component_filename,
+            product_token=product_token,
+            project_name=scan_artifact.name,
+            requester_email=requester_mail,
+        )
+        # TODO save scanned commit hash or tag in project tag show scanned version
+        # version for agent will create a new project
+        # https://whitesource.atlassian.net/wiki/spaces/WD/pages/34046170/HTTP+API+v1.1#HTTPAPIv1.1-ProjectTags
