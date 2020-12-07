c9c0493235223c819e1548e409deefc37a3ac7b7
Christian Cwienk <christian.cwienk@sap.com>
2020-12-07 21:56:43 +0100

    declare dependency towards gardener-oci

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ own_dir = os.path.abspath(os.path.dirname(__file__))
 
 def requirements():
     yield 'gardener-cicd-base'
+    yield 'gardener-oci'
 
     with open(os.path.join(own_dir, 'requirements.txt')) as f:
         for line in f.readlines():
