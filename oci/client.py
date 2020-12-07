ea215ffa7be01f1397891b999a246ee6cb3efe75
Christian Cwienk <christian.cwienk@sap.com>
2020-12-07 13:59:12 +0100

    custom OCI-distribution-API-client
    Implement a custom client adhering almost to the OCI Distribution Spec
    [0], featuring the lowlevel operations required for verbatim OCI
    Artifact replication.
    
    During testing, there were some deviations found, where GCR will not
    behave according to the spec (see inline comments).
    
    Still tbd:
    
    - replace containerregistry-cccwienk-based parts of cc-utils with new
      client
    - test against other OCI-registries (e.g. registry-1.docker.io,
      artifactory..)
    - parallelise
    - detect known blobs, and omit unnecessary (re-)uploads
    
    [0]
    https://github.com/opencontainers/distribution-spec/blob/master/spec.md

--- oci/client.py
+++ oci/client.py
@@ -1,10 +1,15 @@
-import dacite
 import dataclasses
 import datetime
+import hashlib
+import logging
+import typing
+
+import dacite
 import dateutil.parser
 import requests
 import requests.auth
 import urllib.parse
+import urllib
 import www_authenticate
 
 import oci.auth as oa
@@ -12,6 +17,8 @@ import oci.model as om
 import oci.util
 
 urljoin = oci.util.urljoin
+
+logger = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass(frozen=True)
@@ -23,7 +30,7 @@ class OauthToken:
 
     def valid(self):
         issued_at = dateutil.parser.isoparse(self.issued_at)
-        # pessimistically decuct 30s, to be on the safe side
+        # pessimistically deduct 30s, to be on the safe side
         expiry_date = issued_at + datetime.timedelta(seconds=self.expires_in - 30)
 
         now = datetime.datetime.now(tz=datetime.timezone.utc)
@@ -81,10 +88,30 @@ def artifact_base_url(image_reference: str) -> str:
     )
 
 
-def blob_url(image_reference: str, digest: str):
+def blobs_url(image_reference: str) -> str:
     return urljoin(
         artifact_base_url(image_reference),
         'blobs',
+    )
+
+
+def uploads_url(image_reference: str) -> str:
+    return urljoin(
+        blobs_url(image_reference),
+        'uploads',
+    ) + '/'
+
+
+def put_blob_url(image_reference: str, digest: str) -> str:
+    query = urllib.parse.urlencode({
+        'digest': digest,
+    })
+    return uploads_url(image_reference=image_reference) + '?' + query
+
+
+def blob_url(image_reference: str, digest: str):
+    return urljoin(
+        blobs_url(image_reference=image_reference),
         digest
     )
 
@@ -116,6 +143,7 @@ class Client:
     def __init__(self, credentials_lookup: callable):
         self.credentials_lookup = credentials_lookup
         self.token_cache = OauthTokenCache()
+        self.session = requests.Session()
 
     def _authenticate(
         self,
@@ -139,7 +167,7 @@ class Client:
         )
 
         url = base_api_url(image_reference=image_reference)
-        res = requests.get(url)
+        res = self.session.get(url)
 
         auth_challenge = www_authenticate.parse(res.headers['www-authenticate'])
         bearer = auth_challenge['bearer']
@@ -150,7 +178,7 @@ class Client:
             'service': service,
         })
 
-        res = requests.get(
+        res = self.session.get(
             realm,
             auth=requests.auth.HTTPBasicAuth(
               username=oci_creds.username,
@@ -168,19 +196,35 @@ class Client:
 
         self.token_cache.set_token(token)
 
-    def _request(self, url: str, image_reference: str, scope: str, method: str='GET'):
+    def _request(
+        self,
+        url: str,
+        image_reference: str,
+        scope: str,
+        method: str='GET',
+        headers: dict=None,
+        **kwargs,
+    ):
         self._authenticate(
             image_reference=image_reference,
             scope=scope,
         )
+        headers = headers or {}
 
-        return requests.request(
+        res = requests.request(
             method=method,
             url=url,
             headers={
-              'Authorization': f'Bearer {self.token_cache.token(scope=scope)}',
+              'Authorization': f'Bearer {self.token_cache.token(scope=scope).token}',
+              **headers,
             },
+            **kwargs,
         )
+        if not res.ok:
+            logger.warning(f'rq against {url=} failed {res.status_code=} {res.reason=}')
+
+        res.raise_for_status()
+        return res
 
     def manifest_raw(self, image_reference: str):
         scope = _scope(image_reference=image_reference, action='pull')
@@ -190,6 +234,7 @@ class Client:
             image_reference=image_reference,
             scope=scope,
         )
+        res.raise_for_status()
 
         return res
 
@@ -200,3 +245,177 @@ class Client:
             data_class=om.OciImageManifest,
             data=res.json(),
         )
+
+    def put_manifest(self, image_reference: str, manifest: bytes):
+        scope = _scope(image_reference=image_reference, action='push,pull')
+
+        res = self._request(
+            url=manifest_url(image_reference=image_reference),
+            image_reference=image_reference,
+            scope=scope,
+            method='PUT',
+            data=manifest,
+        )
+
+        res.raise_for_status()
+
+    def blob(
+        self,
+        image_reference: str,
+        digest: str,
+        stream=True,
+    ) -> requests.models.Response:
+        scope = _scope(image_reference=image_reference, action='pull')
+
+        res = self._request(
+            url=blob_url(image_reference=image_reference, digest=digest),
+            image_reference=image_reference,
+            scope=scope,
+            method='GET',
+            stream=stream,
+            timeout=None,
+        )
+
+        return res
+
+    def put_blob(
+        self,
+        image_reference: str,
+        digest: str,
+        octets_count: int,
+        data: requests.models.Response,
+        max_chunk=1024 * 1024 * 1, # 1 MiB
+    ):
+        if octets_count < max_chunk:
+            return self._put_blob_single_post(
+                image_reference=image_reference,
+                digest=digest,
+                octets_count=octets_count,
+                data=data.content,
+            )
+        else:
+            with data:
+              return self._put_blob_chunked(
+                  image_reference=image_reference,
+                  digest=digest,
+                  octets_count=octets_count,
+                  data_iterator=data.iter_content(chunk_size=max_chunk),
+                  chunk_size=max_chunk,
+              )
+
+    def _put_blob_chunked(
+        self,
+        image_reference: str,
+        digest: str,
+        octets_count: int,
+        data_iterator: typing.Iterator[bytes],
+        chunk_size: int=1024 * 1024 * 16, # 16 MiB
+    ):
+        scope = _scope(image_reference=image_reference, action='push,pull')
+        logger.debug(f'chunked-put {chunk_size=}')
+
+        # start uploading session
+        res = self._request(
+            url=uploads_url(image_reference=image_reference),
+            image_reference=image_reference,
+            scope=scope,
+            method='POST',
+            headers={
+                'content-length': '0',
+            }
+        )
+        res.raise_for_status()
+
+        upload_url = res.headers['location']
+
+        octets_left = octets_count
+        octets_sent = 0
+        offset = 0
+        sha256 = hashlib.sha256()
+
+        while octets_left > 0:
+            octets_to_send = min(octets_left, chunk_size)
+            octets_left -= octets_to_send
+
+            data = next(data_iterator)
+            sha256.update(data)
+
+            logger.debug(f'{octets_to_send=} {octets_left=} {len(data)=}')
+            logger.debug(f'{octets_sent + offset}-{octets_sent + octets_to_send + offset}')
+
+            res = self._request(
+                url=upload_url,
+                image_reference=image_reference,
+                scope=scope,
+                method='PATCH',
+                data=data,
+                headers={
+                 'content-length': str(octets_to_send),
+                 'content-type': 'application/octet-stream',
+                 'content-range': f'{octets_sent + offset}-{octets_sent + octets_to_send + offset}',
+                }
+            )
+            offset = 1
+            res.raise_for_status()
+
+            # XXX interestingly, "the spec" [0] recommends that clients use the `location` URL as
+            # indicated from each PATCH-response for each subsequent request. However, doing so
+            # will reproducibly lead to a BrokenPipe-error upon the second request.
+            # leaving the code in along with this warning to ensure this be not changed e.g.
+            # if discovering this implementation does not the spec.
+
+            # upload_url = res.headers['location'] # DO NOT use location header - see above
+
+            octets_sent += octets_to_send
+
+        sha256_digest = f'sha256:{sha256.hexdigest()}'
+
+        # XXX according to spec [0], the chunked upload MUST be finalised with a PUT, optionally
+        # containing the last chunk (or empty body otherwise). However, for some reason,
+        # GCR will always return a HTTP-400 if doing so. Also, according to some test-uploads,
+        # the uploads will actually succeed w/o the final HTTP-PUT.
+        # will still leave the (dead) HTTP-PUT for reference, but early-exit before issueing it.
+        # [0]
+        # https://github.com/opencontainers/distribution-spec/blob/master/spec.md
+
+        return sha256_digest
+
+        # close uploading session
+        query = urllib.parse.urlencode({'digest': sha256_digest})
+        upload_url = res.headers['location'] + '?' + query
+        res = self._request(
+            url=upload_url,
+            image_reference=image_reference,
+            scope=scope,
+            method='PUT',
+            headers={
+                 'Content-Length': '0',
+            },
+        )
+
+    def _put_blob_single_post(
+        self,
+        image_reference: str,
+        digest: str,
+        octets_count: int,
+        data: bytes,
+    ):
+        logger.debug(f'single-post {image_reference=} {octets_count=}')
+        scope = _scope(image_reference=image_reference, action='push,pull')
+
+        res = self._request(
+            url=put_blob_url(
+                image_reference=image_reference,
+                digest=digest,
+            ),
+            image_reference=image_reference,
+            scope=scope,
+            method='POST',
+            headers={
+                'content-type': 'application/octet-stream',
+                'content-length': str(octets_count),
+            },
+            data=data,
+        )
+
+        res.raise_for_status()
