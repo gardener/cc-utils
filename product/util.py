d8ae8b10d47136d75fc9e185c16802600ebaef55
Johannes Krayl <johannes.krayl@sap.com>
2021-01-08 12:32:49 +0100

    adept cd v2 in component diff alg

--- product/util.py
+++ product/util.py
@@ -32,6 +32,9 @@ from .model import (
     ContainerImage,
     ComponentDescriptor,
 )
+import product.v2
+
+import gci.componentmodel as cm
 
 
 class ComponentResolutionException(Exception):
@@ -154,15 +157,52 @@ def merge_products(left_product, right_product):
     return merged
 
 
-def diff_products(left_product, right_product, ignore_component_names=()):
+@dataclasses.dataclass
+class ComponentDiff:
+    cidentities_only_left: set = dataclasses.field(default_factory=set)
+    cidentities_only_right: set = dataclasses.field(default_factory=set)
+    cpairs_version_changed: list = dataclasses.field(default_factory=list)
+    # only set when new component is added/removed
+    names_only_left: set = dataclasses.field(default_factory=set)
+    names_only_right: set = dataclasses.field(default_factory=set)
+    # only set on update
+    names_version_changed: set = dataclasses.field(default_factory=set)
+
+
+# for v2 =  pass cd v2 and resolve components
+# make use of caching
+def diff_products(
+    left_product: cm.ComponentDescriptor,
+    right_product: cm.ComponentDescriptor,
+    ignore_component_names=(),
+    cache_dir=None,
+) -> ComponentDiff:
+    if type(left_product) is not cm.ComponentDescriptor:
+        raise NotImplementedError(
+            f'left product unsupported type {type(left_product)=}.'
+            ' Only ComponentDesciptorV2 is supported',
+        )
+    if type(right_product) is not cm.ComponentDescriptor:
+        raise NotImplementedError(
+            f'unsupported type {type(right_product)=}.'
+            ' Only ComponentDesciptorV2 is supported',
+        )
     # only take component references into account for now and assume
     # that component versions are always identical content-wise
-    left_components = {
-        c for c in left_product.components() if c.name() not in ignore_component_names
-    }
-    right_components = {
-        c for c in right_product.components() if c.name() not in ignore_component_names
-    }
+    left_components: typing.Generator[cm.Component] = product.v2.components(
+        component_descriptor_v2=left_product,
+        cache_dir=cache_dir,
+    )
+    right_components: typing.Generator[cm.Component] = product.v2.components(
+        component_descriptor_v2=right_product,
+        cache_dir=cache_dir,
+    )
+    left_components = tuple(
+        c for c in left_components if c.name not in ignore_component_names
+    )
+    right_components = tuple(
+        c for c in right_components if c.name not in ignore_component_names
+    )
 
     return diff_components(
         left_components=left_components,
@@ -171,50 +211,58 @@ def diff_products(left_product, right_product, ignore_component_names=()):
     )
 
 
-@dataclasses.dataclass
-class ComponentDiff:
-    crefs_only_left: set = dataclasses.field(default_factory=set)
-    crefs_only_right: set = dataclasses.field(default_factory=set)
-    crefpairs_version_changed: set = dataclasses.field(default_factory=set)
-    names_only_left: set = dataclasses.field(default_factory=set)
-    names_only_right: set = dataclasses.field(default_factory=set)
-    names_version_changed: set = dataclasses.field(default_factory=set)
+def diff_components(
+    left_components: typing.Tuple[cm.Component],
+    right_components: typing.Tuple[cm.Component],
+    ignore_component_names=(),
+) -> ComponentDiff:
+    left_identities = {
+        c.identity() for c in left_components if c.name not in ignore_component_names
+    }
+    right_identities = {
+        c.identity() for c in right_components if c.name not in ignore_component_names
+    }
 
+    left_only_identities = left_identities - right_identities
+    right_only_identities = right_identities - left_identities
 
-def diff_components(left_components, right_components, ignore_component_names=()) -> ComponentDiff:
-    left_components = set(left_components)
-    right_components = set(right_components)
-
-    if left_components == right_components:
+    if left_only_identities == right_only_identities:
         return None # no diff
 
-    components_only_left = left_components - right_components
-    components_only_right = right_components - left_components
+    left_components = tuple((
+        c for c in left_components if c.identity() in left_only_identities
+    ))
+    right_components = tuple((
+        c for c in right_components if c.identity() in right_only_identities
+    ))
 
-    def find_changed_component(changed_component, components):
+    def find_changed_component(
+        changed_component: cm.Component,
+        components: typing.List[cm.Component],
+    ):
         for c in components:
-            if c.name() == changed_component.name():
+            if c.name == changed_component.name:
                 return (changed_component, c)
         return (changed_component, None) # no pair component found
 
-    components_with_changed_versions = FluentIterable(items=components_only_left) \
+    components_with_changed_versions = FluentIterable(items=left_components) \
         .map(functools.partial(find_changed_component, components=right_components)) \
         .filter(lambda cs: cs[1] is not None) \
         .as_list()
-    # pairs of crefs (left-version:right-version)
+    # pairs of components (left:right-version)
 
-    left_names = set(map(lambda c: c.name(), components_only_left))
-    right_names = set(map(lambda c: c.name(), components_only_right))
-    names_version_changed = set(map(lambda cp: cp[0].name(), components_with_changed_versions))
+    left_names = {i.name for i in left_identities}
+    right_names = {i.name for i in right_identities}
+    names_version_changed = {c[0].name for c in components_with_changed_versions}
 
     both_names = left_names & right_names
     left_names -= both_names
     right_names -= both_names
 
     return ComponentDiff(
-        crefs_only_left=components_only_left,
-        crefs_only_right=components_only_right,
-        crefpairs_version_changed=set(components_with_changed_versions),
+        cidentities_only_left=left_only_identities,
+        cidentities_only_right=right_only_identities,
+        cpairs_version_changed=components_with_changed_versions,
         names_only_left=left_names,
         names_only_right=right_names,
         names_version_changed=names_version_changed,
@@ -222,113 +270,138 @@ def diff_components(left_components, right_components, ignore_component_names=()
 
 
 @dataclasses.dataclass
-class ImageDiff:
-    left_component: Component
-    right_component: Component
-    irefs_only_left: set = dataclasses.field(default_factory=set)
-    irefs_only_right: set = dataclasses.field(default_factory=set)
-    irefpairs_version_changed: set = dataclasses.field(default_factory=set)
-    names_only_left: set = dataclasses.field(default_factory=set)
-    names_only_right: set = dataclasses.field(default_factory=set)
-    names_version_changed: set = dataclasses.field(default_factory=set)
+class ResourceDiff:
+    left_component: cm.Component
+    right_component: cm.Component
+    resource_refs_only_left: typing.List[cm.Resource] = dataclasses.field(default_factory=list)
+    resource_refs_only_right: typing.List[cm.Resource] = dataclasses.field(default_factory=list)
+    resourcepairs_version_changed: typing.List[typing.Tuple[cm.Resource, cm.Resource]] = dataclasses.field(default_factory=list) # noqa:E501
 
 
-def diff_images(
-    left_component_descriptor,
-    right_component_descriptor,
-    left_component,
-    right_component
-):
-    left_images = set(_effective_images(left_component_descriptor, left_component))
-    right_images = set(_effective_images(right_component_descriptor, right_component))
+def _add_if_not_duplicate(list, res):
+    if res.name not in list:
+        list.append(res)
 
-    left_names_to_imgs = {i.name(): i for i in left_images}
-    right_names_to_imgs = {i.name(): i for i in right_images}
 
-    img_diff = ImageDiff(
+def diff_resources(
+    left_component: cm.Component,
+    right_component: cm.Component,
+) -> ResourceDiff:
+    if type(left_component) is not cm.Component:
+        raise NotImplementedError(
+            f'unsupported {type(left_component)=}. Only cm.Component is supported',
+        )
+    if type(right_component) is not cm.Component:
+        raise NotImplementedError(
+            f'unsupported {type(right_component)=}. Only cm.Component is supported',
+        )
+
+    left_resource_identities = {
+        r.identity(left_component.resources + right_component.resources): r
+        for r in left_component.resources
+    }
+    right_resource_identities_to_resource = {
+        r.identity(left_component.resources + right_component.resources): r
+        for r in right_component.resources
+    }
+
+    resource_diff = ResourceDiff(
         left_component=left_component,
         right_component=right_component,
     )
 
-    if left_images == right_images:
-        return img_diff
+    if left_resource_identities.keys() == right_resource_identities_to_resource.keys():
+        return resource_diff
 
-    for name, img in left_names_to_imgs.items():
-        if not name in right_names_to_imgs:
-            img_diff.irefs_only_left.add(img)
+    left_names_to_resource = {r.name: r for r in left_component.resources}
+    right_names_to_resource = {r.name: r for r in right_component.resources}
+    # get left exclisive resources
+    for resource in left_resource_identities.values():
+        if not resource.name in right_names_to_resource:
+            _add_if_not_duplicate(resource_diff.resource_refs_only_left, resource)
 
-    for name, img in right_names_to_imgs.items():
-        if not name in left_names_to_imgs:
-            img_diff.irefs_only_right.add(img)
+    # get right exclusive images
+    for resource in right_resource_identities_to_resource.values():
+        if not resource.name in left_names_to_resource:
+            _add_if_not_duplicate(resource_diff.resource_refs_only_right, resource)
 
-    lgroups = list(_grouped_effective_images(
-        left_component,
-        component_descriptor=left_component_descriptor,
-        )
-    )
-    rgroups = list(_grouped_effective_images(
-        right_component,
-        component_descriptor=right_component_descriptor,
-        )
-    )
+    def enumerate_group_pairs(
+        left_resources: typing.List[cm.Resource],
+        right_resources: typing.List[cm.Resource]
+    ) -> typing.Tuple[typing.List[cm.Resource], typing.List[cm.Resource]]:
+        # group the images with the same name on both sides
+        for key in left_names_to_resource.keys():
+            left_resource_group = [r for r in left_resources if r.name == key]
+            right_resource_group = [r for r in right_resources if r.name == key]
 
-    def enumerate_group_pairs(lgroups, rgroups):
-        for lgroup in lgroups:
-            lgroup = list(lgroup)
-            # img-group must always be non-empty
-            img_name = lgroup[0].name()
-            if not img_name in right_names_to_imgs:
-                continue # not all images exist on both sides
-            for rgroup in rgroups:
-                rgroup = list(rgroup)
-                if not rgroup[0].name() == img_name:
-                    continue
-                else:
-                    yield (lgroup, rgroup)
+            # key is always in left group
+            if len(right_resource_group) == 0:
+                continue
+            else:
+                yield (left_resource_group, right_resource_group)
 
-    for lgroup, rgroup in enumerate_group_pairs(lgroups, rgroups):
-        # trivial case: image groups have length of 1
-        if len(lgroup) == 1 and len(rgroup) == 1:
-            if lgroup[0].version() != rgroup[0].version():
-                img_diff.irefpairs_version_changed.add((lgroup[0], rgroup[0]))
+    for left_resource_group, right_resource_group in enumerate_group_pairs(
+        left_resources=left_component.resources,
+        right_resources=right_component.resources,
+    ):
+        if len(left_resource_group) == 1 and len(right_resource_group) == 1:
+            # if versions are equal resource will be ignored, resource is unchanged
+            if left_resource_group[0].version != right_resource_group[0].version:
+                resource_diff.resourcepairs_version_changed.append(
+                    (left_resource_group[0], right_resource_group[0]),
+                )
             continue
 
-        lgroup = sorted(lgroup)
-        rgroup = sorted(rgroup)
+        left_identities = {
+            r.identity(left_component.resources + right_component.resources): r
+            for r in left_resource_group
+        }
+        right_identities = {
+            r.identity(left_component.resources + right_component.resources): r
+                            for r in right_resource_group
+        }
+
+        left_resource_ids = sorted(left_identities.keys())
+        right_resource_ids = sorted(right_identities.keys())
+
+        left_resources = [left_identities.get(id) for id in left_resource_ids]
+        right_resources = [right_identities.get(id) for id in right_resource_ids]
 
         # remove all images present in both
         versions_in_both = {
-            i.version() for i in lgroup
+            r.version for r in left_resources
         } & {
-            i.version() for i in rgroup
+            r.version for r in right_resources
         }
-        lgroup = [
-            i for i in lgroup
-            if not i.version() in versions_in_both
+        left_resources = [
+            i for i in left_resources
+            if not i.version in versions_in_both
         ]
-        rgroup = [
-            i for i in rgroup
-            if not i.version() in versions_in_both
+        right_resources = [
+            i for i in right_resources
+            if not i.version in versions_in_both
         ]
 
         i = 0
-        for i, left_image in enumerate(lgroup):
-            if i >= len(rgroup):
-                img_diff.irefs_only_left.add(left_image)
+        for i, left_resource in enumerate(left_resources):
+            if i >= len(right_resources):
+                _add_if_not_duplicate(resource_diff.resource_refs_only_left, left_resource)
+
             else:
-                right_image = rgroup[i]
-                img_diff.irefpairs_version_changed.add((left_image, right_image))
+                right_resource = right_resources[i]
+                resource_diff.resourcepairs_version_changed.append((left_resource, right_resource))
 
-        lgroup = lgroup[i:]
-        rgroup = rgroup[i:]
+        # returns an empyt dict if index out of bounds
+        left_resource = left_resources[i:]
+        right_resource = right_resources[i:]
 
-        for i in lgroup:
-            img_diff.irefs_only_left.add(i)
+        for i in left_resource:
+            _add_if_not_duplicate(resource_diff.resource_refs_only_left, i)
 
-        for i in rgroup:
-            img_diff.irefs_only_right.add(i)
+        for i in right_resource:
+            _add_if_not_duplicate(resource_diff.resource_refs_only_right, i)
 
-    return img_diff
+    return resource_diff
 
 
 def _dep_overwrites_for_component(
--- test/product_/product_util_test.py
+++ test/product_/product_util_test.py
@@ -13,11 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import functools
 import pytest
 
 import product.util as util
 import product.model as model
+
+import gci.componentmodel as cm
 
 # functions under test
 diff_components = util.diff_components
@@ -27,16 +28,29 @@ def component_ref(name, version, prefix='gh.com/o/'):
     return model.ComponentReference.create(name=prefix + name, version=version)
 
 
+def component_id_v2(name: str, version: str):
+    return cm.ComponentIdentity(name=name, version=version)
+
+
 @pytest.fixture
-def cref():
-    return component_ref
+def cid():
+    return component_id_v2
+
+
+def comp(name, version) -> cm.Component:
+    return cm.Component(name, version, [],[],[],[],[],[])
 
 
 def image_ref(name, version):
-    return model.ContainerImage.create(
+    return cm.Resource(
         name=name,
         version=version,
-        image_reference='dont:care',
+        access=cm.OciAccess,
+        type=cm.AccessType.OCI_REGISTRY,
+        extraIdentity={},
+        labels=[],
+        srcRefs=[],
+        relation=None,
     )
 
 
@@ -45,112 +59,103 @@ def iref():
     return image_ref
 
 
-def test_diff_components(cref):
+def test_diff_components(cid):
     left_components = (
-        cref('c1', '1.2.3'),
-        cref('c2', '1.2.3'),
-        cref('c3', '1.2.3'),
+        comp('c1', '1.2.3'),
+        comp('c2', '1.2.3'),
+        comp('c3', '1.2.3'),
+        # comp('c4', '1.2.3'), # missing on left
+        comp('c5', '1.2.3'), # version change
+
     )
     right_components = (
-        cref('c1', '2.2.3'), # version changed
-        cref('c2', '1.2.3'), # no change
-        #cref('c3', '1.2.3'), # missing on right
-        cref('c4', '1.2.3'), # added on right
+        comp('c1', '2.2.3'), # version changed
+        comp('c2', '1.2.3'), # no change
+        #cid('c3', '1.2.3'), # missing on right
+        comp('c4', '1.2.3'), # added on right
+        comp('c5', '2.3.4'), # version changed
     )
 
     result = diff_components(left_components, right_components)
 
-    assert result.crefs_only_left == {cref('c3', '1.2.3'), cref('c1', '1.2.3')}
-    assert result.crefs_only_right == {cref('c4', '1.2.3'), cref('c1', '2.2.3')}
-    assert result.crefpairs_version_changed == {(cref('c1', '1.2.3'), cref('c1', '2.2.3'))}
-    assert result.names_only_left == {'gh.com/o/c3'}
-    assert result.names_only_right == {'gh.com/o/c4'}
-    assert result.names_version_changed == {'gh.com/o/c1'}
+    assert result.cidentities_only_left == {
+        cid('c1', '1.2.3'), cid('c3', '1.2.3'), cid('c5', '1.2.3'),
+    }
+    assert result.cidentities_only_right == {
+        cid('c1', '2.2.3'), cid('c4', '1.2.3'), cid('c5', '2.3.4'),
+    }
+    assert result.cpairs_version_changed == [
+        (comp('c1', '1.2.3'), comp('c1', '2.2.3')),
+        (comp('c5', '1.2.3'), comp('c5', '2.3.4')),
+    ]
+    assert result.names_only_left == {'c3'}
+    assert result.names_only_right == {'c4'}
+    assert result.names_version_changed == {'c1','c5'}
 
 
-def test_diff_images(cref, iref):
-    comp_desc = model.ComponentDescriptor.from_dict({})
-    left_comp = model.Component.create('x.o/a/b', '1.2.3')
-    right_comp = model.Component.create('x.o/a/b', '2.3.4')
-    comp_desc.add_component(left_comp)
-    comp_desc.add_component(right_comp)
-    l_deps = left_comp.dependencies()
-    r_deps = right_comp.dependencies()
-
-    examinee = functools.partial(
-        util.diff_images,
-        left_component_descriptor=comp_desc,
-        right_component_descriptor=comp_desc,
-    )
+#TODO add other resources than OCI images
+def test_diff_resources(iref):
+    left_comp = comp('x.o/a/b', '1.2.3')
+    right_comp = comp('x.o/a/b', '2.3.4')
 
     img1 = iref('i1', '1.2.3')
 
-    l_deps.add_container_image_dependency(img1)
-    r_deps.add_container_image_dependency(img1)
+    left_comp.resources.append(img1)
+    right_comp.resources.append(img1)
 
-    img_diff = examinee(left_component=left_comp, right_component=right_comp)
+    img_diff = util.diff_resources(left_component=left_comp, right_component=right_comp)
 
     # same image added declared by left and right - expect empty diff
     assert img_diff.left_component == left_comp
     assert img_diff.right_component == right_comp
-    assert len(img_diff.irefs_only_left) == 0
-    assert len(img_diff.irefs_only_right) == 0
+    assert len(img_diff.resource_refs_only_right) == 0
+    assert len(img_diff.resource_refs_only_left) == 0
 
     img2 = iref('i2', '1.2.3')
     img3 = iref('i3', '1.2.3')
-    l_deps.add_container_image_dependency(img2)
-    r_deps.add_container_image_dependency(img3)
+    left_comp.resources.append(img2)
+    right_comp.resources.append(img3)
 
     # img2 only left, img3 only right
-    img_diff = examinee(left_component=left_comp, right_component=right_comp)
-    assert len(img_diff.irefs_only_left) == 1
-    assert len(img_diff.irefs_only_right) == 1
-    assert list(img_diff.irefs_only_left)[0] == img2
-    assert list(img_diff.irefs_only_right)[0] == img3
+    resource_diff = util.diff_resources(left_component=left_comp, right_component=right_comp)
+    assert len(resource_diff.resource_refs_only_left) == 1
+    assert len(resource_diff.resource_refs_only_right) == 1
+    assert list(resource_diff.resource_refs_only_left)[0] == img2
+    assert list(resource_diff.resource_refs_only_right)[0] == img3
 
     img4_0 = iref('i4', '1.2.3')
-    img4_1 = iref('i4', '2.0.0') # "change version"
-    l_deps.add_container_image_dependency(img4_0)
-    r_deps.add_container_image_dependency(img4_1)
-    img_diff = examinee(left_component=left_comp, right_component=right_comp)
-    assert len(img_diff.irefs_only_left) == 1
-    assert len(img_diff.irefs_only_right) == 1
-    assert len(img_diff.irefpairs_version_changed) == 1
-    left_i, right_i = list(img_diff.irefpairs_version_changed)[0]
+    img4_1 = iref('i4', '2.0.0') # changed version
+    left_comp.resources.append(img4_0)
+    right_comp.resources.append(img4_1)
+    resource_diff = util.diff_resources(left_component=left_comp, right_component=right_comp)
+    assert len(resource_diff.resource_refs_only_left) == 1
+    assert len(resource_diff.resource_refs_only_right) == 1
+    assert len(resource_diff.resourcepairs_version_changed) == 1
+    left_i, right_i = list(resource_diff.resourcepairs_version_changed)[0]
     assert type(left_i) == type(img4_0)
-    assert left_i.raw == img4_0.raw
     assert left_i == img4_0
     assert right_i == img4_1
 
+    # test whether exclusive images with the same name are working
+    img5_0 = iref('res5', '1.2.3')
+    img5_1 = iref('res5', '1.2.4')
+    img5_2 = iref('res5', '1.2.5')
+    left_comp.resources.append(img5_0)
+    left_comp.resources.append(img5_1)
+    left_comp.resources.append(img5_2)
+    resource_diff = util.diff_resources(left_component=left_comp, right_component=right_comp)
+    assert len(resource_diff.resource_refs_only_left) == 4
+    assert len(resource_diff.resource_refs_only_right) == 1
+    assert list(resource_diff.resource_refs_only_left)[1] == img5_0
+    assert list(resource_diff.resource_refs_only_left)[2] == img5_1
+    assert list(resource_diff.resource_refs_only_left)[3] == img5_2
 
-def test_grouped_effective_images(cref):
-    comp_desc = model.ComponentDescriptor.from_dict({})
+    # test if grouping semantic does work
+    right_comp.resources.append(img5_0)
+    img5_3 = iref('res5', '1.2.6')
+    right_comp.resources.append(img5_3)
+    resource_diff = util.diff_resources(left_component=left_comp, right_component=right_comp)
 
-    c1 = cref('c1', '1.2.3')
-    comp_desc.add_component(c1)
-    c1 = comp_desc.component(c1)
-
-    c1deps = c1.dependencies()
-    image1 = model.ContainerImage.create(name='i1', version='1.2.3', image_reference='i:1')
-    # image2 shares same logical name
-    image2 = model.ContainerImage.create(name='i1', version='1.2.4', image_reference='i:3')
-    # image3 has different logical name
-    image3 = model.ContainerImage.create(name='xo', version='1.2.4', image_reference='i:3')
-
-    c1deps.add_container_image_dependency(image1)
-    c1deps.add_container_image_dependency(image2)
-    c1deps.add_container_image_dependency(image3)
-
-    grouped_images = tuple(
-        util._grouped_effective_images(
-            c1,
-            component_descriptor=comp_desc,
-        )
-    )
-
-    # image1 and image2 should have been grouped, so expect two groups
-    assert len(grouped_images) == 2
-    img1_and_img2, img3 = grouped_images
-
-    assert len(img1_and_img2) == 2
-    assert len(img3) == 1
+    assert len(resource_diff.resource_refs_only_left) == 3
+    assert len(resource_diff.resource_refs_only_right) == 1
+    assert len(resource_diff.resourcepairs_version_changed) == 2
