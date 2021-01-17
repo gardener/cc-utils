a44ea468f493701a0911323463c10430e15a7559
Christian Cwienk <christian.cwienk@sap.com>
2021-01-17 05:49:22 +0100

    rm v1-code

--- concourse/steps/component_descriptor_util.py
+++ concourse/steps/component_descriptor_util.py
@@ -3,22 +3,18 @@ import os
 
 import gci.componentmodel
 
-import product.model
-
 
 def component_descriptor_fname(
-    schema_version=gci.componentmodel.SchemaVersion.V1,
+    schema_version=gci.componentmodel.SchemaVersion.V2,
 ):
-    if schema_version is gci.componentmodel.SchemaVersion.V1:
-      return 'component_descriptor_v1'
-    elif schema_version is gci.componentmodel.SchemaVersion.V2:
+    if schema_version is gci.componentmodel.SchemaVersion.V2:
       return 'component_descriptor_v2'
     else:
         raise NotImplementedError(schema_version)
 
 
 def component_descriptor_path(
-    schema_version=gci.componentmodel.SchemaVersion.V1,
+    schema_version=gci.componentmodel.SchemaVersion.V2,
 ):
     fname = component_descriptor_fname(schema_version=schema_version)
     return os.path.join(
@@ -28,18 +24,9 @@ def component_descriptor_path(
 
 
 def parse_component_descriptor(
-    schema_version=gci.componentmodel.SchemaVersion.V1,
+    schema_version=gci.componentmodel.SchemaVersion.V2,
 ):
-    if schema_version is gci.componentmodel.SchemaVersion.V1:
-      component_descriptor = product.model.ComponentDescriptor.from_dict(
-        raw_dict=ci.util.parse_yaml_file(
-          component_descriptor_path(
-            schema_version=schema_version,
-          )
-        )
-      )
-      return component_descriptor
-    elif schema_version is gci.componentmodel.SchemaVersion.V2:
+    if schema_version is gci.componentmodel.SchemaVersion.V2:
       component_descriptor = gci.componentmodel.ComponentDescriptor.from_dict(
           component_descriptor_dict=ci.util.parse_yaml_file(
             component_descriptor_path(
