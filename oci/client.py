dd7cfef203bf629ec9311a2e71f49826c18c32e4
Christian Cwienk <christian.cwienk@sap.com>
2021-01-20 12:46:08 +0100

    fix: raise unless http-404

--- oci/client.py
+++ oci/client.py
@@ -270,7 +270,7 @@ class Client:
             if he.response.status_code == 404:
                 if absent_ok:
                     return None
-                raise om.OciImageNotFoundException(he.response) from he
+            raise om.OciImageNotFoundException(he.response) from he
 
         return res
 
