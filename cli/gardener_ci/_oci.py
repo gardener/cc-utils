9f0d99d2140dd2856bc3d5b601c7cff9fbf74fb3
Christian Cwienk <christian.cwienk@sap.com>
2021-01-27 11:05:30 +0100

    rm one more legacy-function

--- cli/gardener_ci/_oci.py
+++ cli/gardener_ci/_oci.py
@@ -1,10 +1,14 @@
-import container.registry as cr
+import ccc.oci
+import oci
 
 __cmd_name__ = 'oci'
 
 
 def cp(src:str, tgt:str):
-    cr.cp_oci_artifact(
+    oci_client = ccc.oci.oci_client()
+
+    oci.replicate_artifact(
         src_image_reference=src,
         tgt_image_reference=tgt,
+        oci_client=oci_client,
     )
