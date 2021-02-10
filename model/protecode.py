79a51d85ba9655821db8a58518d478622e1c4571
Andreas Burger <andreas.burger@sap.com>
2021-02-10 16:02:59 +0000

    Add support for oauth to protecode client

--- model/protecode.py
+++ model/protecode.py
@@ -12,11 +12,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import enum
+import typing
 
 from model.base import (
     BasicCredentials,
     NamedModelElement,
+    TokenCredentials,
 )
+
+
+class ProtecodeAuthScheme(enum.Enum):
+    BASIC_AUTH = 'basic_auth'
+    BEARER_TOKEN = 'bearer_token'
 
 
 class ProtecodeConfig(NamedModelElement):
@@ -24,15 +32,39 @@ class ProtecodeConfig(NamedModelElement):
     Not intended to be instantiated by users of this module
     '''
 
-    def credentials(self):
-        return ProtecodeCredentials(self.raw.get('credentials'))
+    def auth_scheme(self) -> ProtecodeAuthScheme:
+        return ProtecodeAuthScheme(self.raw['credentials'].get('auth_scheme', 'basic_auth'))
 
-    def api_url(self):
+    def credentials(self) -> typing.Union[BasicCredentials, TokenCredentials]:
+        if (auth_scheme := self.auth_scheme()) is ProtecodeAuthScheme.BASIC_AUTH:
+            return BasicCredentials(
+                raw_dict={
+                    'password': self.raw['credentials']['password'],
+                    'username': self.raw['credentials']['username']
+                }
+            )
+        elif auth_scheme is ProtecodeAuthScheme.BEARER_TOKEN:
+            return TokenCredentials(
+                raw_dict={
+                    'token': self.raw['credentials']['token'],
+                }
+            )
+        else:
+            raise NotImplementedError(auth_scheme)
+
+    def api_url(self) -> str:
         return self.raw.get('api_url')
 
-    def tls_verify(self):
+    def tls_verify(self) -> bool:
         return self.raw.get('tls_verify', True)
 
+    def _required_attributes(self):
+        yield from super()._required_attributes()
+        yield from (
+            'api_url',
+            'credentials',
+        )
 
-class ProtecodeCredentials(BasicCredentials):
-    pass
+    def validate(self):
+        # call credentials method to validate credentials
+        self.credentials()
