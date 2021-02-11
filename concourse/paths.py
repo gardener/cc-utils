637017cd8f3cda74f8121045638149ad4bc5b4cb
Christian Cwienk <christian.cwienk@sap.com>
2021-02-11 12:44:24 +0100

    template-path-def

--- concourse/paths.py
+++ concourse/paths.py
@@ -2,5 +2,6 @@ import os
 
 own_dir = os.path.abspath(os.path.dirname(__file__))
 res_dir = os.path.join(own_dir, 'resources')
+template_dir = os.path.join(own_dir, 'templates')
 
 last_released_tag_file = os.path.join(res_dir, 'LAST_RELEASED_TAG')
