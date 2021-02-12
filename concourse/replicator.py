aeae49f5e643bb43fbb4bd3b0c5cee8fe317daea
Christian Cwienk <christian.cwienk@sap.com>
2021-02-12 10:51:47 +0100

    refactor + add convenience-method

--- concourse/replicator.py
+++ concourse/replicator.py
@@ -1,17 +1,4 @@
-# Copyright (c) 2019-2020 SAP SE or an SAP affiliate company. All rights reserved. This file is
-# licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import dataclasses
 import os
 
 from enum import Enum, IntEnum
@@ -200,16 +187,21 @@ class DeployStatus(IntEnum):
     CREATED = 8
 
 
+@dataclasses.dataclass
 class DeployResult:
-    def __init__(
-        self,
-        definition_descriptor,
-        deploy_status,
-        error_details=None,
-    ):
-        self.definition_descriptor = not_none(definition_descriptor)
-        self.deploy_status = not_none(deploy_status)
-        self.error_details = error_details
+    definition_descriptor: DefinitionDescriptor
+    deploy_status: DeployStatus
+    error_details: str = None
+
+    def ok(self):
+        if self.deploy_status in (
+            DeployStatus.SUCCEEDED, DeployStatus.SKIPPED, DeployStatus.CREATED
+        ):
+            return True
+        elif self.deploy_status is DeployStatus.FAILED:
+            return False
+        else:
+            raise NotImplementedError(self.deploy_status)
 
 
 class DefinitionDeployer:
