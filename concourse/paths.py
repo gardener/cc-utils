f2d48d80ac7885171957fc160e7f8a85d748961a
Christian Cwienk <christian.cwienk@sap.com>
2021-02-12 08:48:18 +0100

    expose template-include-dir (alias)

--- concourse/paths.py
+++ concourse/paths.py
@@ -3,5 +3,6 @@ import os
 own_dir = os.path.abspath(os.path.dirname(__file__))
 res_dir = os.path.join(own_dir, 'resources')
 template_dir = os.path.join(own_dir, 'templates')
+template_include_dir = own_dir
 
 last_released_tag_file = os.path.join(res_dir, 'LAST_RELEASED_TAG')
