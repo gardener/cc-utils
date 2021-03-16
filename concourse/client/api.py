e88e562d6423a2a445f462e2934da28fa4bc68f4
Christian Cwienk <christian.cwienk@sap.com>
2021-03-16 09:13:50 +0100

    refactor cc-client some more

--- concourse/client/api.py
+++ concourse/client/api.py
@@ -200,7 +200,7 @@ class ConcourseApiBase:
         if isinstance(pipeline_names, str):
             pipeline_names = [pipeline_names]
 
-        resources = map(lambda name: self.pipeline_cfg(pipeline_name=name).resources, pipeline_names)
+        resources = (self.pipeline_cfg(pipeline_name=name).resources for name in pipeline_names)
         for resource_list in resources:
             for resource in resource_list:
                 # resource is of type concourse.client.model.PipelineConfigResource
