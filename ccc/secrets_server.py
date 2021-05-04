7cf95840e6ee03443bb630e1e83d84639d2e2b6f
Johannes Krayl <johannes.krayl@sap.com>
2021-05-04 09:01:32 +0200

    Revert "adding log message for secret decryption env vars"
    This reverts commit 5f85b41473a425943e7efd6311cc10279d3ae320.

--- ccc/secrets_server.py
+++ ccc/secrets_server.py
@@ -15,7 +15,6 @@
 
 import base64
 import json
-import logging
 import os
 import socket
 import typing
@@ -26,8 +25,6 @@ import requests
 from ci.util import urljoin
 import model.secret
 
-logger = logging.getLogger(__name__)
-
 
 def get_secret_cfg_from_env_if_available(
     key_env_var='SECRET_KEY',
@@ -37,16 +34,9 @@ def get_secret_cfg_from_env_if_available(
         secret_key = base64.b64decode(os.environ.get(key_env_var).encode('utf-8'))
         cipher_algorithm = model.secret.Cipher(os.environ.get(cipher_algorithm))
 
-        logger.info(
-            'SECRET_KEY and SECRET_CIPHER_ALGORITHM environment variables present and '
-            'will be used for secret decryption'
-        )
-        return model.secret.SecretData(key=secret_key, cipher_algorithm=cipher_algorithm)
-    else:
-        logger.info(
-            'SECRET_KEY and SECRET_CIPHER_ALGORITHM environment variables not found in env. '
-            'Falling back to unencrypted secret path',
-        )
+        secret = model.secret.SecretData(key=secret_key, cipher_algorithm=cipher_algorithm)
+
+        return secret
 
 
 class SecretsServerClient:
