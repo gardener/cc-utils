2dcf0f5a67f4c5d672ffe4e4252117b840d5e00f
Philipp Heil <dev@zeekay.dev>
2021-05-26 11:50:54 +0200

    Add ctf support (#606)
    Select cd from dir
    
    Fix linter
    
    Enhance logging
    
    Undo alias
    
    Fix linter

--- concourse/steps/component_descriptor_util.py
+++ concourse/steps/component_descriptor_util.py
@@ -1,7 +1,14 @@
 import ci.util
+import logging
 import os
 
+import cnudie.util
+# do not use cm alias here, it does not work with templating
 import gci.componentmodel
+import product.v2
+
+
+logger: logging.Logger = logging.getLogger(__name__)
 
 
 def component_descriptor_fname(
@@ -37,3 +44,81 @@ def parse_component_descriptor(
       return component_descriptor
     else:
         raise NotImplementedError(schema_version)
+
+
+def component_descriptor_from_component_descriptor_path(
+    cd_path: str,
+) -> gci.componentmodel.ComponentDescriptor:
+
+    if not os.path.isfile(cd_path):
+        raise FileNotFoundError(
+            f'{os.path.abspath(cd_path)=} not found'
+        )
+
+    descriptor_v2 = gci.componentmodel.ComponentDescriptor.from_dict(
+        ci.util.parse_yaml_file(cd_path)
+    )
+    logger.info(f'found component-descriptor (v2) at {cd_path=}')
+    return descriptor_v2
+
+
+def component_descriptor_from_ctf_path(ctf_path: str) -> gci.componentmodel.ComponentDescriptor:
+
+    if not os.path.exists(ctf_path):
+        raise FileNotFoundError(
+            f'{os.path.abspath(ctf_path)=} not found'
+        )
+
+    if not os.path.isfile(ctf_path):
+        raise ValueError(
+            f'{os.path.abspath(ctf_path)=} is not a file'
+        )
+
+    component_descriptors = [
+        cd
+        for cd in cnudie.util.component_descriptors_from_ctf_archive(ctf_path)
+    ]
+
+    if len(component_descriptors) == 0:
+        raise RuntimeError(
+            f'No component descriptor found in ctf archive at {os.path.abspath(ctf_path)}'
+        )
+    if len(component_descriptors) > 1:
+        raise NotImplementedError(
+            f'More than one component_descriptor found at {os.path.abspath(ctf_path)}'
+        )
+    logger.info(f'found component-descriptor (v2) in {ctf_path=}')
+    return component_descriptors[0]
+
+
+def component_descriptor_from_dir(dir_path: str) -> gci.componentmodel.ComponentDescriptor:
+
+    if not os.path.isdir(dir_path):
+        raise NotADirectoryError(
+            f'{os.path.abspath(dir_path)=} is no directory'
+        )
+
+    v2_outfile = os.path.join(
+        dir_path,
+        component_descriptor_fname(
+            schema_version=gci.componentmodel.SchemaVersion.V2,
+        ),
+    )
+    ctf_out_path = os.path.abspath(
+        os.path.join(
+            dir_path,
+            product.v2.CTF_OUT_DIR_NAME,
+        )
+    )
+
+    have_ctf = os.path.exists(ctf_out_path)
+    have_cd = os.path.exists(v2_outfile)
+
+    if not have_ctf ^ have_cd:
+        logger.error(f'exactly one of {ctf_out_path=}, {v2_outfile=} must exist')
+
+    elif have_cd:
+        return component_descriptor_from_component_descriptor_path(cd_path=v2_outfile)
+
+    elif have_ctf:
+        return component_descriptor_from_ctf_path(ctf_path=ctf_out_path)
--- concourse/steps/scan_sources.mako
+++ concourse/steps/scan_sources.mako
@@ -13,17 +13,17 @@ checkmarx_cfg = source_scan_trait.checkmarx()
 whitesource_cfg = source_scan_trait.whitesource()
 email_recipients = source_scan_trait.email_recipients()
 component_trait = job_variant.trait('component_descriptor')
-
+component_descriptor_dir = job_step.input('component_descriptor_dir')
 %>
 ${step_lib('component_descriptor_util')}
 ${step_lib('scan_sources')}
 
-import gci.componentmodel as cm
+component_descriptor = component_descriptor_from_dir(dir_path='${component_descriptor_dir}')
 
 % if checkmarx_cfg:
 scan_sources_and_notify(
     checkmarx_cfg_name='${checkmarx_cfg.checkmarx_cfg_name()}',
-    component_descriptor_path=component_descriptor_path(schema_version=cm.SchemaVersion.V2),
+    component_descriptor=component_descriptor,
     email_recipients=${email_recipients},
     team_id='${checkmarx_cfg.team_id()}',
     threshold=${checkmarx_cfg.severity_threshold()},
@@ -35,7 +35,7 @@ scan_sources_and_notify(
 % if whitesource_cfg:
 component_name = '${component_trait.component_name()}'
 scan_component_with_whitesource(
-    component_descriptor_path=component_descriptor_path(schema_version=cm.SchemaVersion.V2),
+    component_descriptor=component_descriptor,
     cve_threshold=${whitesource_cfg.cve_threshold()},
     extra_whitesource_config={},
     notification_recipients=${email_recipients},
