5d4f09a7eedbed36de7034cf921dfc9683556285
enrico-kaack-comp <enrico.kaack@sap.com>
2021-07-14 17:37:15 +0200

    Fixed: linting error for real

--- oci/client.py
+++ oci/client.py
@@ -464,7 +464,8 @@ class Client:
                 (mediaType := manifest_dict['mediaType']) == distribution_list_mediatype
             ):
             #XXX: ignore for now, need to be fixed later
-            raise NotImplementedError(f'OCI Manifest media type {mediaType} not implemented for {image_reference}')
+            raise NotImplementedError(f'OCI Manifest media type {mediaType}'
+                                       'not implemented for {image_reference}')
 
         if (schema_version := int(manifest_dict['schemaVersion'])) == 1:
             manifest = dacite.from_dict(
