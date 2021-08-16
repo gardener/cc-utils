955e9507383469d1d8dafa33bc783d88f0b7b785
Johannes Krayl <johannes.krayl@sap.com>
2021-08-16 14:44:47 +0200

    adapt changes in cc client factory

--- ccc/concourse.py
+++ ccc/concourse.py
@@ -13,37 +13,101 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import functools
+import warnings
 
 import ci.util
 import concourse.client
 import ctx
+import model.concourse
 
-from ensure import ensure_annotations
+import ensure
+import concourse.client.api
+
+
+def lookup_cc_uam(cfg_set, team_name) -> model.concourse.ConcourseUAM:
+    for cc_uam in cfg_set._cfg_elements('concourse_uam'):
+        if cc_uam.team_name() == team_name:
+            return cc_uam
+
+    raise RuntimeError(f'No concourse uam for team name {team_name} found')
+
+
+@ensure.ensure_annotations
+def from_parameters(
+    base_url: str,
+    password: str,
+    team_name: str,
+    username: str,
+    verify_ssl: bool = 'True',
+    concourse_api_version=None,
+) -> concourse.client.api.ConcourseApiBase:
+    """
+    inits the concourse api object
+    """
+
+    concourse_api = concourse.client.api.ConcourseApiFactory.create_api(
+        base_url=base_url,
+        team_name=team_name,
+        verify_ssl=verify_ssl,
+        concourse_api_version=concourse_api_version,
+    )
+
+    concourse_api.login(
+        username=username,
+        passwd=password,
+    )
+    return concourse_api
 
 
 @functools.lru_cache()
-@ensure_annotations
+@ensure.ensure_annotations
 def client_from_cfg_name(
-    concourse_cfg_name: str,
-    team_name: str
-):
+    team_name: str,
+    concourse_cfg_name: str = '',
+) -> concourse.client.api.ConcourseApiBase:
+    if concourse_cfg_name:
+        warnings.warn(
+            'Parameter concourse_cfg_name is deprecated and will not be used',
+            DeprecationWarning,
+            3,
+        )
+
     cfg_factory = ci.util.ctx().cfg_factory()
+    cc_uam = lookup_cc_uam(cfg_set=cfg_factory, team_name=team_name)
+    cc_endpoint = cfg_factory.concourse_endpoint(cc_uam.concourse_endpoint_name())
 
-    cc_cfg = cfg_factory.concourse(concourse_cfg_name)
-    uam_cfg = cfg_factory.concourse_uam(cc_cfg.concourse_uam_config())
-
-    return concourse.client.from_cfg(
-        concourse_cfg=cc_cfg,
-        concourse_uam_cfg=uam_cfg,
+    return from_parameters(
+        base_url=cc_endpoint,
+        password=cc_uam.password(),
         team_name=team_name,
-        verify_ssl=True,
+        username=cc_uam.username(),
+    )
+
+
+def client_from_cfg_set(
+    cfg_set,
+    team_name: str = '',
+) -> concourse.client.api.ConcourseApiBase:
+
+    if not team_name:
+        team_name = ci.util.check_env('CONCOURSE_CURRENT_TEAM')
+
+    cc_uam = lookup_cc_uam(cfg_set=cfg_set, team_name=team_name)
+    cc_endpoint = cfg_set.concourse_endpoint(cc_uam.concourse_endpoint_name())
+
+    return from_parameters(
+        base_url=cc_endpoint,
+        password=cc_uam.password(),
+        team_name=team_name,
+        username=cc_uam.username(),
     )
 
 
 def client_from_env(
-    team_name: str=None,
-):
-    '''
+    cfg_set = None,
+    team_name: str = None,
+) -> concourse.client.api.ConcourseApiBase:
+    """
     returns a concourse-client w/ the credentials valid for the current execution environment.
     Note that this function must only be called if running in a "central" cicd-job.
     The returned client is authorised to perform operations in the same concourse-team as the
@@ -51,15 +115,19 @@ def client_from_env(
 
     if the (optional) team_name is specified, the returned client is not guaranteed to have the
     required authorisation.
-    '''
-    cfg_set = ctx.cfg_set()
-    cc_cfg = cfg_set.concourse()
-    cc_uam = cfg_set.concourse_uam(cc_cfg.concourse_uam_config())
+    """
+
     if not team_name:
         team_name = ci.util.check_env('CONCOURSE_CURRENT_TEAM')
 
-    return concourse.client.from_cfg(
-        concourse_cfg=cc_cfg,
-        concourse_uam_cfg=cc_uam,
+    if not cfg_set:
+        cfg_set = ctx.cfg_set()
+    cc_uam = lookup_cc_uam(cfg_set=cfg_set, team_name=team_name)
+    cc_endpoint = cfg_set.concourse_endpoint(cc_uam.concourse_endpoint_name())
+
+    return from_parameters(
+        base_url=cc_endpoint.url(),
+        password=cc_uam.password(),
         team_name=team_name,
+        username=cc_uam.username(),
     )
