202619909ebd01c511f01a1104b6e9073fbf3a85
zkdev <dev@zeekay.dev>
2021-10-15 14:51:47 +0200

    Use jsonb instead of hstore

--- dso/deliverydb/model.py
+++ dso/deliverydb/model.py
@@ -1,5 +1,5 @@
-from sqlalchemy.dialects.postgresql import HSTORE
-from sqlalchemy import Column, Integer
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.mutable import MutableDict
 
@@ -10,8 +10,8 @@ Base = declarative_base()
 class Scan(Base):
     __tablename__ = 'compliance_issue'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String, primary_key=True)
     # combination of hstore and mutabledict to allow in-place changes to the dict.
     # https://docs.sqlalchemy.org/en/14/dialects/
     # postgresql.html#sqlalchemy.dialects.postgresql.HSTORE
-    data = Column(MutableDict.as_mutable(HSTORE))
+    data = Column(MutableDict.as_mutable(JSONB), nullable=True, default=dict)
