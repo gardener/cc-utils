5a7e3e0f3fa8f520ccb3d3533602af762bf4744d
zkdev <dev@zeekay.dev>
2021-10-18 15:39:23 +0200

    Mv insert method to db-client

--- dso/deliverydb/db.py
+++ dso/deliverydb/db.py
@@ -1,8 +1,13 @@
+import dataclasses
 import logging
+import typing
 
 import sqlalchemy
 
 from dso.deliverydb.model import Base
+import dso.model
+import dso.util
+import protecode.model
 
 
 class DeliveryDB:
@@ -14,6 +19,7 @@ class DeliveryDB:
         port: int,
         dialect: str = 'postgresql',
     ):
+        self.logger = logging.getLogger(__name__)
         self._engine = sqlalchemy.create_engine(
             f'{dialect}://{username}:{password}@{hostname}:{port}',
             echo=True,
@@ -26,3 +32,46 @@ class DeliveryDB:
         logging.getLogger('sqlalchemy.engine.Engine').handlers.pop()
         self.Base.metadata.create_all(self._engine)
         self.Session = sqlalchemy.orm.Session(self._engine)
+
+    def _insert(
+        self,
+        issue: dso.model.ComplianceIssue,
+    ):
+        self.logger.info('inserting result to deliverydb')
+        scan = dso.deliverydb.model.Scan(
+            id=dataclasses.asdict(issue.id),
+            meta=dataclasses.asdict(issue.meta),
+            data=issue.data,
+        )
+
+        self.Session.add(scan)
+        self.Session.commit()
+
+    def insert_compliance_issue(
+        self,
+        datasource: dso.model.Datasource,
+        # extend typehints with more integrations
+        scan: typing.Union[protecode.model.UploadResult],
+    ):
+        if datasource is dso.model.Datasource.PROTECODE:
+            issue = dso.util.upload_result_to_compliance_issue(
+                upload_result=scan,
+            )
+            self._insert(
+                issue=issue,
+            )
+        else:
+            raise NotImplementedError(datasource)
+
+
+def make_deliverydb(
+    deliverydb_cfg_name: str,
+) -> DeliveryDB:
+    cfg_fac = ci.util.ctx().cfg_factory()
+    cfg = cfg_fac.compliancedb(deliverydb_cfg_name)
+    return DeliveryDB(
+        username=cfg.credentials().username(),
+        password=cfg.credentials().password(),
+        hostname=cfg.hostname(),
+        port=cfg.port(),
+    )
--- dso/util.py
+++ dso/util.py
@@ -1,7 +1,55 @@
+import dataclasses
+import datetime
 import functools
+import uuid
 import logging
+
+import dso.deliverydb
+import dso.deliverydb.db
+import dso.deliverydb.model
+import dso.model
+import protecode.model
 
 
 @functools.lru_cache
 def component_logger(name: str):
     return logging.getLogger(name)
+
+
+def upload_result_to_compliance_issue(
+    upload_result: protecode.model.UploadResult,
+    datasource: dso.model.Datasource = dso.model.Datasource.PROTECODE,
+) -> dso.model.ComplianceIssue:
+
+    artifact = dataclasses.asdict(upload_result.resource)
+    artifact['type'] = upload_result.resource.type.name
+    artifact['access']['type'] = upload_result.resource.access.type.name
+    artifact['relation'] = upload_result.resource.relation.name
+
+    id = dso.model.ComplianceIssueId(
+        componentName=upload_result.component.name,
+        componentVersion=upload_result.component.version,
+        artifact=artifact,
+    )
+
+    meta = dso.model.ComplianceIssueMeta(
+        datasource=datasource.name,
+        creationDate=datetime.datetime.now().isoformat(),
+        uuid=str(uuid.uuid4()),
+    )
+
+    data = [
+        {
+            'component': f'{c.name()}:{c.version()}',
+            'license': c.license().name(),
+            'vulnerabilities': [v.cve() for v in c.vulnerabilities()],
+        }
+        for c in upload_result.result.components()
+    ]
+
+    issue = dso.model.ComplianceIssue(
+        id=id,
+        meta=meta,
+        data={'data': data},
+    )
+    return issue
