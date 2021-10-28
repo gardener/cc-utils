31837b0ae9ea2e1bf7ad397be82726c7532d8847
zkdev <dev@zeekay.dev>
2021-10-28 15:26:41 +0200

    Add optional hostname parameter

--- ccc/deliverydb.py
+++ ccc/deliverydb.py
@@ -4,12 +4,13 @@ from dso.deliverydb.db import DeliveryDB
 
 def default_with_cfg_name(
     cfg_name: str,
+    hostname: str = None,
 ) -> DeliveryDB:
     cfg_fac = ci.util.ctx().cfg_factory()
     cfg = cfg_fac.compliancedb(cfg_name)
     return DeliveryDB(
         username=cfg.credentials().username(),
         password=cfg.credentials().password(),
-        hostname=cfg.hostname(),
+        hostname=cfg.hostname() if not hostname else hostname,
         port=cfg.port(),
     )
