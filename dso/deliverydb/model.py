cb8d135d63cc5118c1a8a35cf4221f2a15aed71b
zkdev <dev@zeekay.dev>
2021-10-28 12:22:58 +0200

    Make Scan class convertable to dict

--- dso/deliverydb/model.py
+++ dso/deliverydb/model.py
@@ -13,3 +13,8 @@ class Scan(Base):
     artifact = Column(JSONB, nullable=True, default=dict)
     meta = Column(JSONB, nullable=True, default=dict)
     data = Column(JSONB, nullable=True, default=dict)
+
+    def __iter__(self):
+        yield 'artifact', self.artifact
+        yield 'meta', self.meta
+        yield 'data', self.data
