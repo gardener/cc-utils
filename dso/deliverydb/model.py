e5fe413add18545efae02bcafb9445e66c88e8b2
zkdev <dev@zeekay.dev>
2021-10-28 10:30:30 +0200

    Make deliverydb columns immutable

--- dso/deliverydb/model.py
+++ dso/deliverydb/model.py
@@ -1,7 +1,6 @@
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.mutable import MutableDict
 
 
 Base = declarative_base()
@@ -11,9 +10,6 @@ class Scan(Base):
     __tablename__ = 'compliance_issue'
 
     database_entry_id = Column(Integer, primary_key=True, autoincrement=True)
-    # combination of hstore and mutabledict to allow in-place changes to the dict.
-    # https://docs.sqlalchemy.org/en/14/dialects/
-    # postgresql.html#sqlalchemy.dialects.postgresql.HSTORE
-    artifact = Column(MutableDict.as_mutable(JSONB), nullable=True, default=dict)
-    meta = Column(MutableDict.as_mutable(JSONB), nullable=True, default=dict)
-    data = Column(MutableDict.as_mutable(JSONB), nullable=True, default=dict)
+    artifact = Column(JSONB, nullable=True, default=dict)
+    meta = Column(JSONB, nullable=True, default=dict)
+    data = Column(JSONB, nullable=True, default=dict)
