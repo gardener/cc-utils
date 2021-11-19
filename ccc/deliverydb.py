71c27302c4a0dbc8c6d128390023b8ebee74baff
zkdev <dev@zeekay.dev>
2021-11-19 14:21:23 +0100

    Add no-orm db client

--- ccc/deliverydb.py
+++ ccc/deliverydb.py
@@ -1,18 +1,32 @@
-from deliverydb.db import DeliveryDB
+import typing
+
+from deliverydb.db import DeliveryDB, delivery_db_no_orm
 import model.compliancedb
+
+import psycopg
 
 
 def default_with_db_cfg(
     db_cfg: model.compliancedb.ComplianceDbConfig,
     hostname: str = None,
-) -> DeliveryDB:
+    orm: bool = True,
+) -> typing.Union[DeliveryDB, psycopg.Connection]:
 
     if not hostname:
         hostname = db_cfg.hostname()
 
-    return DeliveryDB(
-        username=db_cfg.credentials().username(),
-        password=db_cfg.credentials().password(),
-        hostname=hostname,
-        port=db_cfg.port(),
-    )
+    if orm:
+        return DeliveryDB(
+            username=db_cfg.credentials().username(),
+            password=db_cfg.credentials().password(),
+            hostname=hostname,
+            port=db_cfg.port(),
+        )
+
+    else:
+        return delivery_db_no_orm(
+            username=db_cfg.credentials().username(),
+            password=db_cfg.credentials().password(),
+            hostname=hostname,
+            port=db_cfg.port(),
+        )
