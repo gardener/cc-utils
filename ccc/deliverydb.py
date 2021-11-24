d3c729630314114dba4abad0a20db558f552a6fa
zkdev <dev@zeekay.dev>
2021-11-24 12:19:06 +0100

    Use a common db url fac

--- ccc/deliverydb.py
+++ ccc/deliverydb.py
@@ -7,19 +7,18 @@ import model.compliancedb
 def sqlalchemy_with_db_cfg(
     db_cfg: model.compliancedb.ComplianceDbConfig,
     overwrite_hostname: str = None,
+    overwrite_username: str = None,
+    overwrite_password: str = None,
+    overwrite_port: int = None,
 ) -> deliverydb.db.DeliveryDB:
 
-    if overwrite_hostname:
-        db_url = deliverydb.db.database_connection_url_from_custom(
-            hostname=overwrite_hostname,
-            username=db_cfg.credentials().username(),
-            password=db_cfg.credentials().password(),
-            port=db_cfg.port(),
-        )
-    else:
-        db_url = deliverydb.db.database_conncetion_url_from_cfg(
-            db_cfg=db_cfg,
-        )
+    db_url = deliverydb.db.database_conncetion_url_from_cfg(
+        db_cfg=db_cfg,
+        overwrite_hostname=overwrite_hostname,
+        overwrite_username=overwrite_username,
+        overwrite_password=overwrite_password,
+        overwrite_port=overwrite_port,
+    )
 
     return deliverydb.db.DeliveryDB(
         db_conn_url=db_url,
@@ -29,20 +28,18 @@ def sqlalchemy_with_db_cfg(
 def psycopg_with_db_cfg(
     db_cfg: model.compliancedb.ComplianceDbConfig,
     overwrite_hostname: str = None,
+    overwrite_username: str = None,
+    overwrite_password: str = None,
+    overwrite_port: int = None,
 ) -> deliverydb.db.DeliveryDB:
 
-    if overwrite_hostname:
-        db_url = deliverydb.db.database_connection_url_from_custom(
-            hostname=overwrite_hostname,
-            username=db_cfg.credentials().username(),
-            password=db_cfg.credentials().password(),
-            port=db_cfg.port(),
-        )
-
-    else:
-        db_url = deliverydb.db.database_conncetion_url_from_cfg(
-            db_cfg=db_cfg,
-        )
+    db_url = deliverydb.db.database_conncetion_url_from_cfg(
+        db_cfg=db_cfg,
+        overwrite_hostname=overwrite_hostname,
+        overwrite_username=overwrite_username,
+        overwrite_password=overwrite_password,
+        overwrite_port=overwrite_port,
+    )
 
     return psycopg.connect(
         conninfo=db_url,
