e12303c1193790cd4f3068aca6eb7dda40d1adb7
jensh007 <j.huebel@sap.com>
2021-12-14 16:50:43 +0100

    Add config for Azure Community Gallery

--- model/azure.py
+++ model/azure.py
@@ -57,6 +57,9 @@ class AzureStorageAccountCfg(NamedModelElement):
     def container_name(self):
         return self.raw['container_name']
 
+    def container_name_sig(self):
+        return self.raw['container_name_sig']
+
     def _required_attributes(self):
         yield from super()._required_attributes()
         yield from [
@@ -68,3 +71,52 @@ class AzureStorageAccountCfg(NamedModelElement):
 
 # XXX export previous name for - temporary - backwards compatibility
 StorageAccountConfig = AzureStorageAccountCfg
+
+
+class AzureSharedGalleryCfg(NamedModelElement):
+    '''
+    Not intended to be instantiated by users of this module
+    '''
+
+    def resource_group_name(self):
+        return self.raw['resource_group_name']
+
+    def gallery_name(self):
+        return self.raw['gallery_name']
+
+    def location(self):
+        return self.raw['location']
+
+    def published_name(self):
+        return self.raw['published_name']
+
+    def description(self):
+        return self.raw['description']
+
+    def eula(self):
+        return self.raw['eula']
+
+    def release_note_uri(self):
+        return self.raw['release_note_uri']
+
+    def identifier_publisher(self):
+        return self.raw['identifier_publisher']
+
+    def identifier_offer(self):
+        return self.raw['identifier_offer']
+
+    def identifier_sku(self):
+        return self.raw['identifier_sku']
+
+    def _required_attributes(self):
+        yield from super()._required_attributes()
+        yield from [
+            'resource_group_name',
+            'gallery_name',
+            'location',
+            'published_name',
+        ]
+
+
+# XXX export previous name for - temporary - backwards compatibility
+SharedGalleryCfg = AzureSharedGalleryCfg
