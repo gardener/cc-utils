67e0e0aa225bd4835872d3c3f5533bba0d469d0c
Johannes Krayl <johannes.krayl@sap.com>
2022-01-25 14:19:03 +0100

    refactor cfg_mgmt cli

--- cfg_mgmt/util.py
+++ cfg_mgmt/util.py
@@ -1,8 +1,53 @@
+import os.path
 import typing
 
 import cfg_mgmt.model as cmm
 import cfg_mgmt.reporting as cmr
+import ci.util
 import model
+
+
+def generate_cfg_element_status_reports(cfg_dir: str) -> list[cmr.CfgElementStatusReport]:
+    ci.util.existing_dir(cfg_dir)
+
+    cfg_factory = model.ConfigFactory._from_cfg_dir(
+        cfg_dir,
+        disable_cfg_element_lookup=True,
+    )
+
+    policies = cmm.cfg_policies(
+        policies=cmm._parse_cfg_policies_file(
+            path=os.path.join(cfg_dir, cmm.cfg_policies_fname),
+        )
+    )
+    rules = cmm.cfg_rules(
+        rules=cmm._parse_cfg_policies_file(
+            path=os.path.join(cfg_dir, cmm.cfg_policies_fname),
+        )
+    )
+    statuses = cmm.cfg_status(
+        status=cmm._parse_cfg_status_file(
+            path=os.path.join(cfg_dir, cmm.cfg_status_fname),
+        )
+    )
+    responsibles = cmm.cfg_responsibles(
+        responsibles=cmm._parse_cfg_responsibles_file(
+            path=os.path.join(cfg_dir, cmm.cfg_responsibles_fname),
+        )
+    )
+
+    return [
+        determine_status(
+            element=element,
+            policies=policies,
+            rules=rules,
+            statuses=statuses,
+            responsibles=responsibles,
+            element_storage=cfg_dir,
+        ) for element in iter_cfg_elements(cfg_factory=cfg_factory)
+    ]
+
+
 
 
 def iter_cfg_elements(
