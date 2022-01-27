64e95ad150440de7dfcf0a39bbf1baa15105c975
Christian Cwienk <christian.cwienk@sap.com>
2022-01-27 09:43:22 +0100

    optionally filter by responsibles

--- cli/gardener_ci/_cfg_mgmt.py
+++ cli/gardener_ci/_cfg_mgmt.py
@@ -1,12 +1,40 @@
+import re
+
 import cfg_mgmt.reporting as cmr
 import cfg_mgmt.util as cmu
 
 __cmd_name__ = 'cfg_mgmt'
 
 
-def report(cfg_dir: str):
+def report(
+    cfg_dir: str,
+    responsible_names: [str]=[]
+):
     status_reports = cmu.generate_cfg_element_status_reports(cfg_dir)
 
+    def matches_any(name: str):
+        for responsible_name in responsible_names:
+            if re.fullmatch(responsible_name, name):
+                return True
+        return False
+
+    def filtered_reports(responsible_names):
+        if not responsible_names:
+            return status_reports
+
+        for report in status_reports:
+            if not (responsible_mapping := report.responsible):
+                continue
+
+            for responsible in responsible_mapping.responsibles:
+                if matches_any(name=responsible.name):
+                    break
+            else:
+                continue
+            yield report
+
+    reports = filtered_reports(responsible_names=responsible_names)
+
     cmr.create_report(
-        cfg_element_statuses=status_reports,
+        cfg_element_statuses=reports,
     )
