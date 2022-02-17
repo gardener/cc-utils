3f82e8816c97f9578f3eecff301fa6166e69ebc3
zkdev <dev@zeekay.dev>
2022-02-17 12:19:34 +0100

    Process config queue

--- cfg_mgmt/rotate.py
+++ cfg_mgmt/rotate.py
@@ -1,6 +1,12 @@
+import dataclasses
 import logging
+import os
 import typing
+import yaml
 
+import traceback
+
+import ccc.gcp
 import cfg_mgmt.gcp as cmg
 import cfg_mgmt.github as cmgh
 import cfg_mgmt.model as cmm
@@ -11,6 +17,90 @@ import oci.model as om
 
 
 logger = logging.getLogger(__name__)
+
+
+def write_config_queue(
+    cfg_queue: typing.Iterable[cmm.CfgQueueEntry],
+    cfg_queue_file_path: str,
+):
+    with open(cfg_queue_file_path, 'w') as f:
+        yaml.dump(
+            {
+                'rotation_queue': [
+                    dataclasses.asdict(cfg_queue_entry)
+                    for cfg_queue_entry in cfg_queue
+                ]
+            },
+            f,
+        )
+
+
+def push_config_queue(
+    git_helper: gitutil.GitHelper,
+    target_ref: str,
+    type_name: str,
+    name: str,
+):
+    git_helper.add_and_commit(
+        message=f'process config queue for {type_name}/{name}',
+    )
+    try:
+        git_helper.push('@', target_ref)
+    except:
+        logger.warning('failed to push processed config queue - reverting')
+        git_helper.repo.git.reset('--hard', '@~')
+
+
+def delete_cfg_element(
+    cfg_dir: str,
+    cfg_queue_entry: cmm.CfgQueueEntry,
+    cfg_fac: model.ConfigFactory,
+    cfg_metadata: cmm.CfgMetadata,
+    git_helper: gitutil.GitHelper,
+    target_ref: str,
+) -> bool:
+    did_remove = False
+    if cfg_queue_entry.target.type == 'container_registry':
+        cfg_element = cfg_fac.container_registry(cfg_queue_entry.target.name)
+        if cfg_element.registry_type() == om.OciRegistryType.GCR:
+            logger.info('deleting old gcr secret')
+            iam_client = ccc.gcp.create_iam_client(
+                cfg_element=cfg_element,
+            )
+            try:
+                cmg.delete_service_account_key(
+                    iam_client=iam_client,
+                    service_account_key_name=cfg_queue_entry.secretId['gcp_secret_key'],
+                    absent_ok=True,
+                )
+                did_remove = True
+            except:
+                logger.warning(f'deleting {cfg_queue_entry.secretId["gcp_secret_key"]} failed')
+                traceback.print_exc()
+        else:
+            logger.warning(
+                f'{cfg_element.registry_type()} is not (yet) supported for automated deletion'
+            )
+    else:
+        logger.warning(f'{cfg_queue_entry.target.type} is not (yet) supported for automated delete')
+
+    if did_remove:
+        new_cfg_queue: typing.List[cmm.CfgQueueEntry] = [
+            entry for entry in cfg_metadata.queue
+            if not entry == cfg_queue_entry
+        ]
+        write_config_queue(
+            cfg_queue=new_cfg_queue,
+            cfg_queue_file_path=os.path.join(cfg_dir, cmm.cfg_queue_fname),
+        )
+        push_config_queue(
+            git_helper=git_helper,
+            target_ref=target_ref,
+            type_name=cfg_element._type_name,
+            name=cfg_element.name()
+        )
+
+    return did_remove
 
 
 def rotate_cfg_element(
