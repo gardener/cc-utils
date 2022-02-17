859057ce5de4cd9051d6ecb0d681b49462090be0
zkdev <dev@zeekay.dev>
2022-02-17 12:23:05 +0100

    Add config_queue processing to cli

--- cli/gardener_ci/_cfg_mgmt.py
+++ cli/gardener_ci/_cfg_mgmt.py
@@ -6,7 +6,9 @@ import cfg_mgmt.reporting as cmr
 import cfg_mgmt.rotate
 import cfg_mgmt.model
 import cfg_mgmt.util as cmu
+import gitutil
 import model
+
 
 __cmd_name__ = 'cfg_mgmt'
 logger = logging.getLogger(__name__)
@@ -45,6 +47,65 @@ def report(
 
     for _ in cmr.create_report(cfg_element_statuses=reports):
         pass
+
+
+def process_config_queue(
+    cfg_dir: str,
+    github_cfg: str, # e.g. github_wdf_sap_corp
+    cfg_repo_path: str, # e.g. kubernetes/cc-config
+    type_name: str=None,
+    name: str=None,
+    tgt_ref: str='refs/heads/master',
+):
+    '''
+    Iterates to be deleted cfg_queue_entries with optional cfg_target filter.
+    Processes very first supported entry and terminates.
+    '''
+
+    # ensure cfg_target filter is correct (None or name and type_name)
+    if not type_name and not name:
+        cfg_target = None
+    elif not type_name or not name:
+        logger.error('when specifying cfg_target filter, both name and type_name must be given')
+        return
+
+    cfg_metadata = cfg_mgmt.model.cfg_metadata_from_cfg_dir(cfg_dir=cfg_dir)
+    cfg_factory = model.ConfigFactory.from_cfg_dir(
+        cfg_dir=cfg_dir,
+        disable_cfg_element_lookup=True,
+    )
+    github_cfg = cfg_factory.github(github_cfg)
+    git_helper = gitutil.GitHelper(
+        repo=cfg_dir,
+        github_cfg=github_cfg,
+        github_repo_path=cfg_repo_path,
+    )
+
+    cfg_target = cfg_mgmt.model.CfgTarget(
+        type=type_name,
+        name=name,
+    )
+
+    # cfg_queue_entries cannot be select with much comfort as cli argument.
+    # Therefore, as in pipeline case, they are iterated and the first candidate
+    # is processed.
+    for cfg_queue_entry in cmu.iter_cfg_queue_entries_to_be_deleted(
+        cfg_metadata=cfg_metadata,
+        cfg_target=cfg_target,
+    ):
+        if not cfg_mgmt.rotate.delete_cfg_element(
+            cfg_dir=cfg_dir,
+            cfg_queue_entry=cfg_queue_entry,
+            cfg_fac=cfg_factory,
+            cfg_metadata=cfg_metadata,
+            git_helper=git_helper,
+            target_ref=tgt_ref,
+        ):
+            continue
+
+        # stop after first successful rotation (avoid causing too much trouble at one time
+        return
+    logger.info('no to be deleted config queue entry found')
 
 
 def rotate(
