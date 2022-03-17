bd15ccf6fa2c56dd3e66a2f7ffdd841d77176026
zkdev <dev@zeekay.dev>
2022-03-17 11:16:31 +0100

    Mk cc main team cfg required

--- model/concourse.py
+++ model/concourse.py
@@ -50,7 +50,7 @@ class ConcourseConfig(NamedModelElement):
     '''
 
     def concourse_main_team_config(self):
-        return self.raw.get('concourse_main_team_config')
+        return self.raw['concourse_main_team_config']
 
     def external_url(self):
         return self.raw.get('externalUrl')
@@ -136,6 +136,7 @@ class ConcourseConfig(NamedModelElement):
             'helm_chart_version',
             'helm_chart_values',
             'oauth_config_name',
+            'concourse_main_team_config',
         ]
 
     def _optional_attributes(self):
