4723d314d47df0345e1dc5742ad314dede58adcc
zkdev <dev@zeekay.dev>
2022-06-07 16:22:58 +0200

    Update compliance model - each compliance data (greatestCve, OsID, ...) receives its own   dataclass - decouple datatype (e.g. licenses/aggregated) and datasource (e.g.   BDBA) - explicitly use ocm artefact fields rather than whole model class   (sent to delivery-service, shrink to minimum + prevent enum   serialisation)
    Co-authored-by: Christian Cwienk <christian.cwienk@sap.com>

--- dso/model.py
+++ dso/model.py
@@ -22,56 +22,125 @@ class ScanArtifact:
 
 class Datasource:
     WHITESOURCE = 'whitesource'
-    PROTECODE = 'protecode'
+    BDBA = 'bdba' # formerly protecode
     CHECKMARX = 'checkmarx'
     CLAMAV = 'clamav'
+    CC_UTILS = 'cc-utils'
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
+class Artefact:
+    artefact_name: str
+    artefact_version: str
+    artefact_type: str
+    artefact_extra_id: dict
+
+
+@dataclasses.dataclass(frozen=True)
 class ArtefactReference:
-    componentName: str
-    componentVersion: str
-    artefact: typing.Union[
-        cm.Resource,
-        cm.ComponentSource,
-    ]
+    component_name: str
+    component_version: str
+    artefact: Artefact
 
 
-@dataclasses.dataclass
-class ComplianceMetadata:
+def artefact_ref_from_ocm(
+    component: cm.Component,
+    artefact: cm.Resource | cm.ComponentSource
+) -> ArtefactReference:
+    artefact = Artefact(
+        artefact_name=artefact.name,
+        artefact_version=artefact.version,
+        artefact_type=artefact.type,
+        artefact_extra_id=artefact.extraIdentity,
+    )
+    return ArtefactReference(
+        component_name=component.name,
+        component_version=component.version,
+        artefact=artefact,
+    )
+
+
+class Datatype:
+    VULNERABILITIES_AGGREGATED = 'vulnerabilities/aggregated'
+    VULNERABILITIES_RAW = 'vulnerabilities/raw'
+    MALWARE_RAW = 'malware/raw'
+    LICENSES_AGGREGATED = 'licenses/aggregated'
+    COMPONENTS_BDBA = 'components/bdba'
+    OS_IDS_RAW = 'os_ids/raw'
+
+
+@dataclasses.dataclass(frozen=True)
+class Metadata:
     datasource: str
-    creationDate: typing.Union[datetime.datetime, str]
+    type: str
+    creation_date: datetime.datetime
 
 
-@dataclasses.dataclass
-class ComplianceData:
+@dataclasses.dataclass(frozen=True)
+class GreatestCVE:
+    greatestCvss3Score: typing.Optional[float]
+    reportUrl: str
+
+
+@dataclasses.dataclass(frozen=True)
+class FindingMeta:
+    scanned_octets: int
+    receive_duration_seconds: float
+    scan_duration_seconds: float
+
+
+@dataclasses.dataclass(frozen=True)
+class Finding:
+    result: str
+    message: str
+    details: typing.Optional[str]
+    meta: FindingMeta
+
+
+@dataclasses.dataclass(frozen=True)
+class ClamavFinding:
+    findings: list[Finding]
+
+
+@dataclasses.dataclass(frozen=True)
+class OsInfo:
+    name: str
+    id: str
+    pretty_name: str
+    cpe_name: str
+    variant: str
+    variant_id: str
+    version: str
+    version_id: str
+    version_codename: str
+    build_id: str
+    image_id: str
+    image_version: str
+
+
+@dataclasses.dataclass(frozen=True)
+class OsID:
+    osInfo: OsInfo
+
+
+@dataclasses.dataclass(frozen=True)
+class License:
+    licenses: list[str]
+
+
+@dataclasses.dataclass(frozen=True)
+class ComponentVersion:
+    name: str
+    version: str
+
+
+@dataclasses.dataclass(frozen=True)
+class Component:
+    components: list[ComponentVersion]
+
+
+@dataclasses.dataclass(frozen=True)
+class ArtefactMetadata:
     artefact: ArtefactReference
-    meta: ComplianceMetadata
-    data: dict
-
-    @staticmethod
-    def create(
-        artefact: typing.Union[cm.Resource, cm.SourceReference],
-        component: cm.Component,
-        type: str,
-        data: dict,
-    ):
-        '''
-        convenient method to create ComplianceData
-        type: metadata type (implies expected data structure)
-        data: type-specific compliance data
-        '''
-        ar = ArtefactReference(
-            componentName=component.name,
-            componentVersion=component.version,
-            artefact=artefact,
-        )
-        cm = ComplianceMetadata(
-            datasource=type,
-            creationDate=datetime.datetime.now().isoformat(),
-        )
-        return ComplianceData(
-            artefact=ar,
-            data=data,
-            meta=cm
-        )
+    meta: Metadata
+    data: GreatestCVE | License | Component | OsID | dict
