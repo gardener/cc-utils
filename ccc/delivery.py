65609c96b1407f9d9b5ac61745924943f48b2fcf
Christian Cwienk <christian.cwienk@sap.com>
2022-06-08 16:48:40 +0200

    add factory-function for delivery-client w/ explicit cfg

--- ccc/delivery.py
+++ ccc/delivery.py
@@ -34,6 +34,26 @@ def default_client_if_available(cfg_set=None):
         logger.warning('unable to build delivery client')
 
 
+def client(
+    cfg_name: str=None,
+    cfg_factory=None,
+) -> delivery.client.DeliveryServiceClient:
+    if not cfg_factory:
+        cfg_factory = ctx.cfg_factory()
+
+    if not cfg_name:
+        if delivery_client := default_client_if_available():
+            return delivery_client
+        raise ValueError('no (default) delivery-client could be determined - pass cfg_name')
+
+    delivery_endpoints = cfg_factory.delivery_endpoints(cfg_name)
+    routes = delivery.client.DeliveryServiceRoutes(
+        base_url=delivery_endpoints.base_url(),
+    )
+
+    return delivery.client.DeliveryServiceClient(routes=routes)
+
+
 def endpoints(cfg_set=None):
     if not ci.util._running_on_ci() and not cfg_set:
         return None
