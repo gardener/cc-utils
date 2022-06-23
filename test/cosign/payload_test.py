d24c54e00d97eabad98efa40477e0f6d5157b63d
Johannes Schicktanz <johannes.schicktanz@sap.com>
2022-06-23 08:26:41 +0200

    review feedback

--- test/cosign/payload_test.py
+++ test/cosign/payload_test.py
@@ -13,50 +13,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
+import pytest
 
 import cosign.payload as cp
 
 
-class PayloadTest(unittest.TestCase):
-    def test_json_marshaling_with_annotations(self):
-        expected_json = '{"critical":{"identity":{"docker-reference":"eu.gcr.io/test/img"},' \
-            '"image":{"docker-manifest-digest":' \
-            '"sha256:a904c847d39ae82ec8859ce623ae14ccbfff36d53ce1490b43d9bf5caa47f33b"},' \
-            '"type":"cosign container image signature"},"optional":{"key":"val"}}'
+def test_json_marshaling_with_annotations():
+    expected_json = '{"critical":{"identity":{"docker-reference":"eu.gcr.io/test/img"},' \
+        '"image":{"docker-manifest-digest":' \
+        '"sha256:a904c847d39ae82ec8859ce623ae14ccbfff36d53ce1490b43d9bf5caa47f33b"},' \
+        '"type":"cosign container image signature"},"optional":{"key":"val"}}'
 
-        img_ref = 'eu.gcr.io/test/img@' \
-            'sha256:a904c847d39ae82ec8859ce623ae14ccbfff36d53ce1490b43d9bf5caa47f33b'
-        annotations = {
-            "key": "val",
-        }
+    img_ref = 'eu.gcr.io/test/img@' \
+        'sha256:a904c847d39ae82ec8859ce623ae14ccbfff36d53ce1490b43d9bf5caa47f33b'
+    annotations = {
+        "key": "val",
+    }
 
-        payload = cp.Payload(
-            image_ref=img_ref,
-            annotations=annotations,
-        )
+    payload = cp.Payload(
+        image_ref=img_ref,
+        annotations=annotations,
+    )
 
-        actual_json = payload.json()
+    actual_json = payload.json()
 
-        self.assertEqual(actual_json, expected_json)
+    assert actual_json == expected_json
 
-    def test_json_marshaling_without_annotations(self):
-        expected_json = '{"critical":{"identity":{"docker-reference":"eu.gcr.io/test/img"},' \
-            '"image":{"docker-manifest-digest":' \
-            '"sha256:a904c847d39ae82ec8859ce623ae14ccbfff36d53ce1490b43d9bf5caa47f33b"},' \
-            '"type":"cosign container image signature"},"optional":null}'
 
-        img_ref = 'eu.gcr.io/test/img@' \
-            'sha256:a904c847d39ae82ec8859ce623ae14ccbfff36d53ce1490b43d9bf5caa47f33b'
+def test_json_marshaling_without_annotations():
+    expected_json = '{"critical":{"identity":{"docker-reference":"eu.gcr.io/test/img"},' \
+        '"image":{"docker-manifest-digest":' \
+        '"sha256:a904c847d39ae82ec8859ce623ae14ccbfff36d53ce1490b43d9bf5caa47f33b"},' \
+        '"type":"cosign container image signature"},"optional":null}'
 
-        payload = cp.Payload(
-            image_ref=img_ref,
-        )
+    img_ref = 'eu.gcr.io/test/img@' \
+        'sha256:a904c847d39ae82ec8859ce623ae14ccbfff36d53ce1490b43d9bf5caa47f33b'
 
-        actual_json = payload.json()
+    payload = cp.Payload(
+        image_ref=img_ref,
+    )
 
-        self.assertEqual(actual_json, expected_json)
+    actual_json = payload.json()
 
-    def test_raise_error_for_img_ref_without_digest(self):
-        img_ref = 'eu.gcr.io/test/img:1.0.0'
-        self.assertRaises(ValueError, cp.Payload, self, img_ref)
+    assert actual_json == expected_json
+
+
+def test_raise_error_for_img_ref_without_digest():
+    img_ref = 'eu.gcr.io/test/img:1.0.0'
+    with pytest.raises(ValueError):
+        cp.Payload(image_ref=img_ref)
