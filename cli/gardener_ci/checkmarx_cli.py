5b4b7b226e7bfd78708d0faf9ea23fdb69fa2cba
jensh007 <j.huebel@sap.com>
2022-07-01 10:57:11 +0200

    checkmarx: add force flag, bug-fixing, remove email-notification

--- cli/gardener_ci/checkmarx_cli.py
+++ cli/gardener_ci/checkmarx_cli.py
@@ -6,14 +6,15 @@ def upload_and_scan_from_component_descriptor(
         checkmarx_cfg_name: str,
         component_descriptor_path: str,
         team_id: str=None,
+        force: bool=False,
 ):
     component_descriptor = cdu.component_descriptor_from_component_descriptor_path(
         cd_path=component_descriptor_path,
     )
 
-    concourse.steps.scan_sources.scan_sources_and_notify(
+    concourse.steps.scan_sources.scan_sources(
         checkmarx_cfg_name=checkmarx_cfg_name,
         team_id=team_id,
         component_descriptor=component_descriptor,
-        email_recipients=None
+        force=force,
     )
