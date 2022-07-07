34c064bbb1b7d0d9f13ee484a56f0415d021f38e
Andreas Burger <andreas.burger@sap.com>
2022-07-07 13:29:53 +0200

    update tarfile-generator-function
    it now also allows to pass an iterator of source-tarfiles. In that
    case the result is a tarfile with the combined contents of all
    source-tarfiles.

--- tarutil.py
+++ tarutil.py
@@ -21,14 +21,14 @@ class _FilelikeProxy:
 
 
 def filtered_tarfile_generator(
-    src_tf: tarfile.TarFile,
+    src_tf: tarfile.TarFile | typing.Iterable[tarfile.TarFile],
     filter_func: typing.Callable[[tarfile.TarInfo], bool]=lambda tarinfo: True,
     chunk_size=tarfile.BLOCKSIZE,
     chunk_callback: typing.Callable[[bytes], None]=None,
 ) -> typing.Generator[bytes, None, None]:
     '''
     returns a generator yielding a tarfile that will by default contain the same members as
-    the passed tarfile (src_tf). If a filter-function is given, any entries (TarInfo objects)
+    the passed tarfile(s) (src_tf). If a filter-function is given, any entries (TarInfo objects)
     for which this function will return a "falsy" value will be removed from the resulting
     tarfile stream (which is the actual value-add from this function).
 
@@ -41,64 +41,89 @@ def filtered_tarfile_generator(
     '''
     offset = 0
 
-    for member in src_tf:
-        if not filter_func(member):
-            logger.debug(f'filtered out {member=}')
-            continue
+    def filter_tarfile(
+        src_tf: tarfile.TarFile,
+        filter_func: typing.Callable[[tarfile.TarInfo], bool],
+        chunk_size,
+        chunk_callback: typing.Callable[[bytes], None],
+    ):
+        nonlocal offset
+        for member in src_tf:
+            if not filter_func(member):
+                logger.debug(f'filtered out {member=}')
+                continue
 
-        # need to create a cp (to patch offsets w/o modifying original members, which would
-        # break accessing file-contents)
-        member_raw = member.tobuf()
-        if len(member_raw) > tarfile.BLOCKSIZE:
-            member_info = member.get_info()
-            member_info['offset'] = offset
-            member_info['offset_data'] = offset + len(member_raw)
+            # need to create a cp (to patch offsets w/o modifying original members, which would
+            # break accessing file-contents)
+            member_raw = member.tobuf()
+            if len(member_raw) > tarfile.BLOCKSIZE:
+                member_info = member.get_info()
+                member_info['offset'] = offset
+                member_info['offset_data'] = offset + len(member_raw)
 
-            member_buf = member.create_pax_header(
-                info=member.get_info(),
-                encoding=tarfile.ENCODING
-            )
-        else:
-            member_cp = tarfile.TarInfo.frombuf(
-                member_raw,
-                encoding=tarfile.ENCODING,
-                errors='surrogateescape',
-            )
-            member_cp.offset = offset
-            member_cp.offset_data = offset + len(member_raw)
-
-            member_buf = member_cp.tobuf()
-
-        if chunk_callback:
-            chunk_callback(member_buf)
-        yield member_buf
-        offset += tarfile.BLOCKSIZE
-
-        if member.size > 0:
-            if member.isfile():
-                fobj = src_tf.extractfile(member=member)
-                octets_sent = 0
-                octets_left = member.size
-                while octets_left and (chunk := fobj.read(min(octets_left, chunk_size))):
-                    offset += (leng := len(chunk))
-                    octets_sent += leng
-                    octets_left -= leng
-                    if chunk_callback:
-                        chunk_callback(chunk)
-                    yield chunk
-
-                # pad to full 512-blocks if member is not "aligned"
-                if member.size % tarfile.BLOCKSIZE == 0:
-                    continue
-                if (missing := tarfile.BLOCKSIZE - (octets_sent % tarfile.BLOCKSIZE)):
-                    padding = tarfile.NUL * missing
-                    if chunk_callback:
-                        chunk_callback(padding)
-                    yield padding
-                    offset += missing
+                member_buf = member.create_pax_header(
+                    info=member.get_info(),
+                    encoding=tarfile.ENCODING
+                )
             else:
-                # TODO: handle symlinks (will not work for streaming-mode)
-                raise NotImplementedError(member.type)
+                member_cp = tarfile.TarInfo.frombuf(
+                    member_raw,
+                    encoding=tarfile.ENCODING,
+                    errors='surrogateescape',
+                )
+                member_cp.offset = offset
+                member_cp.offset_data = offset + len(member_raw)
+
+                member_buf = member_cp.tobuf()
+
+            if chunk_callback:
+                chunk_callback(member_buf)
+            yield member_buf
+            offset += tarfile.BLOCKSIZE
+
+            if member.size > 0:
+                if member.isfile():
+                    fobj = src_tf.extractfile(member=member)
+                    octets_sent = 0
+                    octets_left = member.size
+                    while octets_left and (chunk := fobj.read(min(octets_left, chunk_size))):
+                        offset += (leng := len(chunk))
+                        octets_sent += leng
+                        octets_left -= leng
+                        if chunk_callback:
+                            chunk_callback(chunk)
+                        yield chunk
+
+                    # pad to full 512-blocks if member is not "aligned"
+                    if member.size % tarfile.BLOCKSIZE == 0:
+                        continue
+                    if (missing := tarfile.BLOCKSIZE - (octets_sent % tarfile.BLOCKSIZE)):
+                        padding = tarfile.NUL * missing
+                        if chunk_callback:
+                            chunk_callback(padding)
+                        yield padding
+                        offset += missing
+                else:
+                    # TODO: handle symlinks (will not work for streaming-mode)
+                    raise NotImplementedError(member.type)
+
+    if isinstance(src_tf, tarfile.TarFile):
+        yield from filter_tarfile(
+            src_tf=src_tf,
+            filter_func=filter_func,
+            chunk_size=chunk_size,
+            chunk_callback=chunk_callback,
+        )
+    elif isinstance(src_tf, typing.Iterable):
+        for tf in src_tf:
+            yield from filter_tarfile(
+                src_tf=tf,
+                filter_func=filter_func,
+                chunk_size=chunk_size,
+                chunk_callback=chunk_callback,
+            )
+    else:
+        raise ValueError(src_tf)
 
     final_padding = 2 * tarfile.BLOCKSIZE * tarfile.NUL # tarfiles should end w/ two empty blocks
     yield final_padding
