24b784e6e8bc746ca2f77f294645c3b583037447
Christian Cwienk <christian.cwienk@sap.com>
2022-08-12 11:57:11 +0200

    refactoring (preparation): recursive node-iterator for ocm-components

---
+++ cli/gardener_ci/ocm.py
@@ -0,0 +1,47 @@
+import gci.componentmodel as cm
+
+import cnudie.retrieve
+import cnudie.iter
+import ctx
+
+_cfg = ctx.cfg
+
+
+def traverse(
+    name: str,
+    version: str,
+    ctx_base_url: str=None,
+):
+    if not ctx_base_url:
+        ctx_base_url = _cfg.ctx.ocm_repo_base_url
+
+    ctx_repo = cm.OciRepositoryContext(
+            baseUrl=ctx_base_url,
+            componentNameMapping=cm.OciComponentNameMapping.URL_PATH,
+    )
+
+    component_descriptor = cnudie.retrieve.component_descriptor(
+        name=name,
+        version=version,
+        ctx_repo=ctx_repo,
+    )
+    component = component_descriptor.component
+
+    lookup = cnudie.iter.dictbased_lookup(
+        components=cnudie.retrieve.components(
+            component=component,
+        )
+    )
+
+    for node in cnudie.iter.iter(
+        component=component,
+        lookup=lookup,
+    ):
+        indent = len(node.path * 2)
+        if isinstance(node, cnudie.iter.ComponentNode):
+            prefix = 'c'
+            print(f'{prefix}{" " * indent}{node.component.name}')
+        if isinstance(node, cnudie.iter.ResourceNode):
+            prefix = 'r'
+            indent += 1
+            print(f'{prefix}{" " * indent}{node.resource.name}')
---
+++ cnudie/iter.py
@@ -0,0 +1,116 @@
+import dataclasses
+import typing
+
+import gci.componentmodel as cm
+
+
+ComponentLookupById = typing.Callable[[cm.ComponentIdentity], cm.Component]
+
+
+def dictbased_lookup(components: typing.Iterable[ComponentLookupById]):
+    lookup_dict = {c.identity(): c for c in components}
+
+    def lookup(component_id: cm.ComponentIdentity):
+        return lookup_dict[component_id]
+
+    return lookup
+
+
+@dataclasses.dataclass
+class Node:
+    path: tuple[cm.Component]
+
+    @property
+    def component(self):
+        return self.path[-1]
+
+    @property
+    def component_id(self):
+        return self.component.identity()
+
+
+@dataclasses.dataclass
+class ComponentNode(Node):
+    pass
+
+
+@dataclasses.dataclass
+class ResourceNode(Node):
+    resource: cm.Resource
+
+
+class Filter:
+    @staticmethod
+    def components(node: Node):
+        return isinstance(node, ComponentNode)
+
+    @staticmethod
+    def resources(node: Node):
+        return isinstance(node, ResourceNode)
+
+
+def iter(
+    component: cm.Component,
+    lookup: ComponentLookupById,
+    prune_unique: bool=True,
+    node_filter: typing.Callable[[Node], bool]=None
+):
+    '''
+    returns a generator yielding the transitive closure of nodes accessible from the given component.
+
+    See `cnudie.retrieve` for retrieving components/component descriptors.
+
+    @param component:    root component for iteration
+    @param lookup:       used to lookup referenced components
+                         (thus abstracting from retrieval method)
+    @param prune_unique: if true, redundant component-versions will only be traversed once
+    @node_filter:        use to filter emitted nodes (see Filter for predefined filters)
+    '''
+    seen_component_ids = set()
+
+    # need to nest actual iterator to keep global state of seen component-IDs
+    def inner_iter(
+        component: cm.Component,
+        lookup: ComponentLookupById,
+        path: tuple[cm.ComponentIdentity]=(),
+    ):
+        path = (*path, component)
+
+        yield ComponentNode(
+            path=path,
+        )
+
+        for resource in component.resources:
+            yield ResourceNode(
+                path=path,
+                resource=resource,
+            )
+
+        for cref in component.componentReferences:
+            cref_id = cm.ComponentIdentity(
+                name=cref.componentName,
+                version=cref.version,
+            )
+            referenced_component = lookup(cref_id)
+
+            yield from inner_iter(
+                component=referenced_component,
+                lookup=lookup,
+                path=path,
+            )
+
+    for node in inner_iter(
+        component=component,
+        lookup=lookup,
+        path=(),
+    ):
+        if node_filter and not node_filter(node):
+            continue
+
+        if prune_unique and isinstance(node, ComponentNode):
+            if node.component.identity() in seen_component_ids:
+                continue
+            else:
+                seen_component_ids.add(node.component_id)
+
+        yield node
