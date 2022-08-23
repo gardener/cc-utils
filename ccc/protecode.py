af195f4f71f98997b3917dc63170080d4ae71712
Christian Cwienk <christian.cwienk@sap.com>
2022-08-23 11:11:01 +0200

    fix typehint, rm dead code

--- ccc/protecode.py
+++ ccc/protecode.py
@@ -1,14 +1,15 @@
-import ci.util
 import model.protecode
 import protecode.client
 
 
 def client(
-    protecode_cfg: model.protecode.ProtecodeConfig,
+    protecode_cfg: model.protecode.ProtecodeConfig | str,
+    cfg_factory=None,
 ):
     if isinstance(protecode_cfg, str):
         import ci.util
-        cfg_factory = ci.util.ctx().cfg_factory()
+        if not cfg_factory:
+            cfg_factory = ci.util.ctx().cfg_factory()
         protecode_cfg = cfg_factory.protecode(protecode_cfg)
 
     routes = protecode.client.ProtecodeApiRoutes(base_url=protecode_cfg.api_url())
@@ -17,9 +18,3 @@ def client(
         protecode_cfg=protecode_cfg,
     )
     return api
-
-
-def client_from_config_name(protecode_cfg_name: str):
-    cfg_factory = ci.util.ctx().cfg_factory()
-    protecode_config = cfg_factory.protecode(protecode_cfg_name)
-    return client(protecode_config)
