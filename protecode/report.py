06a4660446330bbf23922a50965921220f89d909
Christian Cwienk <christian.cwienk@sap.com>
2022-11-23 23:08:58 +0100

    add rescoring-summary if it affects overall severity

--- protecode/report.py
+++ protecode/report.py
@@ -59,6 +59,28 @@ def _component_and_results_to_report_str(
     return report
 
 
+def _worst_severity_and_worst_rescored_severity(
+    result: pm.AnalysisResult,
+    rescoring_rules: typing.Iterable[dso.cvss.RescoringRule] | None=None,
+    cve_categorisation: dso.cvss.CveCategorisation | None=None,
+):
+    worst_severity = dso.cvss.CVESeverity.NONE
+    worst_rescored = dso.cvss.CVESeverity.NONE
+
+    for component in result.components():
+        for vulnerability in component.vulnerabilities():
+            orig_sev = dso.cvss.CVESeverity.from_cve_score(vulnerability.cve_severity())
+            worst_severity = max(worst_severity, orig_sev)
+
+            rescored = dso.cvss.rescore(
+                rescoring_rules=rescoring_rules,
+                severity=orig_sev,
+            )
+            worst_rescored = max(worst_rescored, rescored)
+
+    return worst_severity, worst_rescored
+
+
 def analysis_result_to_report_str(
     result: pm.AnalysisResult,
     rescoring_rules: typing.Iterable[dso.cvss.RescoringRule] | None=None,
@@ -69,7 +91,7 @@ def analysis_result_to_report_str(
         key=lambda comp_and_vulns: f'{comp_and_vulns[0].name()}:{comp_and_vulns[0].version()}',
     )
 
-    return '\n'.join((
+    report = '\n'.join((
         _component_and_results_to_report_str(
             comp,
             results,
@@ -78,3 +100,14 @@ def analysis_result_to_report_str(
         )
         for comp, results in components_and_cves
     ))
+
+    worst_severity, worst_rescored = _worst_severity_and_worst_rescored_severity(
+        result=result,
+        rescoring_rules=rescoring_rules,
+        cve_categorisation=cve_categorisation,
+    )
+
+    if worst_severity is not worst_rescored:
+        report += f'\n{worst_severity.name=} -> {worst_rescored.name=}'
+
+    return report
