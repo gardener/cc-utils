d397115698ee8e9737e5fb3b7a61f2aa2cf46f22
zkdev <dev@zeekay.dev>
2022-11-25 13:51:38 +0100

    Also accept issue template to be passed directly
    Useful for cases where no lookup is required (as only one cfg template
    is given).

--- github/compliance/report.py
+++ github/compliance/report.py
@@ -582,7 +582,7 @@ def create_or_update_github_issues(
     gh_api: github3.GitHub=None,
     overwrite_repository: github3.repos.Repository=None,
     preserve_labels_regexes: typing.Iterable[str]=(),
-    github_issue_template_cfgs: list[image_scan.GithubIssueTemplateCfg]=None,
+    github_issue_template_cfgs: list[image_scan.GithubIssueTemplateCfg] | str=None,
     delivery_svc_client: delivery.client.DeliveryServiceClient=None,
     delivery_svc_endpoints: model.delivery.DeliveryEndpointsCfg=None,
     license_cfg: image_scan.LicenseCfg=None, # XXX -> callback
@@ -687,13 +687,19 @@ def create_or_update_github_issues(
                 license_cfg=license_cfg,
                 delivery_dashboard_url=delivery_dashboard_url,
             )
-            for issue_cfg in github_issue_template_cfgs:
-                if issue_cfg.type == issue_type:
-                    break
-            else:
-                raise ValueError(f'no template for {issue_type=}')
 
-            body = issue_cfg.body.format(**template_variables)
+            if isinstance(github_issue_template_cfgs, str):
+                template_body = github_issue_template_cfgs
+
+            else:
+                for issue_cfg in github_issue_template_cfgs:
+                    if issue_cfg.type == issue_type:
+                        template_body = issue_cfg.body
+                        break
+                else:
+                    raise ValueError(f'no template for {issue_type=}')
+
+            body = template_body.format(**template_variables)
 
             try:
                 issue = github.compliance.issue.create_or_update_issue(
