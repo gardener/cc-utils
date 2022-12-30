5f293adeca95e3efbd88efc67de2d07a7de731b5
Christian Cwienk <christian.cwienk@sap.com>
2022-12-30 09:13:07 +0100

    refactor unittests

--- test/version_test.py
+++ test/version_test.py
@@ -12,215 +12,177 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import semver
-import unittest
+import pytest
 
 import version as examinee
 
 
-class Version_find_latest_version(unittest.TestCase):
-    def test_find_latest_version(self):
-        versions = (semver.VersionInfo.parse(v) for v in (
-                '0.0.10',
-                '0.20.1',
-                '2.50.100',
-                '3.0.1',
-                '1.0.0',
-        )
-        )
-        result = examinee.find_latest_version(versions)
-        self.assertEqual(str(result), '3.0.1')
-
-    def test_ignore_prerelease_versions(self):
-        versions = (semver.VersionInfo.parse(v) for v in (
-                '0.0.10',
-                '3.0.1',
-                '1.0.0',
-                '3.1.0-foo-bar',
-        )
-        )
-        result = examinee.find_latest_version(versions, ignore_prerelease_versions=True)
-        self.assertEqual(str(result), '3.0.1')
+def test_find_latest_version():
+    versions = (semver.VersionInfo.parse(v) for v in (
+            '0.0.10',
+            '0.20.1',
+            '2.50.100',
+            '3.0.1',
+            '1.0.0',
+    ))
+    result = examinee.find_latest_version(versions)
+    assert str(result) == '3.0.1'
 
 
-class Version_process_version_Test(unittest.TestCase):
-    # def test_invalid_version_operation(self):
-    #     with self.assertRaises(ValueError):
-    #         examinee.process_version(version_str='1.2.3', operation='made-up_op')
+def test_ignore_prerelease_versions():
+    versions = (semver.VersionInfo.parse(v) for v in (
+            '0.0.10',
+            '3.0.1',
+            '1.0.0',
+            '3.1.0-foo-bar',
+    ))
+    result = examinee.find_latest_version(versions, ignore_prerelease_versions=True)
+    assert str(result) == '3.0.1'
 
-    def test_invalid_version(self):
-        with self.assertRaises(ValueError):
-            examinee.process_version(version_str='invalid', operation='noop')
 
-    def test_invalid_args_prerelease_missing(self):
-        with self.assertRaises(ValueError):
-            examinee.process_version(version_str='1.2.3', operation='set_prerelease')
+def test_argument_validation():
+    with pytest.raises(ValueError):
+        examinee.process_version(version_str='invalid', operation='noop')
 
-    def test_invalid_args_build_metadata_missing(self):
-        with self.assertRaises(ValueError):
-            examinee.process_version(version_str='1.2.3', operation='set_build_metadata')
+    # prerelease arg missing
+    with pytest.raises(ValueError):
+        examinee.process_version(version_str='1.2.3', operation='set_prerelease')
 
-    def test_invalid_args_build_metadata_len_less_than_zero(self):
-        with self.assertRaises(ValueError):
-            examinee.process_version(
-            version_str='3.5.4',
-            operation='set_build_metadata',
-            build_metadata='someRandomString',
-            build_metadata_length=-1
-        )
+    # build_metadata_missing
+    with pytest.raises(ValueError):
+        examinee.process_version(version_str='1.2.3', operation='set_build_metadata')
 
-    def test_invalid_args_append_affix_missing(self):
-        with self.assertRaises(ValueError):
-            examinee.process_version(
-            version_str='3.5.4-foo',
-            operation='append_prerelease'
-        )
+    # metadata-len-smaller-than-zero
+    with pytest.raises(ValueError):
+        examinee.process_version(
+        version_str='3.5.4',
+        operation='set_build_metadata',
+        build_metadata='someRandomString',
+        build_metadata_length=-1
+    )
 
-    def test_invalid_args_append_prerelease_missing(self):
-        with self.assertRaises(ValueError):
-            examinee.process_version(
-            version_str='3.5.4',
-            operation='append_prerelease',
-            prerelease='foo'
-        )
+    # prerelease present (in version) if using append_prerelease
+    with pytest.raises(ValueError):
+        examinee.process_version(
+        version_str='3.5.4-foo',
+        operation='append_prerelease'
+    )
 
-    def test_noop(self):
-        parsed = examinee.process_version(version_str='1.2.3-abc', operation='noop')
-        self.assertEqual(parsed, '1.2.3-abc')
+    # prerelease missing if using append_prerelease
+    with pytest.raises(ValueError):
+        examinee.process_version(
+        version_str='3.5.4',
+        operation='append_prerelease',
+        prerelease='foo'
+    )
 
-    def test_set_build_metadata_length(self):
-        parsed = examinee.process_version(
-            version_str='1.3.5',
-            operation='set_build_metadata',
-            build_metadata='someRandomString',
-            build_metadata_length=10
-        )
-        self.assertEqual(parsed, '1.3.5+someRandom')
-
-    def test_set_prerelease_without_suffix(self):
-        parsed = examinee.process_version(
-            version_str='1.2.3',
-            operation='set_prerelease',
-            prerelease='dev'
-        )
-        self.assertEqual(parsed, '1.2.3-dev')
-
-    def test_set_build_metadata_without_suffix(self):
-        parsed = examinee.process_version(
-            version_str='3.3.3',
-            operation='set_build_metadata',
-            build_metadata='build'
-        )
-        self.assertEqual(parsed, '3.3.3+build')
-
-    def test_set_prerelease_with_prerelease(self):
-        parsed = examinee.process_version(
-            version_str='1.2.3-foo',
-            operation='set_prerelease',
-            prerelease='dev'
-        )
-        self.assertEqual(parsed, '1.2.3-dev')
-
-    def test_set_build_metadata_with_prerelease(self):
-        parsed = examinee.process_version(
-            version_str='3.3.3-foo',
-            operation='set_build_metadata',
-            build_metadata='build'
-        )
-        self.assertEqual(parsed, '3.3.3+build')
-
-    def test_set_prerelease_with_build_metadata(self):
-        parsed = examinee.process_version(
-            version_str='1.2.3+foo',
-            operation='set_prerelease',
-            prerelease='dev'
-        )
-        self.assertEqual(parsed, '1.2.3-dev')
-
-    def test_set_build_metadata_with_build_metadata(self):
-        parsed = examinee.process_version(
-            version_str='3.3.3+foo',
-            operation='set_build_metadata',
-            build_metadata='build'
-        )
-        self.assertEqual(parsed, '3.3.3+build')
-
-    def test_set_prerelease_and_build_metadata_without_suffix(self):
-        parsed = examinee.process_version(
-            version_str='6.6.6',
-            operation='set_prerelease_and_build',
-            prerelease='dev',
-            build_metadata='build'
-        )
-        self.assertEqual(parsed, '6.6.6-dev+build')
-
-    def test_set_prerelease_and_build_metadata_with_prerelease(self):
-        parsed = examinee.process_version(
-            version_str='4.3.2-foo',
-            operation='set_prerelease_and_build',
-            prerelease='dev',
-            build_metadata='build'
-        )
-        self.assertEqual(parsed, '4.3.2-dev+build')
-
-    def test_set_prerelease_and_build_metadata_with_build(self):
-        parsed = examinee.process_version(
-            version_str='9.6.3+bar',
-            operation='set_prerelease_and_build',
-            prerelease='dev',
-            build_metadata='build'
-        )
-        self.assertEqual(parsed, '9.6.3-dev+build')
-
-    def test_set_prerelease_and_build_metadata_with_prerelease_and_build(self):
-        parsed = examinee.process_version(
-            version_str='8.1.5-bar+baz',
-            operation='set_prerelease_and_build',
-            prerelease='dev',
-            build_metadata='build'
-        )
-        self.assertEqual(parsed, '8.1.5-dev+build')
-
-    def test_append_prerelease(self):
-        parsed = examinee.process_version(
-            version_str='4.9.16-foo',
-            operation='append_prerelease',
-            prerelease='bar',
-        )
-        self.assertEqual(parsed, '4.9.16-foo-bar')
-
-    def test_append_prerelease_with_build_metadata(self):
-        parsed = examinee.process_version(
-            version_str='3.1.4-foo+bar',
-            operation='append_prerelease',
-            prerelease='baz',
-        )
-        self.assertEqual(parsed, '3.1.4-foo-baz+bar')
-
-    def test_set_verbatim_with_verbatim_version(self):
-        parsed = examinee.process_version(
-            version_str='3.1.4-foo+bar',
+    # version_str missing if using "set_verbatim"
+    with pytest.raises(ValueError):
+        examinee.process_version(
+            version_str='3.1.4-foo',
             operation='set_verbatim',
-            verbatim_version='master',
+            prerelease='baz'
         )
-        self.assertEqual(parsed, 'master')
 
-    def test_set_verbatim_without_verbatim_version(self):
-        with self.assertRaises(ValueError):
-            examinee.process_version(
-                version_str='3.1.4-foo',
-                operation='set_verbatim',
-                prerelease='baz'
-            )
 
-    def test_bump_major(self):
-        parsed = examinee.process_version(version_str='2.4.6', operation='bump_major')
-        self.assertEqual(parsed, '3.0.0')
+def test_noop():
+    parsed = examinee.process_version(version_str='1.2.3-abc', operation='noop')
+    assert parsed == '1.2.3-abc'
 
-    def test_bump_minor(self):
-        parsed = examinee.process_version(version_str='2.4.6', operation='bump_minor')
-        self.assertEqual(parsed, '2.5.0')
 
-    def test_bump_patch(self):
-        parsed = examinee.process_version(version_str='2.4.6', operation='bump_patch')
-        self.assertEqual(parsed, '2.4.7')
+def test_set_build_metadata_length():
+    parsed = examinee.process_version(
+        version_str='1.3.5',
+        operation='set_build_metadata',
+        build_metadata='someRandomString',
+        build_metadata_length=10
+    )
+    assert len(parsed.removeprefix('1.3.5+')) == 10
+
+
+def test_set_prerelease_without_suffix():
+    parsed = examinee.process_version(
+        version_str='1.2.3',
+        operation='set_prerelease',
+        prerelease='dev'
+    )
+    assert parsed == '1.2.3-dev'
+
+
+def test_set_build_metadata_without_suffix():
+    parsed = examinee.process_version(
+        version_str='3.3.3',
+        operation='set_build_metadata',
+        build_metadata='build'
+    )
+    assert parsed == '3.3.3+build'
+
+
+def test_set_prerelease_with_prerelease():
+    parsed = examinee.process_version(
+        version_str='1.2.3-foo',
+        operation='set_prerelease',
+        prerelease='dev'
+    )
+    assert parsed == '1.2.3-dev'
+
+
+def test_set_build_metadata_with_prerelease():
+    parsed = examinee.process_version(
+        version_str='3.3.3-foo',
+        operation='set_build_metadata',
+        build_metadata='build'
+    )
+    assert parsed == '3.3.3+build'
+
+
+def test_set_prerelease_with_build_metadata():
+    parsed = examinee.process_version(
+        version_str='1.2.3+foo',
+        operation='set_prerelease',
+        prerelease='dev'
+    )
+    assert parsed == '1.2.3-dev'
+
+
+def test_set_build_metadata_with_build_metadata():
+    parsed = examinee.process_version(
+        version_str='3.3.3+foo',
+        operation='set_build_metadata',
+        build_metadata='build'
+    )
+    assert parsed == '3.3.3+build'
+
+
+def test_append_prerelease():
+    parsed = examinee.process_version(
+        version_str='4.9.16-foo',
+        operation='append_prerelease',
+        prerelease='bar',
+    )
+    assert parsed == '4.9.16-foo-bar'
+
+
+def test_set_verbatim_with_verbatim_version():
+    parsed = examinee.process_version(
+        version_str='3.1.4-foo+bar',
+        operation='set_verbatim',
+        verbatim_version='master',
+    )
+    assert parsed == 'master'
+
+
+def test_bumping():
+    # major
+    parsed = examinee.process_version(version_str='2.4.6', operation='bump_major')
+    assert parsed == '3.0.0'
+
+    # minor
+    parsed = examinee.process_version(version_str='2.4.6', operation='bump_minor')
+    assert parsed == '2.5.0'
+
+    # patch
+    parsed = examinee.process_version(version_str='2.4.6', operation='bump_patch')
+    assert parsed  == '2.4.7'
