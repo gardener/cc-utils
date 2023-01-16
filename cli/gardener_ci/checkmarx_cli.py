751a2536673ae41ccf936d789ca4adcbc19b0af3
zkdev <dev@zeekay.dev>
2023-01-16 11:16:11 +0100

    Fix indentation

--- cli/gardener_ci/checkmarx_cli.py
+++ cli/gardener_ci/checkmarx_cli.py
@@ -3,10 +3,10 @@ import concourse.steps.component_descriptor_util as cdu
 
 
 def upload_and_scan_from_component_descriptor(
-        checkmarx_cfg_name: str,
-        component_descriptor_path: str,
-        team_id: str=None,
-        force: bool=False,
+    checkmarx_cfg_name: str,
+    component_descriptor_path: str,
+    team_id: str=None,
+    force: bool=False,
 ):
     component_descriptor = cdu.component_descriptor_from_component_descriptor_path(
         cd_path=component_descriptor_path,
