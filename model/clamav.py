564c33fc8d859fedeb7e3471ada620670b5c6b53
Andreas Burger <andreas.burger@sap.com>
2023-02-01 16:22:35 +0100

    update ClamAVConfig model class
    rm unused attrs and add explicit freshclam-config

--- model/clamav.py
+++ model/clamav.py
@@ -18,6 +18,27 @@ from model.base import NamedModelElement
 from model.proxy import DockerImageConfig
 
 
+class FreshclamConfig(NamedModelElement):
+    '''Not intended to be instantiated by users of this module
+    '''
+    def service_name(self):
+        return self.raw.get('service_name')
+
+    def service_port(self):
+        return self.raw.get('service_port')
+
+    def replicas(self):
+        return self.raw.get('replicas')
+
+    def _required_attributes(self):
+        yield from super()._required_attributes()
+        yield from [
+            'replicas',
+            'service_name',
+            'service_port',
+        ]
+
+
 class ClamAVConfig(NamedModelElement):
     '''Not intended to be instantiated by users of this module
     '''
@@ -25,13 +46,9 @@ class ClamAVConfig(NamedModelElement):
     def namespace(self):
         return self.raw.get('namespace')
 
-    def freshclam_image_config(self):
-        return DockerImageConfig(self.raw.get('freshclam_image'))
-
-    def clamav_image_config(self):
-        if image_config := self.raw.get('clamav_image'):
-            return DockerImageConfig(image_config)
-        return None
+    def freshclam_config(self) -> FreshclamConfig | None:
+        if raw := self.raw.get('freshclam'):
+            return FreshclamConfig(name='freshclam_config', raw_dict=raw)
 
     def service_name(self):
         return self.raw.get('service_name')
@@ -52,9 +69,6 @@ class ClamAVConfig(NamedModelElement):
     def clamd_config_values(self):
         return self.raw.get('clamd_config_values', {})
 
-    def container_registry_config_name(self):
-        return self.raw.get('container_registry')
-
     def replicas(self):
         return self.raw.get('replicas')
 
@@ -70,8 +84,6 @@ class ClamAVConfig(NamedModelElement):
     def _optional_attributes(self):
         yield from super()._optional_attributes()
         yield from [
-            'clamav_image',
             'clamd_config_values',
-            'container_registry',
-            'freshclam_image',
+            'freshclam',
         ]
