d88007e27ec4fec507bc4b0bcff3684247b7fd94
Christian Cwienk <christian.cwienk@sap.com>
2023-02-13 20:42:24 +0100

    fix: add missing return

--- doc/lib/attributes.py
+++ doc/lib/attributes.py
@@ -83,7 +83,7 @@ class AttributesDocumentation:
             else:
                 raise NotImplementedError
         elif isinstance(type_, types.UnionType):
-            str(type_)
+            return str(type_)
         else:
             return type_.__name__
 
