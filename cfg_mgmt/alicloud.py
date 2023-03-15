c0170603b2c7063437d3d5a6bfe52f3b418e66db
Andreas Burger <andreas.burger@sap.com>
2023-03-15 10:13:28 +0100

    add validation to alicloud cfg-element rotation

--- cfg_mgmt/alicloud.py
+++ cfg_mgmt/alicloud.py
@@ -11,7 +11,6 @@ import aliyunsdkcore.client
 from aliyunsdkram.request.v20150501.ListAccessKeysRequest import ListAccessKeysRequest
 from aliyunsdkram.request.v20150501.CreateAccessKeyRequest import CreateAccessKeyRequest
 from aliyunsdkram.request.v20150501.DeleteAccessKeyRequest import DeleteAccessKeyRequest
-from aliyunsdkram.request.v20150501.UpdateAccessKeyRequest import UpdateAccessKeyRequest
 from aliyunsdkcore.acs_exception.exceptions import ServerException
 
 import cfg_mgmt
@@ -20,7 +19,10 @@ import model
 import model.aws
 import model.alicloud
 
-from cfg_mgmt.model import CfgQueueEntry
+from cfg_mgmt.model import (
+    CfgQueueEntry,
+    ValidationError,
+)
 
 
 logger = logging.getLogger(__name__)
@@ -74,48 +76,6 @@ def rotate_cfg_element(
         ak=cfg_element.access_key_id(),
         secret=cfg_element.access_key_secret(),
     )
-
-    # If no parameters are set on the request, the user whose access key is used will be used
-    request = ListAccessKeysRequest()
-    try:
-        response = client.do_action_with_exception(request)
-    except ServerException as e:
-        if e.get_http_status() == 403:
-            logger.warning(
-                'User is not allowed to list its own access keys. Please make sure that the '
-                'Account is configured to allow users to manage their own keys.'
-            )
-            raise
-
-    response: ListAccessKeysResponse = dacite.from_dict(
-        data_class=ListAccessKeysResponse,
-        data=json.loads(response),
-        config=dacite.Config(check_types=False),
-    )
-
-    access_keys = response.AccessKeys.AccessKey
-
-    # We either have one or two access keys as Alicloud does not allow more than two and we used one
-    # to get access. For the first case just create a new one. If there are already two, determine
-    # the oldest key and delete it beforehand.
-    # Note: This cannot be undone.
-    if len(access_keys) == 2:
-        logger.info('There are already two SecretAccessKeys present. Deleting oldest key ...')
-        sorted_metadata = sorted(access_keys, key=lambda m: m.CreateDate)
-        oldest_key_id = sorted_metadata[0].AccessKeyId
-        request = DeleteAccessKeyRequest()
-        request.set_UserAccessKeyId(oldest_key_id)
-        try:
-            response = client.do_action_with_exception(request)
-        except ServerException as e:
-            if e.get_http_status() == 403:
-                logger.warning(
-                    'User is not allowed to delete its own access key. Please make sure that the '
-                    'Account is configured to allow users to manage their own keys.'
-                )
-            raise
-
-        logger.info(f'Deleted SecretAccessKey {oldest_key_id}')
 
     request = CreateAccessKeyRequest()
     try:
@@ -177,3 +137,38 @@ def delete_config_secret(
                 'the Account is configured to allow users to manage their own keys.'
             )
         raise
+
+
+def validate_for_rotation(
+    cfg_element: model.alicloud.AlicloudConfig,
+):
+    access_key_id = cfg_element.access_key_id()
+    client = aliyunsdkcore.client.AcsClient(
+        ak=access_key_id,
+        secret=cfg_element.access_key_secret(),
+    )
+
+    request = ListAccessKeysRequest()
+    try:
+        response = client.do_action_with_exception(request)
+    except ServerException as e:
+        if e.get_http_status() == 403:
+            logger.warning(
+                'User is not allowed to list its own access keys. Please make sure that the '
+                'Account is configured to allow users to manage their own keys.'
+            )
+            raise
+
+    response: ListAccessKeysResponse = dacite.from_dict(
+        data_class=ListAccessKeysResponse,
+        data=json.loads(response),
+        config=dacite.Config(check_types=False),
+    )
+
+    access_keys = response.AccessKeys.AccessKey
+
+    if len(access_keys) == 2:
+        raise ValidationError(
+            'There are already two keys present in Alicloud for Access Key '
+            f"'{access_key_id}'. Will not attempt rotation."
+        )
