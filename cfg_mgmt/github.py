1e98f0e39dc8a7596075a51a1bd7df6aa0ab87e8
Andreas Burger <andreas.burger@sap.com>
2023-04-20 14:33:22 +0200

    Support ECC based keys for github-users
    In addition to RSA-based keys the code can now also manage
    ECC-based keys.
    More specifically, if an ECC-based key is encountered, the new
    key created for rotation will also be an ECC-based key (using
    Curve25519). This allows our users to strictly follow the github-
    docs when creating keys for users which currently recommends
    creating an ECC-based OpenSSH key using Curve25519.

--- cfg_mgmt/github.py
+++ cfg_mgmt/github.py
@@ -1,8 +1,9 @@
 import copy
 import logging
 import typing
+import enum
 
-from Crypto.PublicKey import RSA
+from Crypto.PublicKey import RSA, ECC
 
 import ccc.github
 import cfg_mgmt
@@ -22,6 +23,27 @@ ci.log.configure_default_logging()
 logger = logging.getLogger(__name__)
 
 
+class KeyAlgorithm(enum.Enum):
+    RSA = enum.auto()
+    ECC = enum.auto()
+
+
+def _determine_key_algorithm(key: str) -> KeyAlgorithm:
+    try:
+        RSA.import_key(key)
+        return KeyAlgorithm.RSA
+    except ValueError:
+        pass
+
+    try:
+        ECC.import_key(key)
+        return KeyAlgorithm.ECC
+    except ValueError:
+        pass
+
+    raise ValueError('Unsupported Key format. Currently, only RSA and ECC keys are supported.')
+
+
 def rotate_cfg_element(
     cfg_element: model.container_registry.ContainerRegistryConfig,
     cfg_factory: model.ConfigFactory,
@@ -37,18 +59,28 @@ def rotate_cfg_element(
 
     # retrieve current/"old" public keys before updating to be able to store them in deletion
     # queue
-    old_public_keys = [
-        {
-            'name': credential.username(),
-            'public_key': _corresponding_public_key(credential.private_key()),
-        }
-        for credential in technical_user_credentials
-    ]
+    old_public_keys = []
+    new_public_keys = dict()
+    for credential in technical_user_credentials:
 
-    new_public_keys = {
-        credential.username(): update_user(cfg_to_rotate, credential, cfg_factory)
-        for credential in technical_user_credentials
-    }
+        key_algorithm = _determine_key_algorithm(credential.private_key())
+
+        old_public_keys.append({
+            'name': credential.username(),
+            'public_key': _corresponding_public_key(
+                private_key=credential.private_key(),
+                key_algorithm=key_algorithm,
+            ),
+        })
+
+        new_public_keys.update({
+            credential.username(): update_user(
+                github_config=cfg_to_rotate,
+                credential=credential,
+                cfg_factory=cfg_factory,
+                key_algorithm=key_algorithm,
+            )
+        })
 
     secret_id = {'github_users': old_public_keys}
 
@@ -83,13 +115,14 @@ def update_user(
     github_config: GithubConfig,
     credential: GithubCredentials,
     cfg_factory: model.ConfigFactory,
+    key_algorithm: KeyAlgorithm,
 ) -> str:
     gh_api = ccc.github.github_api(
         github_cfg=github_config,
         username=credential.username(),
         cfg_factory=cfg_factory,
     )
-    private_key, public_key = _create_key_pair()
+    private_key, public_key = _create_key_pair(key_algorithm=key_algorithm)
     gh_api.create_key(
         title='Auto-generated key',
         key=public_key,
@@ -118,27 +151,42 @@ def delete_config_secret(
 
 def _corresponding_public_key(
     private_key: str,
+    key_algorithm: KeyAlgorithm,
 ) -> str:
-    # current private key uses EdDSA which is not supported by Pycryptodome
-    # TODO: Remove after first rotation
-    try:
+    if key_algorithm is KeyAlgorithm.RSA:
         key = RSA.import_key(private_key)
-    except ValueError:
-        return ""
-    return key.public_key().export_key(format='OpenSSH').decode('utf-8')
+        public_key_str = key.public_key().export_key(format='OpenSSH').decode('utf-8')
+    elif key_algorithm is KeyAlgorithm.ECC:
+        key = ECC.import_key(private_key)
+        # PEM/OpenSSH keys will be returned as string (not necessarily true for other formats)
+        public_key_str = key.public_key().export_key(format='OpenSSH')
+    else:
+        raise NotImplementedError(key_algorithm)
+
+    return public_key_str
 
 
 def _create_key_pair(
-    bits: int = 4096,
+    key_algorithm: KeyAlgorithm,
 ) -> typing.Tuple[str, str]:
-    '''return a (private-key, public-key)-RSA-keypair as a pair of strings.
+    '''return a (private-key, public-key)-keypair using the given Algorithm as a pair of strings.
 
     The private-key is returned in PEM-Format whereas the public key is returned as specified by
-    OpenSSH (which is the format expected by GitHub)
+    OpenSSH (which is the format expected by GitHub).
     '''
-    private_key = RSA.generate(bits=bits)
-    public_key = private_key.public_key()
+    if key_algorithm is KeyAlgorithm.RSA:
+        private_key = RSA.generate(bits=4096)
+        private_key_str = private_key.export_key(format='PEM').decode("utf-8")
+        public_key_str = private_key.public_key().export_key(format='PEM').decode("utf-8")
+    elif key_algorithm is KeyAlgorithm.ECC:
+        private_key = ECC.generate(curve='ed25519')
+        # PEM/OpenSSH keys will be returned as string (not necessarily true for other formats)
+        private_key_str = private_key.export_key(format='PEM')
+        public_key_str = private_key.public_key().export_key(format='PEM')
+    else:
+        raise NotImplementedError(key_algorithm)
+
     return (
-        private_key.export_key().decode('utf-8'),
-        public_key.export_key(format='OpenSSH').decode('utf-8')
+        private_key_str,
+        public_key_str
     )
