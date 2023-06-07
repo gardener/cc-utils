374507804399ba72b218c9e7934f40e5c1d1a244
Christian Cwienk <christian.cwienk@sap.com>
2023-06-07 16:59:50 +0200

    cleanup: rm deprecated and unused code

--- cli/gardener_ci/containerutil.py
+++ cli/gardener_ci/containerutil.py
@@ -1,38 +1,5 @@
 import ccc.oci
-import ci.util
 import container.util
-
-
-def filter_image_file(
-    in_file:str,
-    out_file:str,
-    remove_files:[str]=[],
-):
-    '''
-    processes an OCI container image [0] (from a local tar file) and writes a
-    modified copy to the specified `out_file`.
-
-    All files (specified as absolute paths w/o loading slash (/)) are removed from
-    all layer archives. Contained metadata is updated accordingly.
-
-    [0] https://github.com/opencontainers/image-spec
-    '''
-    if not remove_files:
-        ci.util.warning('no files to remove were specified - the output will remain unaltered')
-
-    def parse_entries(remove_entries_files):
-        for remove_entries_file in remove_entries_files:
-            with open(remove_entries_file) as f:
-                for l in f.readlines():
-                    yield l.strip()
-
-    remove_entries = [e for e in parse_entries(remove_files)]
-
-    container.util.filter_container_image(
-        image_file=in_file,
-        out_file=out_file,
-        remove_entries=remove_entries,
-    )
 
 
 def filter_image(
