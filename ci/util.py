9f99598912e2f99238398faea2e18f6d928aa143
Christian Cwienk <christian.cwienk@sap.com>
2023-07-25 16:10:09 +0200

    rm dead code

--- ci/util.py
+++ ci/util.py
@@ -168,10 +168,6 @@ def _verbose():
     return ctx().args and ctx().args.verbose
 
 
-def _cli():
-    return bool(ctx().args and hasattr(ctx().args, '._cli') and ctx().args._cli)
-
-
 def _print(msg, colour, outfh=sys.stdout):
     if not msg:
         return
