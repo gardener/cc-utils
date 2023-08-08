5f12965480c90d6982d538f7f04cc016f1e1b2e3
Andreas Burger <andreas.burger@sap.com>
2023-08-08 17:34:19 +0200

    also (try to) refresh gh-oAuth tokens when rotating ssh-keys

--- cfg_mgmt/github.py
+++ cfg_mgmt/github.py
@@ -3,7 +3,10 @@ import copy
 import enum
 import hashlib
 import logging
+import sys
 import typing
+
+import requests
 
 from Crypto.PublicKey import RSA, ECC
 
@@ -68,6 +71,64 @@ def _rsa_sha256_fingerprint(key: str) -> str:
         return ''
 
 
+def _rotate_oauth_token(
+    github_api_url: str,
+    token_to_rotate: str,
+) -> str | None:
+    '''Rotate the token for the given credentials iff it is an oAuth token, as (poorly) determined
+    by the `gho_` prefix.
+
+    If this function is called for a token other than an oAuth token (e.g.: a personal access token)
+    `None` is returned.
+    The returned token has the same scopes as the initial token. If you'd like to update the scope
+    of the token, you need to re-create it with additional scopes.
+
+    Note: This will only work for oAuth tokens associated with 'git-credential-manager'
+    (https://github.com/git-ecosystem/git-credential-manager) due to hardcoded client_id and
+    client_secret.
+    '''
+
+    if not token_to_rotate.startswith('gho_'):
+        logger.warning(
+            f"oAuth token '{token_to_rotate}' does not start with 'gho_' - will not update."
+        )
+        return None
+    # client_id and client_"secret" can be hardcoded here as they are the same on all github
+    # instances. Also, they are hardcoded in the git credential manager sourcecode on github.com as
+    # well, so no actual secrets are leaked here.
+    # See https://github.com/git-ecosystem/git-credential-manager/
+    # blob/f89105b1ce033e8c2756044d8b195eade2889dac/src/shared/GitHub/GitHubConstants.cs#L14-L17
+    client_id = '0120e057bd645470c1ed'
+    client_secret = '18867509d956965542b521a529a79bb883344c90'
+
+    request_url = f'{github_api_url}/applications/{client_id}/token'
+    request_kwargs = {
+        'url': request_url,
+        'auth': (client_id, client_secret), # basic auth
+        'json': {'access_token': token_to_rotate},
+    }
+    # check token (to catch the case if the token not being associated with the git-credentials-
+    # manager)
+    resp = requests.post(**request_kwargs)
+    if not resp.ok:
+        # No way for the user to see the oAuth token that belongs to the app, but at least checking
+        # whether the given token is currently associated with the app can be done.
+        logger.warning(
+            f"Given oAuth token '{token_to_rotate}' did not pass GitHub verification. Does the "
+            'oAuth token belong to the git-credentials-manager application and is valid?'
+        )
+        return None
+    # fire request that causes the actual refresh (i.e. "rotation") to happen.
+    # Note: If successful, old token is immediately invalidated.
+    resp = requests.patch(**request_kwargs)
+    if not resp.ok:
+        resp.raise_for_status()
+
+    response_content = resp.json()
+
+    return response_content['token']
+
+
 def rotate_cfg_element(
     cfg_element: model.github.GithubConfig,
     cfg_factory: model.ConfigFactory,
@@ -83,9 +144,9 @@ def rotate_cfg_element(
 
     # retrieve current/"old" public keys before updating to be able to store them in deletion
     # queue
-    old_public_keys = []
+    old_user_data = []
     new_public_keys = dict()
-    for credential in technical_user_credentials:
+    for i, credential in enumerate(technical_user_credentials):
         if not (private_key := credential.private_key()):
             logger.warn(f'{credential.username()=} did not have private-key - skipping')
             continue
@@ -98,12 +159,53 @@ def rotate_cfg_element(
         )
 
         old_key_fingerprint = _rsa_sha256_fingerprint(old_public_key)
+        old_token = ''
+        # first, try to update token (if possible, see _rotate_oauth_token())
+        try:
+            # We rotate by refreshing the secondary token and afterwards switching
+            # the primary and secondary tokens. In effect, the old primary token will
+            # still be valid until the next rotation happens.
+            # This is done to avoid breaking running jobs, since existing tokens are invalidated
+            # immediately upon refresh.
+            token_to_rotate = credential.secondary_auth_token()
+            if token_to_rotate:
+                if new_token := _rotate_oauth_token(
+                    github_api_url=cfg_to_rotate.api_url(),
+                    token_to_rotate=token_to_rotate,
+                ):
+                    old_token = credential.auth_token()
+                    credential.raw['secondary_authToken'] = old_token
+                    credential.raw['authToken'] = new_token
+            else:
+                logger.info(
+                    'No secondary oAuth token provided for credential with username '
+                    f"'{credential.username}' of github-config '{cfg_to_rotate.name()}' "
+                    '- will not attempt rotation.'
+                )
+        except:
+            exception = sys.exception()
+            if isinstance(exception, requests.exceptions.HTTPError):
+                logger.error(
+                    f'Error when trying to refresh oAuth token: {exception}. Response from server: '
+                    f'{exception.response}'
+                )
+            else:
+                logger.error(f'Error when trying to refresh oAuth token: {exception}')
 
-        old_public_keys.append({
+            # we only abort here if the first rotation failed (assuming that the token was not
+            # refreshed). Otherwise we keep on rotating keys/tokens to prevent the previously
+            # succeded rotations from being lost due to the abort.
+            if i == 0:
+                raise
+        config_queue_data = {
             'name': credential.username(),
             'public_key': old_public_key,
             'fingerprint': old_key_fingerprint,
-        })
+        }
+        if old_token:
+            config_queue_data['oAuthToken'] = old_token
+
+        old_user_data.append(config_queue_data)
 
         new_public_keys.update({
             credential.username(): update_user(
@@ -114,13 +216,14 @@ def rotate_cfg_element(
             )
         })
 
-    secret_id = {'github_users': old_public_keys}
+    secret_id = {'github_users': old_user_data}
 
     def revert():
         logger.warning(
             f"An error occurred during update of github config '{cfg_to_rotate.name()}', "
             'rolling back'
         )
+        # No way to revert oAuth-token rotation. Roll back changes to ssh-keys and return
         for credential in technical_user_credentials:
             if not credential.private_key():
                 logger.warn(f'{credential.username()=} did not have private-key - skipping')
