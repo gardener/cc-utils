gardener/cc-utils

commit 26ba9b0f970f21e0e19b8f04158e7f662a4314d2

write ocm-cfg into component-descriptor-outdir
make ocm-cfg file easier to discover

concourse/steps/component_descriptor.mako
@@ -180,8 +180,7 @@ if os.path.isfile(descriptor_script):
   with open(base_descriptor_file_v2, 'w') as f:
     f.write(dump_component_descriptor_v2(base_descriptor_v2))
 
-  ocm_config_out_dir = tempfile.TemporaryDirectory()
-  ocm_config_file = tempfile.NamedTemporaryFile(dir=ocm_config_out_dir.name, delete=False)
+  ocm_config_file = os.path.join(descriptor_out_dir, 'ocm-cfg.yaml')
   with open(ocm_config_file.name, 'w') as f:
     f.write(mapping_config.to_ocm_software_config())
 
@@ -194,7 +193,7 @@ if os.path.isfile(descriptor_script):
   subproc_env['COMPONENT_VERSION'] = effective_version
   subproc_env['EFFECTIVE_VERSION'] = effective_version
   subproc_env['CURRENT_COMPONENT_REPOSITORY'] = ctx_repository_base_url
-  subproc_env['OCM_CONFIG_PATH'] = ocm_config_file.name
+  subproc_env['OCM_CONFIG_PATH'] = ocm_config_file
 
   # pass predefined command to add dependencies for convenience purposes
   add_dependencies_cmd = ' '.join((

commit f1dae8e7e36f7e232e5bf855d3571dd7f9a5cc2a

fix: AttributeError, `ocm_config_file` is of type <str>
Bug was introduced with https://github.com/gardener/cc-utils/commit/26ba9b0f970f21e0e19b8f04158e7f662a4314d2

concourse/steps/component_descriptor.mako
@@ -181,7 +181,7 @@ if os.path.isfile(descriptor_script):
     f.write(dump_component_descriptor_v2(base_descriptor_v2))
 
   ocm_config_file = os.path.join(descriptor_out_dir, 'ocm-cfg.yaml')
-  with open(ocm_config_file.name, 'w') as f:
+  with open(ocm_config_file, 'w') as f:
     f.write(mapping_config.to_ocm_software_config())
 
   subproc_env = os.environ.copy()