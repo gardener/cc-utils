49d71c2fc6bb14adbc3cfc7978346a6c940c020b
Christian Cwienk <christian.cwienk@sap.com>
2024-01-30 17:32:57 +0100

    fix: make image optional

--- concourse/model/traits/publish.py
+++ concourse/model/traits/publish.py
@@ -69,10 +69,12 @@ IMG_DESCRIPTOR_ATTRIBS = (
         type=str,
         doc='name of the registry config to use when pushing the image.',
     ),
-    AttributeSpec.required(
+    AttributeSpec.optional(
         name='image',
         type=str,
-        doc='image reference to publish the created container image to.',
+        doc='''\
+        image reference to publish the created container image to. required if on `targets` are set
+        ''',
     ),
     AttributeSpec.optional(
         name='extra_push_targets',
