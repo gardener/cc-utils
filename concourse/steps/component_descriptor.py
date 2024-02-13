206636d4a710aa3154a95d3ed0ce0b97de4728cd
Christian Cwienk <christian.cwienk@sap.com>
2024-02-13 13:41:00 +0100

    workaround unittest issue (late-import datetime)

--- concourse/steps/component_descriptor.py
+++ concourse/steps/component_descriptor.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import datetime
 import logging
 
 import git
@@ -49,6 +48,7 @@ def base_component_descriptor_v2(
     commit: str,
     repo_url: str,
 ):
+    import datetime
     import cnudie.migrate
     import gci.componentmodel as cm
     import version as version_util
