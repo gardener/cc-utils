d2665e622f60e1b93493d5bef56c9c05cfb43947
Christian Cwienk <christian.cwienk@sap.com>
2024-02-19 22:16:47 +0100

    fix linter warnings (trailing newlines)

--- model/secret.py
+++ model/secret.py
@@ -1,12 +1,5 @@
 # SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
-#
 # SPDX-License-Identifier: Apache-2.0
-
-# SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
-#
-# SPDX-License-Identifier: Apache-2.0
-
-
 
 import enum
 import dataclasses
