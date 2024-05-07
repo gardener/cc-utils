816ffb9b6b55b85cb14993a850cc2df599e98014
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2024-05-07 10:16:01 +0200

    Add gzip compression to delivery client metadata routes

--- http_requests.py
+++ http_requests.py
@@ -2,9 +2,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import collections.abc
 import enum
 import functools
+import io
+import json as js
 import logging
+import typing
+import zlib
 
 import cachecontrol
 import requests
@@ -221,3 +226,73 @@ class AuthenticatedRequestBuilder:
                 return_type=None,
                 **kwargs
         )
+
+
+class EncodingMethod(enum.StrEnum):
+    GZIP = 'gzip'
+
+
+def encode_request(
+    data: str | bytes | dict | typing.IO=None,
+    json: dict=None,
+    headers: dict[str, str]=None,
+    encoding_method: EncodingMethod=EncodingMethod.GZIP,
+) -> (
+    tuple[collections.abc.Generator[bytes, None, None], dict[str, str]]
+    | collections.abc.Generator[bytes, None, None]
+):
+    '''
+    Encodes the given `data` or `json` property based on the selected `encoding_method`. Only one of
+    `data` or `json` must be set, otherwise a `ValueError` is raised.
+
+    The corresponding `Content-Encoding` header is patched-in to the provided headers dictionary.
+
+    If `headers` is provided and not `None`, the response is a tuple of the compression result and
+    the patched headers, otherwise only the compression result is returned.
+    '''
+    if data and json:
+        raise ValueError('Only one of `data` or `json` must be set')
+
+    if isinstance(data, dict) and encoding_method == EncodingMethod.GZIP:
+        raise ValueError('`data` of type `dict` is not supported for gzip encoding')
+
+    if json:
+        data = js.dumps(json)
+
+    def _encode(obj, encoding: str='utf-8') -> bytes:
+        if isinstance(obj, bytes):
+            return obj
+        elif isinstance(obj, str):
+            return obj.encode(encoding=encoding)
+        elif isinstance(obj, dict):
+            return js.dumps(obj).encode(encoding=encoding)
+        else:
+            raise ValueError(f'Encoding of type {type(obj)} is not (yet) supported')
+
+    def _compress(data, encoding_method) -> collections.abc.Generator[bytes, None, None]:
+        if isinstance(data, io.BufferedIOBase):
+            if encoding_method == EncodingMethod.GZIP:
+                compressor = zlib.compressobj(wbits=31)
+                data.seek(0)
+
+                while (chunk := data.read(4096)):
+                    yield compressor.compress(chunk)
+                yield compressor.flush()
+
+        elif hasattr(data, '__iter__') and not isinstance(data, (str, bytes, dict)):
+            raise ValueError(f'Encoding of iterable {type(data)} is not (yet) supported')
+
+        else:
+            data = _encode(data)
+
+            if encoding_method == EncodingMethod.GZIP:
+                compressor = zlib.compressobj(wbits=31)
+                yield compressor.compress(data) + compressor.flush()
+
+    compressed_data = _compress(data, encoding_method)
+
+    if headers is not None:
+        headers['Content-Encoding'] = encoding_method
+        return compressed_data, headers
+
+    return compressed_data
