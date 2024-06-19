ef974d5407511a867636fa0d1ba84d813cedb999
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2024-06-19 10:57:01 +0200

    Increase timeout for updating artefact metadata in delivery-service
    Since the delivery-service recently switched to using uWSGI
    https://github.com/open-component-model/delivery-service/commit/cb9a42357e52bf6eb0a95f2aea68c00daae00f1b,
    the number of threads increased and thus blocking one of them for a
    longer period of time is not as bad as before.

--- delivery/client.py
+++ delivery/client.py
@@ -357,7 +357,7 @@ class DeliveryServiceClient:
             method='PUT',
             headers=headers,
             data=data,
-            timeout=(4, 121),
+            timeout=(4, 241),
         )
 
         res.raise_for_status()
