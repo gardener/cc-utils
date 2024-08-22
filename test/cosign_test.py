3ae22582b9fa2d8d3f15d8c928c3f16e6eb96d72
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2024-08-22 17:13:24 +0200

    Adjust cosign test to handle annotations and overwrites

--- test/cosign_test.py
+++ test/cosign_test.py
@@ -29,7 +29,14 @@ def image_reference(
 
 
 @pytest.fixture
-def payload(
+def payload_annotations() -> dict[str, str]:
+    return {
+        'key': 'val',
+    }
+
+
+@pytest.fixture
+def payload_without_annotations(
     image_reference: str,
 ) -> bytes:
     return cosign.payload_bytes(
@@ -38,25 +45,51 @@ def payload(
 
 
 @pytest.fixture
-def payload_size(
-    payload: bytes,
+def payload_without_annotations_size(
+    payload_without_annotations: bytes,
 ) -> int:
-    return len(payload)
+    return len(payload_without_annotations)
 
 
 @pytest.fixture
-def payload_digest(
-    payload: bytes,
+def payload_without_annotations_digest(
+    payload_without_annotations: bytes,
 ) -> str:
-    return f'sha256:{hashlib.sha256(payload).hexdigest()}'
+    return f'sha256:{hashlib.sha256(payload_without_annotations).hexdigest()}'
+
+
+@pytest.fixture
+def payload_with_annotations(
+    image_reference: str,
+    payload_annotations: dict[str, str],
+) -> bytes:
+    return cosign.payload_bytes(
+        image_reference=image_reference,
+        annotations=payload_annotations,
+        overwrite_docker_reference=None,
+    )
+
+
+@pytest.fixture
+def payload_with_annotations_size(
+    payload_with_annotations: bytes,
+) -> int:
+    return len(payload_with_annotations)
+
+
+@pytest.fixture
+def payload_with_annotations_digest(
+    payload_with_annotations: bytes,
+) -> str:
+    return f'sha256:{hashlib.sha256(payload_with_annotations).hexdigest()}'
 
 
 @pytest.fixture
 def cfg_blob(
-    payload_digest: str,
+    payload_without_annotations_digest: str,
 ) -> bytes:
     return cosign.cfg_blob_bytes(
-        payload_digest=payload_digest,
+        payload_digest=payload_without_annotations_digest,
     )
 
 
@@ -97,35 +130,26 @@ def public_key_2() -> str:
 
 def test_json_marshaling_with_annotations(
     image_digest_tag: str,
-    image_reference: str,
+    payload_with_annotations: bytes,
 ):
-    expected_json = '{"critical":{"identity":{"docker-reference":"eu.gcr.io/test/img"},' \
+    expected_json = '{"critical":{"identity":{"docker-reference":null},' \
         '"image":{"docker-manifest-digest":' \
         f'"{image_digest_tag}"' \
         '},"type":"gardener.vnd/oci/cosign-signature"},"optional":{"key":"val"}}'
 
-    annotations = {
-        "key": "val",
-    }
-
-    payload = cosign.payload_bytes(
-        image_reference=image_reference,
-        annotations=annotations,
-    ).decode('utf-8')
-
-    assert payload == expected_json
+    assert payload_with_annotations.decode('utf-8') == expected_json
 
 
 def test_json_marshaling_without_annotations(
     image_digest_tag: str,
-    payload: bytes,
+    payload_without_annotations: bytes,
 ):
     expected_json = '{"critical":{"identity":{"docker-reference":"eu.gcr.io/test/img"},' \
         '"image":{"docker-manifest-digest":' \
         f'"{image_digest_tag}"' \
         '},"type":"gardener.vnd/oci/cosign-signature"},"optional":null}'
 
-    assert payload.decode('utf-8') == expected_json
+    assert payload_without_annotations.decode('utf-8') == expected_json
 
 
 def test_raise_error_for_img_ref_without_digest():
@@ -135,8 +159,10 @@ def test_raise_error_for_img_ref_without_digest():
 
 
 def test_image_signing(
-    payload_size: int,
-    payload_digest: str,
+    payload_with_annotations_size: int,
+    payload_with_annotations_digest: str,
+    payload_without_annotations_size: int,
+    payload_without_annotations_digest: str,
     cfg_blob_size: int,
     cfg_blob_digest: str,
     public_key_1: str,
@@ -151,6 +177,7 @@ def test_image_signing(
     2. Re-signing using same algorithm and key but different signature (as algorithm enforces)
     3. Re-signing using `rsassa-pkcs1-v1-5` algorithm and `public_key_1`
     4. Re-signing using `rsassa-pss` algorithm again but with `public_key_2` and changed signature
+    5. Re-signing using `rsassa-pss` algorithm and `public_key_2` again but with changed payload
     '''
     manifest = oci.model.OciImageManifest(
         config=oci.model.OciBlobRef(
@@ -168,8 +195,8 @@ def test_image_signing(
 
     signed_manifest = cosign.sign_manifest(
         manifest=manifest,
-        payload_size=payload_size,
-        payload_digest=payload_digest,
+        payload_size=payload_with_annotations_size,
+        payload_digest=payload_with_annotations_digest,
         signature=signature,
         signing_algorithm=signing_algorithm,
         public_key=public_key_1,
@@ -179,15 +206,15 @@ def test_image_signing(
     assert len(signed_manifest.layers) == 1
     signature_layer = signed_manifest.layers[0]
     assert signature_layer.annotations.get(cosign._cosign_signature_annotation_name) == signature
-    assert signature_layer.size == payload_size
-    assert signature_layer.digest == payload_digest
+    assert signature_layer.size == payload_with_annotations_size
+    assert signature_layer.digest == payload_with_annotations_digest
 
     signature = 'rsassa-pss-signature-dummy-changed'
 
     manifest_resigned = cosign.sign_manifest(
         manifest=dataclasses.replace(signed_manifest), # copy of previous manifest
-        payload_size=payload_size,
-        payload_digest=payload_digest,
+        payload_size=payload_with_annotations_size,
+        payload_digest=payload_with_annotations_digest,
         signature=signature,
         signing_algorithm=signing_algorithm,
         public_key=public_key_1,
@@ -201,8 +228,8 @@ def test_image_signing(
 
     manifest_different_algorithm = cosign.sign_manifest(
         manifest=dataclasses.replace(manifest_resigned), # copy of previous manifest
-        payload_size=payload_size,
-        payload_digest=payload_digest,
+        payload_size=payload_with_annotations_size,
+        payload_digest=payload_with_annotations_digest,
         signature=signature,
         signing_algorithm=signing_algorithm,
         public_key=public_key_1,
@@ -212,16 +239,16 @@ def test_image_signing(
     assert len(manifest_different_algorithm.layers) == 2
     signature_layer = manifest_different_algorithm.layers[1]
     assert signature_layer.annotations.get(cosign._cosign_signature_annotation_name) == signature
-    assert signature_layer.size == payload_size
-    assert signature_layer.digest == payload_digest
+    assert signature_layer.size == payload_with_annotations_size
+    assert signature_layer.digest == payload_with_annotations_digest
 
     signature = 'rsassa-pss-signature-dummy-changed-again'
     signing_algorithm = model.signing_server.SigningAlgorithm.RSASSA_PSS
 
     manifest_different_key = cosign.sign_manifest(
         manifest=dataclasses.replace(manifest_different_algorithm), # copy of previous manifest
-        payload_size=payload_size,
-        payload_digest=payload_digest,
+        payload_size=payload_with_annotations_size,
+        payload_digest=payload_with_annotations_digest,
         signature=signature,
         signing_algorithm=signing_algorithm,
         public_key=public_key_2,
@@ -231,7 +258,26 @@ def test_image_signing(
     assert len(manifest_different_key.layers) == 3
     signature_layer = manifest_different_key.layers[2]
     assert signature_layer.annotations.get(cosign._cosign_signature_annotation_name) == signature
-    assert signature_layer.size == payload_size
-    assert signature_layer.digest == payload_digest
+    assert signature_layer.size == payload_with_annotations_size
+    assert signature_layer.digest == payload_with_annotations_digest
+
+    signature = 'rsassa-pss-signature-dummy-changed-payload'
+    signing_algorithm = model.signing_server.SigningAlgorithm.RSASSA_PSS
+
+    manifest_different_key = cosign.sign_manifest(
+        manifest=dataclasses.replace(manifest_different_algorithm), # copy of previous manifest
+        payload_size=payload_without_annotations_size,
+        payload_digest=payload_without_annotations_digest,
+        signature=signature,
+        signing_algorithm=signing_algorithm,
+        public_key=public_key_2,
+        on_exist=cosign.OnExist.APPEND,
+    )
+
+    assert len(manifest_different_key.layers) == 4
+    signature_layer = manifest_different_key.layers[3]
+    assert signature_layer.annotations.get(cosign._cosign_signature_annotation_name) == signature
+    assert signature_layer.size == payload_without_annotations_size
+    assert signature_layer.digest == payload_without_annotations_digest
 
     json.dumps(manifest_different_key.as_dict()).encode('utf-8')
