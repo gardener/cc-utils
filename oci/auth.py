a46753e6f09aaa335b97231f136d1e0c034dc595
Christian Cwienk <christian.cwienk@sap.com>
2024-09-12 15:26:38 +0200

    add auth-lookup for docker's auth-store
    Prepare oci-package to work better in conjunction w/ docker's
    auth-store, e.g. to allow it to be run easier outside of gardener-cicd.

--- oci/auth.py
+++ oci/auth.py
@@ -1,6 +1,10 @@
+import base64
+import collections.abc
 import dataclasses
 import enum
+import json
 import operator
+import os
 import typing
 
 import oci.util
@@ -133,3 +137,92 @@ def mk_credentials_lookup(
         return valid_cfgs[0].credentials
 
     return lookup_credentials
+
+
+def docker_credentials_lookup(
+    docker_cfg: str | None=None,
+    absent_ok: bool=False,
+) -> collections.abc.Callable[[image_reference, Privileges, bool], OciConfig]:
+    '''
+    returns a credentials-lookup backed by docker's auth-config. By design, docker's auth-config
+    only allows configuring credentials per hostname. By default' docker-cfg is expected at
+    `$HOME/.docker/config.json`. Location of docker-cfg can be customised via docker_cfg parameter.
+
+    if no docker-cfg is found, raises RuntimeError, unless absent_ok is truthy, in which case the
+    returned lookup will never return any credentials (which might still be useful for readonly
+    operations that for many registries allow anonymous access).
+
+    Note that docker does not offer to configure credentials by permissions, too (hence privileges
+    parameter will be ignored)
+    '''
+    if not docker_cfg:
+        docker_cfg = os.path.join(os.environ.get('HOME', ''), '.docker/config.json')
+
+    if not os.path.isfile(docker_cfg):
+        if not absent_ok:
+            raise RuntimeError(f'not an existing file: {docker_cfg=}')
+
+        def find_nothing_lookup(
+            image_reference: str,
+            privileges: Privileges=Privileges.READONLY,
+            absent_ok: bool=False,
+        ):
+            if not absent_ok:
+                raise ValueError(f'no auth-cfg found in {docker_cfg=} for {image_reference=}')
+            return None
+
+        return find_nothing_lookup
+
+    def docker_auth_lookup(
+        image_reference: str,
+        privileges: Privileges=Privileges.READONLY,
+        absent_ok: bool=False,
+    ):
+        # re-read docker-cfg to reflect fs-updates
+        with open(docker_cfg) as f:
+            docker_auth = json.load(f)
+            auths = docker_auth.get('auths', None)
+
+        if not auths:
+            # docker-cfg might be empty - do not handle as an error; however, we can never serve
+            # anything useful
+            if not absent_ok:
+                raise ValueError(f'no auth-cfg found in {docker_cfg=} for {image_reference=}')
+            return None
+
+        if image_reference.startswith('/'):
+            # if it is a relative reference, we have not means to find appropriate cfg.
+            if not absent_ok:
+                raise ValueError(f'no auth-cfg found in {docker_cfg=} for {image_reference=}')
+            return None
+
+        # ignore ports - match cfg only by hostname
+        image_netloc = image_reference.split('/')[0]
+        image_host = image_netloc.split(':')[0]
+
+        for netloc, auth_dict in auths.items():
+            host = netloc.split(':')[0]
+            if host == image_host:
+                break
+        else:
+            if not absent_ok:
+                raise ValueError(
+                    f'no matching auth-cfg found in {docker_cfg=} for {image_reference=}'
+                )
+            return None # no matching cfg was found
+
+        # we found a cfg
+        # docker's auth-cfgs only have a single value `auth` (or so we hope / assume)
+        auth = auth_dict.get('auth', None)
+        if not auth:
+            raise ValueError(f'did not find expected attr `auth` in {docker_cfg=} for {image_host=}')
+
+        auth = base64.b64decode(auth).decode('utf-8')
+        username, passwd = auth.split(':', 1)
+
+        return OciBasicAuthCredentials(
+            username=username,
+            password=passwd,
+        )
+
+    return docker_auth_lookup
