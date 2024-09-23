31c3ba5f1f635692d58ae9e7bf8114fed20002d0
Christian Cwienk <christian.cwienk@sap.com>
2024-09-23 08:17:54 +0200

    fix: guard against erronously identifying component-reference as comp

--- cnudie/util.py
+++ cnudie/util.py
@@ -34,7 +34,8 @@ def to_component_id(
     if isinstance(component, ocm.ComponentDescriptor) or hasattr(component, 'component'):
         component = component.component
         # fall through to next case
-    if isinstance(component, ocm.Component) or hasattr(component, 'name'):
+    if isinstance(component, ocm.Component) or hasattr(component, 'name') \
+        and not hasattr(component, 'componentName'):
         name = component.name
         version = component.version
     if isinstance(component, ocm.ComponentReference) or hasattr(component, 'componentName'):
