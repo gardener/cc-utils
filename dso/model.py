e4b085e2a769707f6a6ec3ad26763b24b4d0eb86
TuanAnh17N <Tuan-Anh_Nguyen@gmx.de>
2025-03-21 14:05:12 +0100

    Introduce finding class for OS Id

--- dso/model.py
+++ dso/model.py
@@ -50,6 +50,7 @@ class Datasource:
     SAST = 'sast'
     CLAMAV = 'clamav'
     CC_UTILS = 'cc-utils'
+    OS_ID = 'os-id'
     CRYPTO = 'crypto'
     DELIVERY_DASHBOARD = 'delivery-dashboard'
     DIKI = 'diki'
@@ -79,6 +80,11 @@ class Datasource:
             ),
             Datasource.CC_UTILS: (
                 Datatype.OS_IDS,
+            ),
+            Datasource.OS_ID: (
+                Datatype.OS_ID,
+                Datatype.OS_ID_FINDING,
+                Datatype.ARTEFACT_SCAN_INFO,
             ),
             Datasource.CRYPTO: (
                 Datatype.ARTEFACT_SCAN_INFO,
@@ -253,6 +259,8 @@ class Datatype:
     SAST_FINDING = 'finding/sast'
     DIKI_FINDING = 'finding/diki'
     OS_IDS = 'os_ids'
+    OS_ID_FINDING = 'finding/os_id'
+    OS_ID = 'os_id'
     RESCORING = 'rescorings'
     COMPLIANCE_SNAPSHOTS = 'compliance/snapshots'
     ARTEFACT_SCAN_INFO = 'meta/artefact_scan_info'
@@ -266,9 +274,11 @@ class Datatype:
             Datatype.LICENSE: Datasource.BDBA,
             Datatype.VULNERABILITY: Datasource.BDBA,
             Datatype.OS_IDS: Datasource.CC_UTILS,
+            Datatype.OS_ID: Datasource.OS_ID,
             Datatype.MALWARE_FINDING: Datasource.CLAMAV,
             Datatype.DIKI_FINDING: Datasource.DIKI,
             Datatype.SAST_FINDING: Datasource.SAST,
+            Datatype.OS_ID_FINDING: Datasource.OS_ID,
             Datatype.CRYPTO_ASSET: Datasource.CRYPTO,
             Datatype.CRYPTO: Datasource.CRYPTO,
             Datatype.FALCO_FINDING: Datasource.FALCO,
@@ -419,6 +429,27 @@ class RescoreSastFinding:
     @property
     def key(self) -> str:
         return _as_key(self.sast_status, self.sub_type)
+
+
+@dataclasses.dataclass(frozen=True)
+class OsIdFinding(Finding):
+    os_id: unixutil.model.OperatingSystemId
+    os_status: OsStatus
+    greatest_version: str | None
+    eol_date: datetime.datetime | None
+
+    @property
+    def key(self) -> str:
+        return _as_key(self.os_id.ID)
+
+
+@dataclasses.dataclass(frozen=True)
+class RescoreOsIdFinding:
+    os_id: unixutil.model.OperatingSystemId
+
+    @property
+    def key(self) -> str:
+        return _as_key(self.os_id.ID)
 
 
 @dataclasses.dataclass(frozen=True)
@@ -608,6 +639,7 @@ class CustomRescoring:
         | MalwareFindingDetails
         | RescoreSastFinding
         | RescoringCryptoFinding
+        | RescoreOsIdFinding
     )
     referenced_type: str
     severity: str
@@ -818,6 +850,7 @@ class ArtefactMetadata:
         | SastFinding
         | DikiFinding
         | OsID
+        | OsIdFinding
         | CustomRescoring
         | ComplianceSnapshot
         | CryptoAsset
