aadc04b0a8ba2990f39922cc79028be1ba7d7af2
Christian Cwienk <christian.cwienk@sap.com>
2025-04-14 11:49:43 +0200

    allow passing in digest as alternative to file (to be hashed)

--- cli/gardener_ci/sign.py
+++ cli/gardener_ci/sign.py
@@ -2,17 +2,21 @@ import signingserver
 
 
 def sign(
-    filepath: str,
     signing_server_url: str,
     signing_server_client_cert: str,
     signing_server_client_cert_key: str,
+    digest: str=None,
+    filepath: str=None,
     output: str='signature',
     signing_server_certificate_ca: str=None,
     signing_algorithm: str='rsassa-pkcs1-v1_5',
     tls_validation: bool=True,
 ):
-    with open(filepath, 'rb') as f:
-        payload_bytes = f.read()
+    if filepath:
+        with open(filepath, 'rb') as f:
+            payload_bytes = f.read()
+    else:
+        payload_bytes = None
 
     signingserver_client = signingserver.SigningserverClient(
         cfg=signingserver.SigningserverClientCfg(
@@ -26,11 +30,12 @@ def sign(
 
     signature = signingserver_client.sign(
         content=payload_bytes,
+        digest=digest,
         signing_algorithm=signing_algorithm,
     )
 
     if output == 'signature':
-        print(signature.signature.replace('\n', ''), end='')
+        print(signature.signature)
     elif output == 'raw':
         print(signature.raw)
     elif output == 'certificate':
