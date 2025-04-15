592d30b95ccec40555184463a2319795d6710447
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2025-04-15 13:01:42 +0200

    Add utility function to create digests based on OCM accesses

--- cnudie/access.py
+++ cnudie/access.py
@@ -1,6 +1,10 @@
+import hashlib
+
 import ocm
 
 import ioutil
+import oci.client
+import oci.model
 
 
 def s3_access_as_blob_descriptor(
@@ -22,3 +26,52 @@ def s3_access_as_blob_descriptor(
         size=size,
         name=name or f's3://{s3_access.bucketName}/{s3_access.objectKey}',
     )
+
+
+def access_to_digest_lookup(
+    access: ocm.Access,
+    oci_client: oci.client.Client=None,
+    s3_client: 'botocore.client.S3'=None,
+    chunk_size: int=4096,
+) -> ocm.DigestSpec:
+    if access.type is ocm.AccessType.OCI_REGISTRY:
+        image_reference = oci.model.OciImageReference(
+            image_reference=oci_client.to_digest_hash(
+                image_reference=access.imageReference,
+                accept=oci.model.MimeTypes.prefer_multiarch,
+            )
+        )
+
+        digest = image_reference.digest
+
+        return ocm.DigestSpec(
+            hashAlgorithm='SHA-256',
+            normalisationAlgorithm=ocm.NormalisationAlgorithm.OCI_ARTIFACT_DIGEST,
+            value=digest,
+        )
+
+    elif access.type is ocm.AccessType.LOCAL_BLOB:
+        reference = access.globalAccess.digest if access.globalAccess else access.localReference
+
+        digest = reference.lower().removeprefix('sha256:')
+
+        return ocm.DigestSpec(
+            hashAlgorithm='SHA-256',
+            normalisationAlgorithm=ocm.NormalisationAlgorithm.GENERIC_BLOB_DIGEST,
+            value=digest,
+        )
+
+    elif access.type is ocm.AccessType.S3:
+        blob = s3_client.get_object(Bucket=access.bucketName, Key=access.objectKey)['Body']
+
+        digest = hashlib.sha256()
+        for chunk in blob.iter_chunks(chunk_size=chunk_size):
+            digest.update(chunk)
+
+        return ocm.DigestSpec(
+            hashAlgorithm='SHA-256',
+            normalisationAlgorithm=ocm.NormalisationAlgorithm.GENERIC_BLOB_DIGEST,
+            value=digest.hexdigest(),
+        )
+
+    return ocm.ExcludeFromSignatureDigest()
