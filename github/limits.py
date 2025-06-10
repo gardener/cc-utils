8c95c9bdc616b2823a26279d53964265c40f1003
Christian Cwienk <christian.cwienk@sap.com>
2025-06-10 10:31:32 +0200

    add limit for comment-bodies

--- github/limits.py
+++ github/limits.py
@@ -7,6 +7,7 @@ limits refer to amount of codepoints (tested empirically for some samples).
 '''
 
 issue_body = 65536
+comment_body = 65536
 issue_title = 256
 pullrequest_body = 262144
 release_body = 125000
