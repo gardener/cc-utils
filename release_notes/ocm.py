f4cc98c4987e623a643c24f4e3870d5997d88dca
Christian Cwienk <christian.cwienk@sap.com>
2025-06-17 08:58:35 +0200

    re-implement release-notes-retrieval from OCM

---
+++ release_notes/ocm.py
@@ -0,0 +1,98 @@
+'''
+another opinionated / gardener-specific module for managing release-notes in
+OCM-Component-Descriptors
+'''
+
+import collections.abc
+import zlib
+
+import oci.client
+import ocm
+import ocm.gardener
+import version as version_mod
+
+
+def release_notes(
+    component: ocm.ComponentIdentity | ocm.Component,
+    oci_client: oci.client.Client,
+    component_descriptor_lookup: ocm.ComponentDescriptorLookup | None=None,
+    absent_ok: bool=True,
+) -> str | None:
+    '''
+    retrieves (raw, i.e. in markdown / text fmt) release-notes for the given component version.
+
+    The release-notes are expected to be stored as a local-blob, and referenced from a resource
+    named `release-notes`.
+
+    If a full ocm.Component is passed-in, component_descriptor_lookup can be omitted (it is
+    otherwise used to retrieve component-descriptor). Either way, the component-descriptor is
+    expected to be stored in an OCI-Registry (hence the need for passing-in an oci-client).
+    '''
+    if not isinstance(component, ocm.Component):
+        component = component_descriptor_lookup(component).component
+
+    for resource in component.resources:
+        if resource.name == 'release-notes':
+            break
+    else:
+        if absent_ok:
+            return None
+        raise ValueError(f'{component=} has no resource named `release-notes`')
+
+    access = resource.access
+    if not access.type is ocm.AccessType.LOCAL_BLOB:
+        raise ValueError(f'do not know how to handle {access.type=} ({component=})')
+    access: ocm.LocalBlobAccess
+
+    oci_ref = component.current_ocm_repo.component_version_oci_ref(
+        name=component.name,
+        version=component.version,
+    )
+
+    release_notes_blob = oci_client.blob(
+        image_reference=oci_ref,
+        digest=access.localReference,
+    )
+
+    if access.mediaType.endswith('/gzip'):
+        release_notes_bytes = zlib.decompress(release_notes_blob.content, wbits=31)
+    else:
+        release_notes_bytes = release_notes_blob.content
+
+    return release_notes_bytes.decode('utf-8')
+
+
+def release_notes_range(
+    version_vector: ocm.gardener.UpgradeVector,
+    versions: collections.abc.Iterable[version_mod.Version],
+    oci_client: oci.client.Client,
+    component_descriptor_lookup: ocm.ComponentDescriptorLookup | None=None,
+    absent_ok: bool=True,
+) -> collections.abc.Iterable[tuple[ocm.ComponentIdentity, str]]:
+    '''
+    yields pairs of component-id and release-notes in specified range,
+    excluding release-notes for `whence`-version,
+    including release-notes for `whither`-version.
+    '''
+    versions_in_range = version_mod.iter_upgrade_path(
+        whence=version_vector.whence.version,
+        whither=version_vector.whither.version,
+        versions=versions,
+    )
+
+    for version in versions_in_range:
+        component_id = ocm.ComponentIdentity(
+            name=version_vector.component_name,
+            version=version,
+        )
+        notes = release_notes(
+           component=component_id,
+           oci_client=oci_client,
+           component_descriptor_lookup=component_descriptor_lookup,
+           absent_ok=absent_ok,
+        )
+
+        if not notes: # previous call would already have failed, if absent_ok were falsy
+            continue
+
+        yield component_id, notes
