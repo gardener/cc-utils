40d017c7d1b2b882ca5ea2d664ca729a5a69fcbf
Christian Cwienk <christian.cwienk@sap.com>
2025-12-19 13:54:35 +0100

    add more unittests for ocm.gardener

--- test/ocm/gardener_test.py
+++ test/ocm/gardener_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 import ocm
 import ocm.gardener as og
 
@@ -100,3 +102,150 @@ def test_oci_image_dict_from_resource():
         'tag': 'tag',
         'targetVersion': 'version-from-label',
     }
+
+
+def test_image_dict_from_image_dict_and_resource():
+    assert og.image_dict_from_image_dict_and_resource(
+        component_name='cname',
+        image={
+            'name': 'iname',
+        },
+        resource=res(),
+    ) == {
+        'name': 'iname',
+        'repository': 'acme.org/image-ref',
+        'sourceRepository': 'cname',
+        'tag': 'tag',
+    }
+
+    # labels should be retained
+    assert og.image_dict_from_image_dict_and_resource(
+        component_name='cname',
+        image={
+            'name': 'iname',
+            'labels': [{'name': 'some-label', 'value': 42}],
+        },
+        resource=res(),
+    ) == {
+        'name': 'iname',
+        'repository': 'acme.org/image-ref',
+        'sourceRepository': 'cname',
+        'tag': 'tag',
+        'labels': [{'name': 'some-label', 'value': 42}],
+    }
+
+    # target-version should be honoured
+    assert og.image_dict_from_image_dict_and_resource(
+        component_name='cname',
+        image={
+            'name': 'iname',
+            'targetVersion': 'tversion',
+        },
+        resource=res(),
+    ) == {
+        'name': 'iname',
+        'repository': 'acme.org/image-ref',
+        'sourceRepository': 'cname',
+        'tag': 'tag',
+        'targetVersion': 'tversion',
+    }
+
+
+def test_iter_image_dicts_from_image_dicts_and_resources():
+    # smoketest only as function mostly delegates
+    result = tuple(
+        og.iter_image_dicts_from_image_dicts_and_resources(
+            images=[{
+                'name': 'resource-name',
+            }],
+            component_name='acme.org/other-component',
+            resources=[res()],
+        )
+    )
+
+    assert len(result) == 1
+    result, = result
+    assert result == {
+        'name': 'resource-name',
+        'repository': 'acme.org/image-ref',
+        'tag': 'tag',
+        'sourceRepository': 'acme.org/other-component',
+    }
+
+
+def test_iter_oci_image_dicts_from_component():
+    # smoketest only as function mostly delegates
+    # important aspects to check:
+    # -> should process component-references
+    # -> should process component's resources
+
+    referenced_component = ocm.Component(
+        name='acme.org/referenced-component',
+        version='dontcare',
+        provider='acme.org',
+        sources=(),
+        repositoryContexts=(),
+        componentReferences=(),
+        resources=(
+            res(name='referenced-component-res-name'),
+        ),
+    )
+
+    root_component = ocm.Component(
+        name='acme.org/c1',
+        version='dontcare',
+        repositoryContexts=(),
+        provider='acme.org',
+        sources=(),
+        componentReferences=(
+            ocm.ComponentReference(
+                name='cref',
+                componentName=referenced_component.name,
+                version=referenced_component.version,
+                labels=(
+                    ocm.Label(
+                        name='imagevector.gardener.cloud/images',
+                        value={'images': [{
+                            'name': 'referenced-component-res-name',
+                        }]}
+                    ),
+                ),
+            ),
+        ),
+        resources=(
+            res(),
+        ),
+    )
+
+    def lookup(cid):
+        if cid != root_component.componentReferences[0]:
+            pytest.fail(f'component-descriptor-lookup was called with unexpected {cid=}')
+
+        return referenced_component
+
+    # call as parameterised in first round of iter_oci_image_dicts_from_rooted_component
+    result = og.iter_oci_image_dicts_from_component(
+        component=root_component,
+        resource_names_from_label=True,
+        fallback_to_target_version_from_resource=False,
+        resource_names=None,
+        component_descriptor_lookup=lookup,
+    )
+    result = tuple(result)
+
+    assert len(result) == 2
+
+    assert result == (
+        { # this image-dict is yielded from root_component's sub-component-reference
+            'name': 'referenced-component-res-name',
+            'repository': 'acme.org/image-ref',
+            'tag': 'tag',
+            'sourceRepository': 'acme.org/referenced-component',
+        },
+        { # this image-dict is yielded from root-component's resource
+            'name': 'name-from-label',
+            'repository': 'acme.org/image-ref',
+            'tag': 'tag',
+            'targetVersion': 'version-from-label',
+        }
+    )
