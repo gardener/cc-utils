470357f0e13e4fd76302801c0bed33d1983ec3c9
Christian Cwienk <christian.cwienk@sap.com>
2026-07-07 17:29:45 +0200

    ocm-validate: add transitive recursion support, default to -1

--- .github/actions/ocm-validate/validate.py
+++ .github/actions/ocm-validate/validate.py
@@ -1,5 +1,6 @@
 import yaml
 
+import cnudie.retrieve
 import ocm
 import ocm.iter
 import ocm.validate
@@ -10,16 +11,31 @@ import oci.client
 def validate(
     component_descriptor_path: str,
     validation_cfg: ocm.validate.ValidationCfg,
+    ocm_repositories: str = '',
+    recursion_depth: int = -1,
 ) -> tuple[int, list[ocm.validate.ValidationError], list[ocm.validate.ValidationError]]:
     with open(component_descriptor_path) as f:
         component_descriptor = yaml.safe_load(f)
 
     component_descriptor = ocm.ComponentDescriptor.from_dict(component_descriptor)
 
+    if recursion_depth != 0:
+        if not ocm_repositories:
+            raise ValueError(
+                '`ocm-repositories` must be provided when `recursion-depth` is non-zero'
+            )
+        repos = [r.strip() for r in ocm_repositories.split(',') if r.strip()]
+        ocm_repo_lookup = cnudie.retrieve.ocm_repository_lookup(*repos)
+        lookup = cnudie.retrieve.create_default_component_descriptor_lookup(
+            ocm_repository_lookup=ocm_repo_lookup,
+        )
+    else:
+        lookup = None
+
     nodes = ocm.iter.iter(
         component=component_descriptor,
-        lookup=None,
-        recursion_depth=0,
+        lookup=lookup,
+        recursion_depth=recursion_depth,
     )
 
     total = 0
