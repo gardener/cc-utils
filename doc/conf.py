b93c878527542485edeabce976f8526d790ea21a
Marc Vornetran <marc.vornetran@sap.com>
2026-08-26 17:09:53 +0200

    Fix copyright text: remove redundant "Copyright" prefix
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: Copyright Contributors to the Gardener project
+# SPDX-FileCopyrightText: Contributors to the Gardener project
 #
 # SPDX-License-Identifier: Apache-2.0
 
